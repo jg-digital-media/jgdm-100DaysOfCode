1ea30d4c413e568ab1e06a2c708762ef9c1593fa
jg-digital-media <mail@jonniegrieve.co.uk>
2018-09-17 14:52:46 +0100

    R1D6 finish up constructor methods

--- four_in_a_row/js/Board.js
+++ four_in_a_row/js/Board.js
@@ -2,7 +2,30 @@ class Board {
     constructor(rows = 6, columns = 7, spaces = []){
         this.rows = 6;
         this.columns = 7;
-        this.spaces = []
+        this.spaces = this.createSpaces();
+    }
+
+    /**
+     * Generates a 2D array of spaces
+     * @return {array}      An array of space objects
+     */
+    
+    createSpaces()  {
+    
+        const spaces = [];
+      
+        for(let x=0; x<this.spaces; x++) {
+            const column = [];
+    
+            for(let y=0; y<this.rows; x++) {
+                    const space = new Space(x, y);
+                    column.push(space);
+            }
+         
+            space.push(column);
+        }
+    
+        return spaces;
     }
     
 }
--- four_in_a_row/js/Game.js
+++ four_in_a_row/js/Game.js
@@ -1,3 +1,25 @@
 class Game {
-    
+
+    constructor(board, players, ready) {
+        this.board =  new Board();
+        this.players = this.createPlayers();
+        this.ready = false;
+
+    }
+
+    /** 
+     * Creates two player objects
+     * @return  {Array}    An array of two Player objects.
+     */
+    createPlayers(){
+
+        //array of objects - initialised as empty array.
+        player = []
+
+        let playerOne = new Player("Player 1","#e15258", 1, true);
+        let playerTwo = new Player("Player 2","#e59a13", 2);
+        player.push(playerOne, playerTwo)
+        return player;
+    }
+
 }

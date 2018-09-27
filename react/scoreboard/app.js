5d1c845e92926707010055557e25cf79f3ed467c
jg-digital-media <mail@jonniegrieve.co.uk>
2018-09-27 16:48:20 +0100

    R1D14: React with JSX and ReactDOM.render

---
+++ react/scoreboard/app.js
@@ -0,0 +1,21 @@
+//Write JSX
+const title = '<h1>My First React Element</h1>';
+const desc = '<p>I just learned how to create a react node and Render it into the DOM</p>';
+const myTitleID = 'main-title';
+const name = "Jonnie";
+
+const header = (
+    <header>
+        <h1 id={ myTitleID }>{ name }'s First React Element</h1>
+        <p> { desc }</p>
+        <input value={ 10 * 20 } />
+    </header>
+);
+
+//console.log(title);  //inspect newly defined React Element
+
+//render header component to the DOM
+ReactDOM.render(
+    header,
+    document.getElementById('root')
+)

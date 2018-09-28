a0694f37a225773e068985eec6431f15e559c97b
jg-digital-media <mail@jonniegrieve.co.uk>
2018-09-28 13:28:29 +0100

    R1D15:  React component syntax

--- react/scoreboard/app-arrow.js
+++ react/scoreboard/app-arrow.js
@@ -1,12 +1,12 @@
 //write a React Component as a JavaScript function
 
 /**
- *  React Component: Basic Syntax
+ *  React Component: Arrow Function Syntax
  *  @returns JSX 
  */
 
-function Header(){
-    return(
+const Header = () => {
+    return (
         <header>
             <h1>Scoreboard</h1>
             <span className="stats">Player 1: </span>

e5e01191c1bc0c57da8161911a98fad918c0fedc
Jonathan Grieve <mail@jonniegrieve.co.uk>
2018-10-25 15:54:10 +0100

    R1D34: implement high score feature

--- react/scoreboard_v2/src/components/App.js
+++ react/scoreboard_v2/src/components/App.js
@@ -64,7 +64,19 @@ class App extends Component {
     });
   }
 
+  getHighScore = () => {
+    const scores = this.state.players.map( p => p.score );
+    const highScore = Math.max(...scores);
+    if (highScore) {
+      return highScore;
+    } 
+    return null;
+  }
+
   render() {
+
+    const highScore = this.getHighScore();
+
     return (
       <div className="scoreboard">
         <Header 
@@ -81,7 +93,8 @@ class App extends Component {
             id={player.id}
             key={player.id.toString()} 
             changeScore={ this.handleScoreChange }
-            removePlayer={this.handleRemovePlayer}           
+            removePlayer={ this.handleRemovePlayer }  
+            isHighScore={ highScore === player.score }         
           />
         )}
         

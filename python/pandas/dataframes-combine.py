6ab20a37f2cd135a4fb4c085cef66dd3dd193378
jg-digital-media <mail@jonniegrieve.co.uk>
2018-11-29 09:29:58 +0000

    R1D58: dataframes and debug attempts for react scoreboard

--- python/pandas/dataframes-combine.py
+++ python/pandas/dataframes-combine.py
@@ -34,5 +34,21 @@ successful_requests['sent_date'] = pd.to_datetime(successful_requests['sent_date
 # And now we can see they are converted
 successful_requests.dtypes
 
+#
+successful_requests['time_passed'] = successful_requests.sent_date - successful_requests.request_date
+successful_requests.sort_values(by='time_passed', ascending=False).head(5)
+
+#
+"Wow! ${:,.2f} has passed through the request system in {} transactions!!!".format(
+    successful_requests.amount.sum(),
+    len(successful_requests),
+)
 
 # Further research.
+
+# Create a boolean Series of records that are duplicated. 
+#   Note that `keep=False` marks all that are duplicated
+dupes = requests[requests.duplicated(('from_user', 'to_user', 'amount'), keep=False)]
+# Order by requester and the date of request. 
+#   Note that `request_date` in this case is a string, but this string date format sorts properly still.
+dupes.sort_values(['from_user', 'request_date'])

edaa26067d44c9f186fb98db2eb58249d114c64d
Jonathan Grieve <mail@jonniegrieve.co.uk>
2018-12-06 12:22:04 +0000

    R1D64: styling and dynamic HTML with Vue

--- javascript/vue/scripts/app.js
+++ javascript/vue/scripts/app.js
@@ -9,32 +9,40 @@ const vue = new Vue({
 
     el: '#vue',
     data: {
-        pageTitle: 'New Vue Project',
-        pageDesc: 'Desc...',
-        register: register,
-        links: links
+        pageTitle: 'Vue.js Blog Page', //render with mustahce syntax
+        articleTitle: 'Using Vue',
+        pageDesc: 'Article name generated via the Vue Instance',
+        register: register,  //rendering arrays
+        links: links,
+        rawHtml: '<span style="color: red">Written by: </span>',  //rendering html
     },
     methods: {
+
+    },
+
+    computed: {
 
     }
 
 });
 
-Vue.component('to-do',{
-    template: '<li>Use of Component</li>'
-})
+
 
 /*
 Directives are prefixed with v- to indicate that they are special attributes provided by Vue, 
+
 {{ templateSyntax }} - for rendring directly to the screen via the html file.
-v-bind - for declarative rendering syntax
-v-if - loops and conditions
+v-bind - for declarative rendering syntax  - Mustaches cannot be used inside HTML attributes. Instead, use a v-bind directive:v-if - loops and conditions
 v-for - <li v-for="todo in todos">
 v-model - for 2 way data binding.
 v-on - for handling user inputs
 
 Vue.component('repeated-element')  <repeated-element></repeated-element>
 
+Vue.component('to-do',{
+    template: '<li>Use of Component</li>'
+})
 
+Properties: methods vs computed vs watched
 
 */ 

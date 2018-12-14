95a70b4c055bf6a8854563707249f1ce88e23f04
jg-digital-media <mail@jonniegrieve.co.uk>
2018-12-14 16:27:38 +0000

    R1D70: work on portfolio

--- css/portfolio/scripts/app.js
+++ css/portfolio/scripts/app.js
@@ -13,3 +13,9 @@ $(document).ready(function() {
         }
     });
 });
+
+//lite window
+
+function MM_openBrWindow(theURL,winName,features) { //v2.0
+    window.open(theURL,winName,features);
+  }

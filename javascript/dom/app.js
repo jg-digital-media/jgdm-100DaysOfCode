15e0c3c55c5e781677109b1890a8d9195867ab0a
jg-digital-media <mail@jonniegrieve.co.uk>
2018-12-17 15:33:08 +0000

    R1D71: start development on dom project.

--- javascript/dom/app.js
+++ javascript/dom/app.js
@@ -1,1 +1,15 @@
-console.log("app.js is connected to the app");
+//Sanity Check
+//console.log("app.js is connected to the app");
+
+
+const form = document.getElementById("add-to-list");
+const input = form.querySelector('input');
+
+form.addEventListener('submit', (e) => {
+
+    e.preventDefault();
+    console.log("The returned message is: " + input.value)
+    input.value = "";
+})
+
+//https://teamtreehouse.com/library/registering-names  5 minutes in.

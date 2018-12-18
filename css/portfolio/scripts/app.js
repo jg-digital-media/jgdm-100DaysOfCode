edcefe3efea0b043675953fc98ed1d650e9f8dcd
jg-digital-media <mail@jonniegrieve.co.uk>
2018-12-18 13:38:13 +0000

    R1D72: jgdm website development

--- css/portfolio/scripts/app.js
+++ css/portfolio/scripts/app.js
@@ -1,6 +1,19 @@
 //check for jQuery
 //alert("jQuery in place");
 
+//testimonials
+const testimonials = [
+ "I’ve always been pleased with the results Jonnie Grieve Digital Media has given me.",
+ "Not only did he give my website an important new visual look his SEO changes vastly improved my website ranking",
+ "I’ve acquired new business directly from it. We’ve worked together to make sure my website looks good on all devices.",
+ "Jonnie is professional, devoted to his work and always available to help no matter how small the job. Highly recommended",
+ "Sam Payne: Auto Cleanse North East: ",
+ "I have been delighted with the service we have had.  Excellent and caring, with a real determination to get it right.  Would thoroughly recommend."
+
+]
+
+
+//sticky header
 $(document).ready(function() {
     $(window).scroll(function(){
         let sticky = $('.main-nav'),

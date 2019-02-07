d0cecbc361f6532636e8154c0c3f5609ac143079
Jonathan Grieve <mail@jonniegrieve.co.uk>
2019-02-07 12:32:37 +0000

    R1D96: prepare for new vue website

--- javascript/vue/scripts/app.js
+++ javascript/vue/scripts/app.js
@@ -1,15 +1,14 @@
-//new vue instance. 
-
+//Declare arrays
 const register = ["Jack Bauer", "Tony Almeida", "Samiya Lynn", "Clayton Sampson", "Claire Holloway"];
 const links = ["http://freewarejava.com", "http://javascriptkit.com", "http://dynamicdrive.com", "http://cnn.com", "https://www.google.com"]
 
 
-//
+//New Vue Instance
 const vue = new Vue({
 
     el: '#vue',
     data: {
-        pageTitle: 'Vue.js Blog Page', //render with mustahce syntax
+        pageTitle: 'Vue.js Blog Page', //render with mustache syntax
         articleTitle: 'Using Vue',
         pageDesc: 'Article name generated via the Vue Instance',
         register: register,  //rendering arrays
@@ -28,14 +27,22 @@ const vue = new Vue({
 
 
 
-/*
+/*NOTES: 
+
+{{ templateSyntax }} - for rendering values directly to the screen via the html file.
+
 Directives are prefixed with v- to indicate that they are special attributes provided by Vue, 
 
-{{ templateSyntax }} - for rendring directly to the screen via the html file.
-v-bind - for declarative rendering syntax  - Mustaches cannot be used inside HTML attributes. Instead, use a v-bind directive:v-if - loops and conditions
-v-for - <li v-for="todo in todos">
-v-model - for 2 way data binding.
-v-on - for handling user inputs
+
+v-bind - for declarative rendering syntax  - Mustaches cannot be used inside HTML attributes. Instead, use a v-bind directive:
+
+v-if - loops and conditions
+
+v-for - <li v-for="todo in todos">  used for looping through arrays and values
+
+v-model - used for 2 way data binding.
+
+v-on - used for handling user inputs
 
 Vue.component('repeated-element')  <repeated-element></repeated-element>
 

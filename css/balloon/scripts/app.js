c0caaa7737e63391854b5ad612e4bc77889fea11
Jonathan Grieve <mail@jonniegrieve.co.uk>
2019-02-21 16:28:28 +0000

    R2D6: add slick and video content to balloon page

--- css/balloon/scripts/app.js
+++ css/balloon/scripts/app.js
@@ -1,1 +1,17 @@
 console.log("connected to app.js");
+
+$(document).ready(function(){
+    $('.stock-images').slick({
+      infinite: true,
+      slidesToShow: 1,
+
+    });
+  });
+
+  $(document).ready(function(){
+    $('.gallery').slick({
+      infinite: true,
+      slidesToShow: 1,
+
+    });
+  });

668d1f4114df6da454f8b3ec70d34cb71d46fd98
jg-digital-media <mail@jonniegrieve.co.uk>
2019-02-27 16:51:47 +0000

    R2D10: grammar, spelling checks

--- css/portfolio/thank-you.php
+++ css/portfolio/thank-you.php
@@ -2,9 +2,8 @@
 Author:         Jonathan Grieve @ Jonnie Grieve Digital Media
 Contact:        On Twitter  @jg_digitalMedia; On Facebook: https://www.facebook.com/jgDigitalMedia/?ref=bookmarks; 
 Email:          mail@jonniegrieve.co.uk
-Last Updated:   26th February 2019
+Last Updated:   27th February 2019
 --->
-
 
 <!DOCTYPE html>
 <html lang="en">

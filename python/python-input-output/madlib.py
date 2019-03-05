d0e8d6b0df02ba0839ca73476483a9f5a6f91631
Jonathan Grieve <mail@jonniegrieve.co.uk>
2019-03-05 12:29:45 +0000

    R2D14:  madllibs.py

--- python/python-input-output/madlib.py
+++ python/python-input-output/madlib.py
@@ -13,14 +13,14 @@ print("Welcome to my Simple Madlibs game")
 print("---------------------------------")
 
 verb = input("Give me a Verb: ")
-noun = input("Give me noun: ")
+noun = input("Give me a noun: ")
 adjective = input("Give me an adjective: ")
 
 # TODO: Output the template to the screen with the blanks filled out with what the user stated
 
 print("\nThank you.  Here are your results\n")
 
-print("I enjoy practice! I find it helps me to {verb} better", verb)
-print("Without practice, {%s} would probably not even work", noun)
-print("My code is getting more {%d} every single day!", adjective)
+print("I enjoy practice! I find it helps me to " +  verb + " better.")
+print("Without practice my", noun, "would probably not even work.")
+print("My code is getting more", adjective,"every single day!")
 print("\n\nThat's it! Thanks for using Madlibs\n\n")

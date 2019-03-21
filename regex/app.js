9a3393f301af58f2280a0612ba39fb0e0ad098b0
Jonathan Grieve <mail@jonniegrieve.co.uk>
2019-03-21 11:57:29 +0000

    R2D26: toggle all boxes - toggle first box

--- regex/app.js
+++ regex/app.js
@@ -1,9 +1,19 @@
 //console.log("The script is connected!");
 
 $(document).ready(function(){
-    $("#specific + h2").click(function(){
-        $("article").fadeOut("slow", function(){
+    //close/open all h2 within a section tag
+    $(".js-toggleAll").click(function(){
+        $("h2 + article").slideToggle("slow", function(){
+            
+        });
+    });
+
+    //Specific to first box    
+    $("#specific > h2").click(function(){
+        $("#specific > article").slideToggle("slow", function(){
             
         });
     });
 });
+
+

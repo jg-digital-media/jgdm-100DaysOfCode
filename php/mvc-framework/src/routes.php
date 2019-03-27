992f4b8cc5d33a647efdd526b93adf1e7b69726a
Jonathan Grieve <mail@jonniegrieve.co.uk>
2019-03-27 16:18:26 +0000

    R2D30: implement routes for the contact page

--- php/mvc-framework/src/routes.php
+++ php/mvc-framework/src/routes.php
@@ -5,6 +5,26 @@ use Slim\Http\Response;
 
 // Routes
 
+$app->get('/contact', function (Request $request, Response $response, array $args) {
+    // Render index view
+    return $this->renderer->render($response, 'contact-form.phtml', $args);
+});
+
+$app->post('/contact', function (Request $request, Response $response, array $args) {
+    // Render index view
+    return $this->renderer->render($response, 'contact-form.phtml', $args);
+
+    $args = array_merge($args, $request->getParsedBody());
+    if(!empty($args['name']) && !empty($args['email']) && !empty($args['msg'])) {
+        $mail = json_encode([$args['name'], $args['email'], $args['msg']]);
+        // Sample log message
+        $this->logger->notice($mail);
+
+        return $this->renderer->render($response, 'thankyou.phtml', $args);
+
+    }
+});
+
 $app->get('/[{name}]', function (Request $request, Response $response, array $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");

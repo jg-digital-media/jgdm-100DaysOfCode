09bb32f578e24155bfa0891c2b6b69229434722c
Jonathan Grieve <mail@jonniegrieve.co.uk>
2019-03-27 13:24:53 +0000

    R2D30: update regex pages

---
+++ regex/sections/groups.php
@@ -0,0 +1,84 @@
+        
+        <section id="groups">
+
+            <h2></h2>
+
+            <article>
+
+                <h3> <span>Regex:</span> - <span></span></h3>
+
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+
+                <p></p>
+
+                <p></p>
+
+                <p></p>
+
+                <h3> <span>Regex:</span> - <span></span></h3>
+
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+
+                <p></p>
+
+                <p></p>
+
+                <p></p>
+
+                <h3> <span>Regex:</span> - <span></span></h3>
+
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+
+                <p></p>
+
+                <p></p>
+
+                <p></p>
+
+                <h3> <span>Regex:</span> - <span></span></h3>
+
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+
+                <p></p>
+
+                <p></p>
+
+                <p></p>
+
+                <h3> <span>Regex:</span> - <span></span></h3>
+
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+
+                <p></p>
+
+                <p></p>
+
+                <p></p>
+
+            </article>
+        </section>
---
+++ regex/sections/language.php
@@ -0,0 +1,70 @@
+
+        <section id="language">
+
+            <h2></h2>
+
+            <article>
+
+                <h3> <span>Regex:</span> - <span></span></h3>
+
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+
+                <p></p>
+
+                <p></p>
+
+                <p></p>
+
+                <h3> <span>Regex:</span> - <span></span></h3>
+
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+
+                <p></p>
+
+                <p></p>
+
+                <p></p>
+
+                <h3> <span>Regex:</span> - <span></span></h3>
+
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+
+                <p></p>
+
+                <p></p>
+
+                <p></p>
+
+                <h3> <span>Regex:</span> - <span></span></h3>
+
+
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+
+                <p></p>
+
+                <p></p>
+
+                <p></p>
+
+             </article>
+        </section>

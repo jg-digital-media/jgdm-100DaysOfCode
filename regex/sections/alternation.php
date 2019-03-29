55c401dfce2eeb01df7483201beabb3ebaed0c2f
Jonathan Grieve <mail@jonniegrieve.co.uk>
2019-03-29 11:24:31 +0000

    R2D32: alternation.php

--- regex/sections/alternation.php
+++ regex/sections/alternation.php
@@ -1,67 +1,66 @@
 
         <section id="alternation">
 
-            <h2></h2>
+            <h2>Alternation</h2>
 
             <article>
 
-                <h3> <span>Regex:</span> - <span></span></h3>
+                <h3> <span>Regex:</span> - <span>grizzlybear|pandabear</span></h3>
 
                 <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
+                    <li>grizzlybearr</li>
+                    <li>pandabear</li>
+                    <li>koalabear (exclude)</li>
+                    <li>bear (exclude)</li>
                 </ul>
 
-                <p></p>
+                <p>Match either grizzlybear or pandabear with pipe (|) character.</p>
 
-                <p></p>
+                <p>Last 2 words not matched and therefore excluded.</p>
 
-                <p></p>
+                <p>Use grizzlybear|pandabear|[^(|koalabear|bear)] to negate the last 2 words.</p>
 
-                <h3> <span>Regex:</span> - <span></span></h3>
+                <h3> <span>Regex:</span> - <span>firestation|firehouse</span></h3>
 
                 <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
+                    <li>firestation</li>
+                    <li>firehouse</li>
+                    <li>firewalker (exclude)</li>
+                    <li>fire (exclude)</li>
+                </ul>                
 
-                <p></p>
+                <p>Match by alternating the 2 literal words excluding the last 2 by simply not specifying them.</p>
 
-                <p></p>
+                <p>The Pipe (|) naturally groups the expression it separates.</p>
 
-                <p></p>
+                <p>fire(station|house)  - would match the literal word fire and 2 other words with alternation.</p>
 
-                <h3> <span>Regex:</span> - <span></span></h3>
-
-                <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
-
-                <p></p>
-
-                <p></p>
-
-                <p></p>
-
-                <h3> <span>Regex:</span> - <span></span></h3>
+                <h3> <span>Regex:</span> - <span>farmhouse|firehouse|bighouse</span></h3>
 
                 <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
+                    <li>farmhouse</li>
+                    <li>bighouse</li>
+                    <li>firehouse</li>
+                    <li>house (exclude)</li>
                 </ul>
 
-                <p></p>
+                <p>Uses a group to match the 3 different wordsin the first 3 matches - matching the beginning of each word </p>
 
-                <p></p>
+                <p>Finishes by matching the literal word "house"</p>
+
+                <h3> <span>Regex:</span> - <span>prot|proj|ector|ractor</span></h3>
+
+                <ul>
+                    <li>projector</li>
+                    <li>protractor</li>
+                    <li>proctor (exclude)</li>
+                </ul>
+
+                <p>Alternating groups of words to match the first 2 words above. </p>
+
+                <p>proj - matches projector but not protractor</p>
+
+                <p>prot - matches protractor but not projector - never matches proctor</p>
 
                 <p></p>
 

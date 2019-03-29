380c9fed15105bfc5ad9da783f512da253af8eac
Jonathan Grieve <mail@jonniegrieve.co.uk>
2019-03-29 11:25:35 +0000

    Add tests to all pages

--- regex/sections/beginning-end.php
+++ regex/sections/beginning-end.php
@@ -8,11 +8,11 @@
                 <h3> <span>Regex:</span> - <span></span></h3>
 
                 <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li>)</li>
+                    <li>tart</li>
+                    <li>start (exclude)</li>
+                    <li>tartan (exclude)</li>
                 </ul>
+
 
                 <p></p>
 
@@ -23,10 +23,14 @@
                 <h3> <span>Regex:</span> - <span></span></h3>
 
                 <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li>)</li>
+                    <li>img_01.jpg</li>
+                    <li>img_02.png</li>
+                    <li>img_03.gif</li>
+                    <li>img_04.png</li>
+                    <li>img_05.gif</li>
+                    <li>img_06.jpg</li>
+                    <li>6_img_07.gif (exclude)</li>
+                    <li>mov_01.avi (exclude)</li>
                 </ul>
 
                 <p></p>
@@ -39,11 +43,15 @@
                 <h3> <span>Regex:</span> - <span></span></h3>
 
                 <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li>)</li>
+                    <li>projector</li>
+                    <li>protractor</li>
+                    <li>projected</li>
+                    <li>proctor (exclude) </li>
+                    <li>my projector (exclude)</li>
+                    <li>projecting (exclude)</li>
+                    <li>projectorlight (exclude)</li>
                 </ul>
+
 
                 <p></p>
 
@@ -54,11 +62,12 @@
                 <h3> <span>Regex:</span> - <span></span></h3>
 
                 <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
+                    <li>3456</li>
+                    <li>34567</li>
+                    <li>23456 (exclude)</li>
+                    <li>345678 (exclude)</li>
                 </ul>
+
 
                 <p></p>
 
--- regex/sections/groups.php
+++ regex/sections/groups.php
@@ -1,17 +1,38 @@
         
         <section id="groups">
 
-            <h2></h2>
+            <h2>Grouping</h2>
 
             <article>
 
                 <h3> <span>Regex:</span> - <span></span></h3>
 
+                <p>Grouping characters allow you to apply modifications to multiple characters the way you have learned to do with single characters.</p>
+
                 <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
+                    <li>img_01.jpg</li>
+                    <li>img_02.png</li>
+                    <li>img_03.gif</li>
+                    <li>img_04.png</li>
+                    <li>img_05.gif</li>
+                    <li>img_06.jpg</li>
+                    <li>mov_01.avi (exclude)</li>
+                </ul>
+
+                <p>Grouping characters allow you to apply modifications to multiple characters the way you have learned to do with single characters.</p>
+
+                <p></p>
+
+                <p></p>
+
+                <h3> <span>Regex:</span> - <span></span></h3>
+
+                <ul>
+                    <li>projector</li>
+                    <li>protractor</li>
+                    <li>proctor</li>
+                    <li>prospector (exclude)</li>
+
                 </ul>
 
                 <p></p>
@@ -23,11 +44,18 @@
                 <h3> <span>Regex:</span> - <span></span></h3>
 
                 <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
+                    <li>img_sm_01.jpg</li>
+                    <li>img_01.jpg</li>
+                    <li>img_sm_02.jpg</li>
+                    <li>img_02.jpg</li>
+                    <li>img_sm_03.jpg</li>
+                    <li>img_03.jpg</li>
+                    <li>img_sm_04.png</li>
+                    <li>img_04.png</li>
+                    <li>img.gif (exclude)</li>
                 </ul>
+       
+
 
                 <p></p>
 
@@ -38,47 +66,17 @@
                 <h3> <span>Regex:</span> - <span></span></h3>
 
                 <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
+                    <li>www.github.com</li>
+                    <li>github.com</li>
+                    <li>www.teamtreehouse.com</li>
+                    <li>teamtreehouse.com</li>
+                    <li>api.github.com</li>
+                    <li>jsfiddle.net (exclude)</li>
+                    <li>www.jsfiddle.net (exclude)</li>
                 </ul>
 
-                <p></p>
 
-                <p></p>
 
-                <p></p>
-
-                <h3> <span>Regex:</span> - <span></span></h3>
-
-                <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
-
-                <p></p>
-
-                <p></p>
-
-                <p></p>
-
-                <h3> <span>Regex:</span> - <span></span></h3>
-
-                <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
-
-                <p></p>
-
-                <p></p>
-
-                <p></p>
 
             </article>
         </section>

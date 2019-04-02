a46f544ca710797fe6f83d63a6602fa3c3f67bd3
Jonathan Grieve <mail@jonniegrieve.co.uk>
2019-04-02 16:30:10 +0100

    R2D34: experiment with new twig project

---
+++ php/twig/public/index.php
@@ -0,0 +1,18 @@
+<?php 
+
+//path to the composer vendor file
+require_once __DIR__. '/../vendor/autoload.php';
+
+//render index template to same file
+/* $loader = new \Twig\Loader\ArrayLoader([
+    'index' => 'Hello {{ name }}!',
+]); */
+
+
+//render a directory template
+$loader = new \Twig\Loader\FilesystemLoader( __DIR__ . '/../templates');
+$twig = new \Twig\Environment($loader);
+
+//specify a template or a file to render
+echo $twig->render('home.twig', ['name' => 'Jonnie']);
+?>

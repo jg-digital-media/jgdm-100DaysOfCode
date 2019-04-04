d65d2dc0f2643091123d1bd1b62c02966467c2f3
Jonathan Grieve <mail@jonniegrieve.co.uk>
2019-04-04 10:24:41 +0100

    R2D36: update header

--- regex/sections/header.php
+++ regex/sections/header.php
@@ -27,17 +27,20 @@
         <aside class="list-container">
 
             <h2>Contents</h2>
-            <ul>Contents
+            
+            <ul>
                 <li><a href="#" class="list">Matching Specific Characters</a></li>
                 <li><a href="#" class="list">Matching Character Ranges</a></li>
                 <li><a href="#" class="list">Using Wildcard Characters</a></li>
                 <li><a href="#" class="list">Finding Repeated Characters</a></li>
                 <li><a href="#" class="list">Excluding Characters</a></li>
-                <li><a href="#" class="list">Atnernation</a></li>
+                <li><a href="#" class="list">Alternation</a></li>
                 <li><a href="#" class="list">Groups</a></li>
                 <li><a href="#" class="list">Beginning and Ending of Strings</a></li>
-                <li><a href="#" class="list js-toggleAll" id="toggle">Toggle All</a></li>
             </ul>
+            
         </aside>
+
+        <a href="#" class="toggle-btn js-toggleAll">Toggle All</a>
 
        

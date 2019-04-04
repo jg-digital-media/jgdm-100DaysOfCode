1bc5fd1561a3a85faf57e81dc83ddd2177a084d6
Jonathan Grieve <mail@jonniegrieve.co.uk>
2019-04-04 15:55:41 +0100

    R2D36: twig arrays and dates

--- php/twig/public/index.php
+++ php/twig/public/index.php
@@ -13,6 +13,22 @@ require_once __DIR__. '/../vendor/autoload.php';
 $loader = new \Twig\Loader\FilesystemLoader( __DIR__ . '/../templates');
 $twig = new \Twig\Environment($loader);
 
+
+//an array for navigation
+$nav = [
+    'home' => [
+        'href' => '/jgdm-100daysofcode/php/twig/index.php',
+        'caption' => 'Welcome',
+        'status' => 'active'
+    ],
+    'contact' => [
+        'href' => '/contact',
+        'caption' => 'Contact Us',
+        'status' => false
+    ]
+
+];
+
 //specify a template or a file to render
-echo $twig->render('home.twig', ['name' => 'jonnie']);
+echo $twig->render('home.twig', ['name' => 'jonnie', 'nav' => $nav]);
 ?>

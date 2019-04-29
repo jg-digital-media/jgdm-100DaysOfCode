28afdea8ea1c55f0dd1d6bf69d56bdcdf753a8eb
Jonathan Grieve <mail@jonniegrieve.co.uk>
2019-04-29 16:21:50 +0100

    R2D45: new php project list as homepage

---
+++ php/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP Project List</title>
+</head>
+<body>
+
+<ul>
+    <li><strong>mvc-framework</strong> - Treehouse Example - Ralph Waldo Emerson. </li>
+    <li><strong>php_test</strong> -  Gettng into practice with Unit testing for PHP.</li>
+    <li><strong>slim</strong> - Custom Project - A Website built with Slim Routing. </li>
+    <li><strong>slimmin</strong> -  Single plage App with Slim.</li>
+    <li><strong>twig</strong> - Custom Project with the Twig Tmplate Engine in Slim.</li> 
+    <li><strong>twig_project</strong> - Currently empty - A new project in Twig.</li>
+</ul>
+    
+</body>
+</html>

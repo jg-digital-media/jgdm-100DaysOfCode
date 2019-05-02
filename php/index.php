87e92d22a096fd2586bb51652a53af43271bdbb5
Jonathan Grieve <mail@jonniegrieve.co.uk>
2019-05-02 14:56:51 +0100

    R2D48: fix index.php in php dir

--- php/index.php
+++ php/index.php
@@ -10,10 +10,10 @@
 
 <ul>
     <li><strong>mvc-framework</strong> - Treehouse Example - Ralph Waldo Emerson. </li>
-    <li><strong>php_test</strong> -  Gettng into practice with Unit testing for PHP.</li>
+    <li><strong>php_test</strong> -  Getting into practice with Unit testing for PHP.</li>
     <li><strong>slim</strong> - Custom Project - A Website built with Slim Routing. </li>
-    <li><strong>slimmin</strong> -  Single plage App with Slim.</li>
-    <li><strong>twig</strong> - Custom Project with the Twig Tmplate Engine in Slim.</li> 
+    <li><strong>slimmin</strong> - Single page App with Slim.</li>
+    <li><strong>twig</strong> - Custom Project with the Twig Template Engine in Slim.</li> 
     <li><strong>twig_project</strong> - Currently empty - A new project in Twig.</li>
 </ul>
     

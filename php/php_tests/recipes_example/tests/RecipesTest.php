dd689b7d8ce445c539e88b638c2b301063c8c2c5
Jonathan Grieve <mail@jonniegrieve.co.uk>
2019-05-10 09:58:56 +0100

    R2D52: final tests

--- php/php_tests/recipes_example/tests/RecipesTest.php
+++ php/php_tests/recipes_example/tests/RecipesTest.php
@@ -10,4 +10,25 @@ class RecipesTest extends TestCase {
         $this->assertInstanceOf('Recipe', $recipe);
         $this->assertEquals('', $recipe->getTitle());
     }
+
+    function addIngredientMustReceiveValidAmount() {
+        $this->expectException(\InvalidArgumentException::class);
+        $recipe = new Recipe();
+        $recipe->addIngredient("garlic", "two");
+    }
+
+    function addIngredientMustReceiveValidMeasurement() {
+
+        $this->expectExceptionMessage('Please enter a valid measurement: tsp, tsps, cup, oz, fl, pint, quart, gallons');
+        $recipe = new Recipe();
+        $recipe->addIngredient("garlic", 2, "tbl");
+     }
+    
+     function canCallRecipeDirectly() {
+        $recipe = new Recipe();
+        $this->assertStringContainsString( 
+           'The following methods are available', 
+           $recipe );
+       
+       }
 }

c350cb00d7732fcb00acab837dac07f872e7442f
Jonathan Grieve <mail@jonniegrieve.co.uk>
2019-05-15 13:21:46 +0100

    R2D56: add the model for reviews and courses

--- php/php_rest_api/src/Model/Course.php
+++ php/php_rest_api/src/Model/Course.php
@@ -19,27 +19,47 @@ class Course {
         return $statement->fetchAll();
     }
 
+    //get a one course by a specific ID
     public function getCourse($course_id) {
         $statement = $this->database->prepare(
 	        'SELECT * FROM courses WHERE id=: id'
         );
         $statement->bindParam('id', $course_id);
         $statement->execute();
-        return $statement->fetchAll();
+        return $statement->fetch();
+    }
+    
+    //use INSERT INTO to add a new course to the table.
+    public function createCourse($data) {
+        $statement = $this->database->prepare(
+	        'INSERT INTO courses(title, url) VALUES (:title, :url)'
+        );
+        $statement->bindParam('title', $data['title']);
+        $statement->bindParam('url', $data['url']);
+        $statement->execute();
+        return $this->getCourse($this->database->lastInsertId());
+    }
+    
+    //UPDATE a course by a specific id
+    public function updateCourse($data) {
+        $statement = $this->database->prepare(
+	        'UPDATE courses SET title=:title, url=:url) WHERE id:= id'
+        );
+        $statement->bindParam('title', $data['title']);
+        $statement->bindParam('url', $data['url']);
+        $statement->bindParam('id', $data['course_id']);
+        $statement->execute();
+        return $this->getCourse($data['course_id']);
+    }
+    
+    //DELETE a specific course from table.
+    public function deleteCourse($course_id) {
+        $statement = $this->database->prepare(
+	        'DELETE FROM courses WHERE id:= id'
+        );
+        $statement->bindParam('id', $data['course_id']);
+        $statement->execute();
+        return $this->getCourse($data['course_id']);
     }
 
-    public function getReview() {
-
-    }
-    public function createReview() {
-
-    }
-    public function updateReview() {
-
-    }
-    public function deleteReview() {
-
-    }
-
-    public function getReviewsByCourseId() {}
 }
---
+++ php/php_rest_api/src/Model/Review.php
@@ -0,0 +1,73 @@
+<?php 
+
+//Autoload the class
+namespace App\Model;
+
+class Course {
+
+    public function getReviewsByCourseId($course_id)
+    {
+        
+        $statement = $this->database->prepare('SELECT * FROM reviews WHERE course_id=:course_id');
+        $statement->bindParam('course_id', $course_id);
+        $statement->execute();
+        $reviews = $statement->fetchAll();
+        return $reviews;
+    }
+
+    //Reviews CRUD operations
+    public function getReview($review_id) {
+        $statement = $this->database->prepare(
+            'SELECT * FROM reviews WHERE id=: id'
+        );
+
+        $statement->bindParam('id', $course_id);
+        $statement->execute();
+        return $statement->fetchAll();
+
+    }
+    
+    public function createReview($data) {
+        $statement = $this->database->prepare('INSERT INTO reviews (course_id, rating, comment) VALUES (:course_id, :rating, :comment)');
+        $statement->bindParam('course_id', $data['course_id']);
+        $statement->bindParam('rating', $data['rating']);
+        $statement->bindParam('comment', $data['comment']);
+        $statement->execute();
+        if ($statement->rowCount()>0) {
+            return $this->getReview($this->database->lastInsertId());
+        }
+    }
+    
+    public function updateReview($data) {        
+        $this->getReview($data['review_id']);
+        $statement = $this->database->prepare('UPDATE reviews SET rating=:rating, comment=:comment WHERE id=:id');
+        $statement->bindParam('id', $data['review_id']);
+        $statement->bindParam('rating', $data['rating']);
+        $statement->bindParam('comment', $data['comment']);
+        $statement->execute();
+        if ($statement->rowCount()>0) {
+            return $this->getReview($data['review_id']);
+        }
+    }
+
+    
+    public function deleteReview($id) {
+        $statement = $this->database->prepare(
+	        'DELETE FROM reviews WHERE id:= id'
+        );
+        $statement->bindParam('id', $data['course_id']);
+        $statement->execute();
+        return $this->getCourse($data['course_id']);
+
+    }
+    
+    public function deleteReview($review_id) {
+        $statement = $this->database->prepare('DELETE FROM reviews WHERE id=:id');
+        $statement->bindParam('id', $review_id);
+        $statement->execute();
+        if ($statement->rowCount()>0) {
+            return ['message' => 'The review was deleted.'];
+        }
+    }
+}
+

e7d2b5b5c25ea206f651365e5c54a9435edf9823
Jonathan Grieve <mail@jonniegrieve.co.uk>
2019-05-24 14:06:24 +0100

    R2D63: update php project list

--- php/index.php
+++ php/index.php
@@ -5,16 +5,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>PHP Project List</title>
+
+    <style>
+    
+    li {
+        margin: 10px 0;
+    }
+
+    a {
+        color: red;git status
+        
+
+    }
+
+    a:visited {
+        color: red;
+    }
+    </style>
 </head>
 <body>
 
+<h1>PHP Project List - #100DaysOfCode</h1>
+
 <ul>
-    <li><strong>mvc-framework</strong> - Treehouse Example - Ralph Waldo Emerson. </li>
-    <li><strong>php_test</strong> -  Getting into practice with Unit testing for PHP.</li>
-    <li><strong>slim</strong> - Custom Project - A Website built with Slim Routing. </li>
-    <li><strong>slimmin</strong> - Single page App with Slim.</li>
-    <li><strong>twig</strong> - Custom Project with the Twig Template Engine in Slim.</li> 
-    <li><strong>twig_project</strong> - Currently empty - A new project in Twig.</li>
+    <li><strong>mvc-framework</strong> - Treehouse Example - Ralph Waldo Emerson. <a href="http://localhost/jgdm-100daysofcode/php/mvc_framework" target="blank">http://localhost/jgdm-100daysofcode/php/mvc_framework</a> </li>
+    <li><strong>php_auth</strong> - PHP Authentication and Authorisation example. <a href="http://localhost/jgdm-100daysofcode/php/php_auth" target="blank">http://localhost/jgdm-100daysofcode/php/php_auth</a> </li>
+    <li><strong>php_rest_api</strong> - Using a REST API in PHP with Slim. <a href="http://localhost/jgdm-100daysofcode/php/php_rest_api" target="blank">http://localhost/jgdm-100daysofcode/php/php_rest_api</a></li>
+    <li><strong>php_test</strong> -  Getting into practice with Unit testing for PHP. <a href="http://localhost/jgdm-100daysofcode/php/php_test" target="blank">http://localhost/jgdm-100daysofcode/php/php_test</a></li>
+    <li><strong>slim</strong> - Custom Project - A Website built with Slim Routing. <a href="http://localhost/jgdm-100daysofcode/php/slim" target="blank">http://localhost/jgdm-100daysofcode/php/slim</a></li>
+    <li><strong>slimmin</strong> - Single page App with Slim. <a href="http://localhost/jgdm-100daysofcode/php/slim" target="blank">http://localhost/jgdm-100daysofcode/php/slim</a></li>
+    <li><strong>twig</strong> - Custom Project with the Twig Template Engine in Slim. <a href="http://localhost/jgdm-100daysofcode/php/twig target="blank"">http://localhost/jgdm-100daysofcode/php/twig</a></li> 
+    <li><strong>twig_project</strong> - Currently empty - A new project in Twig. <a href="http://localhost/jgdm-100daysofcode/php/twig_project" target="blank">http://localhost/jgdm-100daysofcode/php/twig_project</a></li>
 </ul>
     
 </body>

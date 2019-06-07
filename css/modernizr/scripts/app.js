e4871b52986130c2ec6a5271b228ca8e408d3508
Jonathan Grieve <mail@jonniegrieve.co.uk>
2019-06-07 17:18:13 +0100

    R2D69: add a side navigation

---
+++ css/modernizr/scripts/app.js
@@ -0,0 +1,10 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }

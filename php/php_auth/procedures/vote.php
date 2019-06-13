cf921e34304a646aa0a40def7e0a2b9766273deb
Jonathan Grieve <mail@jonniegrieve.co.uk>
2019-06-13 12:26:19 +0100

    R2D72: update voting system

--- php/php_auth/procedures/vote.php
+++ php/php_auth/procedures/vote.php
@@ -2,6 +2,8 @@
 require __DIR__ . '/../inc/bootstrap.php';
 requireAuth();
 
-vote(request()->get('bookId'), request()->get('vote'));
+$user = getAuthenticatedUser();
+
+vote(request()->get('bookId'), request()->get('vote'), $user['id']);
 
 redirect('/jgdm-100daysofcode/php/php_auth/books.php');

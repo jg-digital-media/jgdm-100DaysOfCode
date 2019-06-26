7460743ef3fc520e1d308fa57e7a439654e4cc23
Jonathan Grieve <mail@jonniegrieve.co.uk>
2019-06-26 13:20:43 +0100

    R2D78: responsive web design + font

--- javascript/ajax/project/app.js
+++ javascript/ajax/project/app.js
@@ -1,3 +1,29 @@
 //sanity check
 console.log("connected!");
 
+// Build JSON Data source
+// Get aside list  #list to populate with json data
+// build html for ajax request  one list item for each stock item
+    //.stock-image
+    //.description
+    //.low-stock
+    //.in-stock
+    //.out-stock
+
+/*ajax
+
+       //instantiate a new object to work with AJAX
+        const xhr = new XMLHttpRequest();
+
+        xhr.onreadystatechange = function() {
+
+
+            if (xhr.readyState === 4) {
+                //code
+                document.getElementById('ajax').innerHTML = xhr.responseText;
+            } else {
+                document.write("Error loading file");
+            }
+
+        };
+*/

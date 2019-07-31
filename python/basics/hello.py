8fbc12c46365dd7d65d717f0171d5cdffeaafa30
Jonathan Grieve <mail@jonniegrieve.co.uk>
2019-07-31 17:25:39 +0100

    R3D1: add hello.py

---
+++ python/basics/hello.py
@@ -0,0 +1,11 @@
+first_name = input("What is your name? >> ")
+
+print(first_name)
+print("Print Hint: ", "These", "will be", "joined together by spaces")
+
+print('Hello,', first_name)
+print(first_name, "is learning Python")
+
+## Catch a NameError: 
+## Catch a SyntaxError: 
+## Cach a TypeError

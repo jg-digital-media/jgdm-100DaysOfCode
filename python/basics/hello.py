087a51f5644a4f4da534322ea974f38e11d9ed8c
jg-digital-media <mail@jonniegrieve.co.uk>
2019-08-02 17:37:21 +0100

    R3D3: update python file

--- python/basics/hello.py
+++ python/basics/hello.py
@@ -1,10 +1,19 @@
+
+#get input from the user
 first_name = input("What is your name? >> ")
 
 print(first_name)
-print("Print Hint: ", "These", "will be", "joined together by spaces")
+#print("Print Hint: ", "These", "will be", "joined together by spaces")
 
-print('Hello,', first_name)
-print(first_name, "is learning Python")
+if first_name == "Jonnie":
+    print(first_name, "is learning Python")
+elif first_name == "Craig":
+    print(first_name, "is learning Python, hello Craig")
+else: 
+    print(first_name, "you should learn Python")
+print("Thanks for your interest in Python! Have a great day")
+
+
 
 ## Catch a NameError: 
 ## Catch a SyntaxError: 

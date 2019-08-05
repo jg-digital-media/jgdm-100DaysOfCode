12f0a344ead43c00a0a61de1bc6c4cf9944798bf
Jonathan Grieve <mail@jonniegrieve.co.uk>
2019-08-05 16:27:49 +0100

    R3D4: add notify.py - functions

---
+++ python/basics/notify.py
@@ -0,0 +1,11 @@
+
+#functions
+def notify(message): 
+    message = message
+    praise += message.upper();
+    number_of_characters = len(message)	
+    result = message + "!" * (number_ofcharacters// 2)
+end
+
+notify("You are doing great")
+

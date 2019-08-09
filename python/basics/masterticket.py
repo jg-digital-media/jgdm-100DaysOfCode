17ab5c9754ef39a45da155839a43cf3e3969a8b4
Jonathan Grieve <mail@jonniegrieve.co.uk>
2019-08-09 17:25:47 +0100

    R3D8: add project file masterticket.py

---
+++ python/basics/masterticket.py
@@ -0,0 +1,12 @@
+# Master Ticket.py
+
+#show the number of tickets left remaining
+#Personalised welcome 
+#Errors reported in a user friendly manner
+#Request a certain amount of tickets and be told the total cost 
+#Confirm order
+#Cannot be able to order tickets that are not available 
+
+
+
+

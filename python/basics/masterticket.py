5b34476983c26746e75cb8b430bab562d72ab822
jg-digital-media <mail@jonniegrieve.co.uk>
2019-08-23 16:14:22 +0100

    R3D17: gather info - master ticket

--- python/basics/masterticket.py
+++ python/basics/masterticket.py
@@ -1,9 +1,39 @@
 # Master Ticket.py
 
-#show the number of tickets left remaining
-#Personalised welcome 
+#Config
+TICKET_PRICE = 10
+tick_remain = 100
+
+
+welcome = "Welcome to MasterTicket"
+rule = "-" * len(welcome)
+print(welcome)
+print(rule)
+print("\n\n")
+
+get_name = input("Enter your name to begin work: ")
+
+
+def greeting(name):
+    print("Hello, {}. Thank you for using MasterTicket".format(get_name))
+
+greeting(get_name)
+
+
+print("There are {} Tickets left.".format(tick_remain))
+
+def calc_price():
+    #Request a certain amount of tickets and be told the total cost 
+    print("The Price for 1 ticket is {}".format(TICKET_PRICE))
+    get_tickets = int(input("How many tickets have been requested? "))
+
+    # calculate the total price
+    calc_tickets = get_tickets * TICKET_PRICE
+    print("The total price is {}".format(calc_tickets))
+
+calc_price()
+
 #Errors reported in a user friendly manner
-#Request a certain amount of tickets and be told the total cost 
 #Confirm order
 #Cannot be able to order tickets that are not available 
 

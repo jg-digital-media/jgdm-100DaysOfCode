d48edf7e18de258272315c67179a8c75916e5f47
Jonathan Grieve <mail@jonniegrieve.co.uk>
2019-09-02 16:36:00 +0100

    R3D19: looping and ticket calculation

--- python/basics/masterticket.py
+++ python/basics/masterticket.py
@@ -4,34 +4,50 @@
 TICKET_PRICE = 10
 tick_remain = 100
 
+# run this code continuously until no tickets
+
 
 welcome = "Welcome to MasterTicket"
 rule = "-" * len(welcome)
 print(welcome)
 print(rule)
-print("\n\n")
+print("\n")
+
+
 
 get_name = input("Enter your name to begin work: ")
-
 
 def greeting(name):
     print("Hello, {}. Thank you for using MasterTicket".format(get_name))
 
-greeting(get_name)
+while tick_remain > 0:
 
+    greeting(get_name)
 
-print("There are {} Tickets left.".format(tick_remain))
+    #print("There are {} Tickets left.".format(tick_remain))
 
-def calc_price():
-    #Request a certain amount of tickets and be told the total cost 
-    print("The Price for 1 ticket is {}".format(TICKET_PRICE))
-    get_tickets = int(input("How many tickets have been requested? "))
+    def calc_price():
+        #Request a certain amount of tickets and be told the total cost 
+        print("The Price for 1 ticket is {}".format(TICKET_PRICE))
+        get_tickets = int(input("How many tickets have been requested? "))
 
-    # calculate the total price
-    calc_tickets = get_tickets * TICKET_PRICE
-    print("The total price is {}".format(calc_tickets))
+        # calculate the total price
+        calc_tickets = get_tickets * TICKET_PRICE
+        print("The total price is {}".format(calc_tickets))
 
-calc_price()
+        proceed = input("Do you want to proceed with this? ").lower()
+
+        if proceed == "y":
+            print("SOLD\n\n")
+            pass_tickets = tick_remain - get_tickets 
+            print("There are now {} ticket(s) remaining\n".format(pass_tickets))
+        else:
+            print("Thanks, {} for using MasterTicket\n".format(get_name))
+
+    calc_price()
+     
+# Notify tht tickets are sold out
+
 
 #Errors reported in a user friendly manner
 #Confirm order

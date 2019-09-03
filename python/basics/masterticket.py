a75e02a77a8a3e984e63a97cab22a5882a7b65de
Jonathan Grieve <mail@jonniegrieve.co.uk>
2019-09-03 15:52:41 +0100

    R3D20: masterticket

--- python/basics/masterticket.py
+++ python/basics/masterticket.py
@@ -20,33 +20,48 @@ get_name = input("Enter your name to begin work: ")
 def greeting(name):
     print("Hello, {}. Thank you for using MasterTicket".format(get_name))
 
-while tick_remain > 0:
+def calculate_price(num_tickets):
+    return num_tickets * TICKET_PRICE    
+    
 
-    greeting(get_name)
+greeting(get_name)
+
+while tick_remain > 0:
 
     #print("There are {} Tickets left.".format(tick_remain))
 
     def calc_price():
         #Request a certain amount of tickets and be told the total cost 
         print("The Price for 1 ticket is {}".format(TICKET_PRICE))
-        get_tickets = int(input("How many tickets have been requested? "))
 
-        # calculate the total price
-        calc_tickets = get_tickets * TICKET_PRICE
-        print("The total price is {}".format(calc_tickets))
+        try:
+            get_tickets = int(input("How many tickets have been requested? "))
+            
 
-        proceed = input("Do you want to proceed with this? ").lower()
+            if tick_remain < get_tickets:
+                raise ValueError("There are only {} ticket(s) remaining".format(tick_remain))
 
-        if proceed == "y":
-            print("SOLD\n\n")
-            pass_tickets = tick_remain - get_tickets 
-            print("There are now {} ticket(s) remaining\n".format(pass_tickets))
+
+        except ValueError as err:
+            print("That's not right. I'm expecting a number. {}".format(err)) 
         else:
-            print("Thanks, {} for using MasterTicket\n".format(get_name))
+            # calculate the total price
+            calc_tickets = calculate_price(get_tickets)
+            
+            print("The total price is {}".format(calc_tickets))
+
+            proceed = input("Do you want to proceed with this? ").lower()
+
+            if proceed == "y":
+                print("SOLD\n\n")
+                pass_tickets = tick_remain - get_tickets 
+                print("There are now {} ticket(s) remaining\n".format(pass_tickets))
+            else:
+                print("Thanks, {} for using MasterTicket\n".format(get_name))
 
     calc_price()
      
-# Notify tht tickets are sold out
+# Notify that tickets are sold out
 
 
 #Errors reported in a user friendly manner

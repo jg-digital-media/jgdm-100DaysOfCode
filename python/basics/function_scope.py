93753e3852da346cfc087473b49ab9f9310609e2
jg-digital-media <mail@jonniegrieve.co.uk>
2019-09-24 16:16:54 +0100

    R3D35: function returns

--- python/basics/function_scope.py
+++ python/basics/function_scope.py
@@ -4,5 +4,12 @@ def set_num():
     num = 5
     print(num)
 
-print(num)
-set_num()
+#print(num)
+#set_num()
+
+def two_plus_two():
+    val = 2+2
+    return val
+
+sum = two_plus_two()
+print(two_plus_two() * 2)

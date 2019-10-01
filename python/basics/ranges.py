6fa31e311bd311f48fb07fd79f0d082d37ceea84
Jonathan Grieve <mail@jonniegrieve.co.uk>
2019-10-01 16:33:54 +0100

    R3D40: update log

---
+++ python/basics/ranges.py
@@ -0,0 +1,36 @@
+"""
+Ranges:
+
+0 10 1
+1 11 1
+10
+5 10 2
+5 10  - 
+"""
+
+
+for i in range(10, 6, -1):
+    print(i)
+    #returns 5, 7, 9
+
+""" 
+for i in range(0, 10, 1):
+    print(i)
+    #returns 1 to 9
+
+for i in range(1, 11, 1):
+    print(i)
+    #returns 1 to 10
+
+for i  in range(10):
+    print(i)
+    #returns 0 to 9 on a
+
+for i in range(5, 10):
+    print(i)
+    #returns 5 to 9
+
+for i in range(5, 10, 2):
+    print(i)
+    #returns 5, 7, 9
+ """

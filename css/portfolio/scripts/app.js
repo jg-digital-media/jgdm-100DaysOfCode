4726cac05117340ad71c7b8b8b1c9bcd4588d521
Jonathan Grieve <mail@jonniegrieve.co.uk>
2019-10-07 16:07:55 +0100

    apply client side validation

--- css/portfolio/scripts/app.js
+++ css/portfolio/scripts/app.js
@@ -15,8 +15,10 @@ const testimonials = [
 
 ]
 
-//sticky header
+/*jQuery Code*/
 $(document).ready(function() {
+
+    //Apply Sticky header element
     $(window).scroll(function(){
         let sticky = $('.main-nav'),
         scroll = $(window).scrollTop();
@@ -27,12 +29,60 @@ $(document).ready(function() {
             sticky.removeClass('sticky');
         }
     });
-});
 
-//lite browser window for pages. 
-function MM_openBrWindow(theURL,winName,features) { //v2.0
-    window.open(theURL,winName,features);
-  }
+
+    /*jQuery Form Validation.*/
+    
+	// Place ID's of all required fields here.
+	required = ["name", "email", "your-url", "subject"];
+	// If using an ID other than #email or #error then replace it here
+	email = $("#email");
+	errornotice = $("#error");
+	// The text to show up within a field when it is incorrect
+	emptyerror = "Please fill out this field.";
+	emailerror = "Please enter a valid e-mail.";
+
+	$("#hire-form").submit(function(){	
+		//Validate required fields
+		for (i=0;i<required.length;i++) {
+			var input = $('#'+required[i]);
+			if ((input.val() == "") || (input.val() == emptyerror)) {
+				input.addClass("js-needsfilled");
+				input.val(emptyerror);
+				errornotice.fadeIn(750);
+			} else {
+				input.removeClass("js-needsfilled");
+			}
+		}
+		// Validate the e-mail.
+		if (!/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(email.val())) {
+			email.addClass("js-needsfilled");
+			email.val(emailerror);
+		}
+
+		//if any inputs on the page have the class 'needsfilled' the form will not submit
+		if ($(":input").hasClass("js-needsfilled")) {
+			return false;
+		} else {
+			errornotice.hide();
+			return true;
+		}
+	});	
+	
+	// Clears any fields in the form when the user clicks on them
+	$(":input").focus(function()  {		
+	   if ($(this).hasClass("js-needsfilled") ) {
+			$(this).val("");
+			$(this).removeClass("js-needsfilled");
+			
+		   $("p").click(function() {
+				$("#error").remove();
+			});
+	   }
+	});
+	
+	/**/
+});
 
 //project status code
 

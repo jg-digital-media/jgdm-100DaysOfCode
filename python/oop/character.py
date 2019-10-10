a0f5759ccece5228cd385a53f4bc548d493d2249
Jonathan Grieve <mail@jonniegrieve.co.uk>
2019-10-10 17:05:24 +0100

    add character.py  oop basics

---
+++ python/oop/character.py
@@ -0,0 +1,26 @@
+## OOP Classes in Python - using classes, attributes and methods.
+
+
+import random
+
+class Thief:
+
+    # attributes of class
+    sneaky = True
+
+    # init controls how the classes are created or initialized,  Sneaky set true by default
+    def __init__ (self, name, sneaky=True, **kwargs):
+        self.name = name
+        self.sneaky = sneaky
+
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def pickpocket(self):
+        return self.sneaky and bool(random.randint(0,1))
+
+    def hide(self, light_level):
+        return self.sneaky and light_level < 10
+
+jonnie = Thief("Jonnie", bravery = 10, favourite_weapon = 'Wit', scars=None)
+print(jonnie.name)

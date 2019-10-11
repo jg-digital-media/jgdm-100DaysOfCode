60c5bc0f827039b4eaf94f138e3932e23b8875bd
Jonathan Grieve <mail@jonniegrieve.co.uk>
2019-10-11 12:20:22 +0100

    edit padding on intro section

--- css/portfolio/assets/main-form.php
+++ css/portfolio/assets/main-form.php
@@ -110,12 +110,12 @@
                             <label for="confirm" tabindex="0">The boring consent bit. :) <span role="required-field">*</span> </label>
                         </div>
 
-                        <div class="col-two">Please let me know you have read and agree with my <a href="javascript:" class="text2" onMouseDown="MM_openBrWindow('./pages/gdpr.html','','scrollbars=yes,width=550,height=250')" title="Click this link to go to our privacy policy" alt="Click this link to go to our privacy policy">privacy policy</a>.
+                        <div class="col-two">Please let me know you have read and agree with my <a href="#" onClick="openWindow('./pages/gdpr.html', '550', '250')" title="Click this link to go to our privacy policy" alt="Click this link to go to our privacy policy">privacy policy</a>.
                             <input type="checkbox" id="confirm" name="confirm" value="Confirmed Consent" 
                             oninvalid="this.setCustomValidity('Have you read my privacy policy? It won\'t take long :)')"
                             placeholder="Click Consent" tabindex="0" />
 
-                            <?php if ( empty($confirm) ) { echo '<span class="field-error">' . $err_privacy . '</span>'; }  ?>
+                            <?php if ( empty($confirm) ) { echo '<p class="field-error">' . $err_privacy . '</p>'; }  ?>
 
                         </div>
 

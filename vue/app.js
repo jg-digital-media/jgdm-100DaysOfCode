2811cd0e5e583e22a76b907b692e4ebabc789f0f
jg-digital-media <mail@jonniegrieve.co.uk>
2019-10-21 17:16:16 +0100

    vue project

---
+++ vue/app.js
@@ -0,0 +1,11 @@
+// https://adrianmejia.com/vue-js-tutorial-for-beginners-create-a-todo-app/
+
+//Used from this repo. - https://github.com/amejiarosario/vue-todo-app.git   start-here branch
+
+const app = new Vue({
+    el: '.todoapp',
+    data: {
+        title: "Title",
+    }
+    
+  });

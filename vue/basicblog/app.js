79ae78189b7ff40cfd99ff834ae47b8fb54323ea
jg-digital-media <mail@jonniegrieve.co.uk>
2019-10-23 13:13:19 +0100

    display static post title data

--- vue/basicblog/app.js
+++ vue/basicblog/app.js
@@ -1,2 +1,41 @@
 console.log("https://sabe.io/tutorials/getting-started-with-vue-js#the-takeaway")
 //https://sabe.io/tutorials/getting-started-with-vue-js#the-takeaway
+
+Vue.component('static-posts', {
+   template: '#static-posts-template',
+   name: 'static-posts',
+   
+   data: () =>({
+        posts: []
+
+   }),
+
+
+   mounted() {
+       this.getPosts();
+   },
+
+   methods: {
+        getPosts() {
+           
+            this.posts = [
+                {
+                    "title": "The first post title!"
+                },
+                {
+                    "title": "The second post title!"
+                },
+                {
+                    "title": "The third post title!"
+                }
+            ];
+        }
+   }
+   
+})
+
+//Vue Component  - must be placed after any components
+
+app = new Vue({
+    el: '#app',
+});

1687f663822f5da7e719031fec861bb1895fad4e
Jonathan Grieve <mail@jonniegrieve.co.uk>
2019-11-01 17:24:25 +0000

    update scoresheet code for yatsy

--- python/oop/yatsy/hand.py
+++ python/oop/yatsy/hand.py
@@ -12,7 +12,40 @@ class Hand(list):
         #sort the yatsyhand
         self.sort()
 
+    
+    #utility for scoring Yatsy hands. 
+    def _by_value(self, value):
+        dice=[]
+        for die in self:
+            if die == value:
+                dice.append(die)
+        return dice
+       
 class YatsyHand(Hand):
     def __init__(self, *args, **kwargs):
         super().__init__(size=5, die_class=D6, *args, **kwargs)
+
+    @property
+    def ones(self):
+        return self._by_value(1)
+   
+    @property
+    def twos(self):
+        return self._by_value(2)
+   
+    @property
+    def threes(self):
+        return self._by_value(3)
+   
+    @property
+    def four(self):
+        return self._by_value(4)
+   
+    @property
+    def five(self):
+        return self._by_value(5)
+   
+    @property
+    def sixes(self):
+        return self._by_value(6)
 

7e99f498026a905093efd663438a6ba8c4493a6b
Jonathan Grieve <mail@jonniegrieve.co.uk>
2019-11-07 11:44:27 +0000

    finish timed quiz

--- python/oop/timed-quiz-app/quiz.py
+++ python/oop/timed-quiz-app/quiz.py
@@ -7,8 +7,10 @@ https://teamtreehouse.com/library/dates-and-times-in-python/lets-build-a-timed-q
 import datetime
 import random
 
+# import ability to add questions to app
 from questions import Add, Multiply
 
+# main quiz class - operates running of the quiz
 class Quiz:
     questions = []
     answers = []
@@ -27,34 +29,53 @@ class Quiz:
             # add these questions into self.questions
             self.questions.append(question)
 
+    """ log questions as correct or incorrectly answered """
     def take_quiz(self):
         # log the start time
+        self.start_time = datetime.datetime.now()
+
         # ask all of the questions
-        # log if they got the question right
-        # log the end time
+        for question in self.questions:
+
+            # log if they got the question right
+            self.answers.append(self.ask(question))
+        else:
+            # log the end time
+            self.end_time = datetime.datetime.now()
         # show a summary
-        pass
+        return self.summary()
 
 
     """ Every time answer that is correct add 1 to self.answer """
     def total_correct(self):
         # return the total number of correct answers
-
+        total = 0
         for answer in self.answers:
             if answer[0] == True:
                 total += 1
-            return total
+        return total
 
-
-    def ask(self, questions):
+    """ ask questions 1 by 1 """
+    def ask(self, question):
+        correct = False
         # log the start time
+        question_start = datetime.datetime.now()
+
         # capture the answer
+        answer = input(question.text + ' = ')
+
         # check the answer
-        # log the end time
-        # if the answer is right, send back true
+        # # if the answer is right, send back true
         # otherwise send back false
+        if answer == str(question.answer):
+            correct = True
+
+        # log the end time
+        question_end = datetime.datetime.now()
+
+        
         # send back the elapsed time
-        pass
+        return correct, question_end - question_start
     
     """ prints the total number of correct questions """
     def summary(self):
@@ -62,3 +83,6 @@ class Quiz:
         print("you got {} out of {} right.".format(self.total_correct(), len(self.questions)))
         # print the total elapsed time for the quiz
         print("it took you {} seconds in total".format((self.end_time-self.start_time).seconds))
+
+
+Quiz().take_quiz()

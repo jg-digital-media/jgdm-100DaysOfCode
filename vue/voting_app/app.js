37ee60e36e89529269dfb100c3f8d76307263361
Jonathan Grieve <mail@jonniegrieve.co.uk>
2019-11-18 17:16:41 +0000

    increment votes per item, add new items

--- vue/voting_app/app.js
+++ vue/voting_app/app.js
@@ -1,6 +1,27 @@
 let app = new Vue({
     el: '#app', 
     data: {
-        message: 'Vue is Working!'
+        frameworks: [
+            {name: "React", votes: 0},
+            {name: "VueJS", votes: 0},
+            {name: "Angular", votes: 0}
+        ]
+        
+    },
+
+    methods: {
+        voteFor: function(f){
+            f.votes += 1;
+        },
+        
+        addNew: function(event) {
+            this.frameworks.push({
+                name: event.target.value,
+                votes: 0,
+            })
+
+            event.target.value = '';
+        }
     }
+    
 })

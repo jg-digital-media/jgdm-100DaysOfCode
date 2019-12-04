a9f57ad365b3c6ea26bcb376338b36acec311fb2
Jonathan Grieve <mail@jonniegrieve.co.uk>
2019-12-04 17:43:26 +0000

    get response data - parse as JSON

--- nodeJS/basics/app.js
+++ nodeJS/basics/app.js
@@ -1,13 +1,9 @@
-//problem  - provide a simple way to look at a Treehouse users badge count and JS points
+//Problem: Provide a simple way to look at a Treehouse users badge count and JS points
 //Solution:  Use node.js to connect to the Treehouse API to get profile information to print out
-
 //Connect to the API URL (https://teamtreehouse.com/username.json)
 
 //Require https module from Node.js
 const https = require('https');
-
-//store student name from API URL
-username = "chalkers"
 
 //simple function with arguments to print a message with dummy API data.
 function printMessage(username, badgeCount, points) {
@@ -17,17 +13,38 @@ function printMessage(username, badgeCount, points) {
 
 printMessage("chalkers", 100, 20000);
 
-const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
 
-
-
+function getProfile(username) {
     //prints all the data available from the api
-      //console.dir(response);
-    //LOG THE Status Code
-    console.log(response.statusCode);
+    const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+        
+        //LOG THE Status Code
+        console.log(response.statusCode);
+        //console.dir(response);
 
-    //read the data
-    //parse the data
-    //print the data
-    
-});
+        let body = "";
+
+        //Get the response body from the api
+        response.on("data", data => {
+            body += data.toString();
+            //console.log(body)
+        })
+
+        //Retrieve the JSON data for a given username
+        response.on('end', () => {
+            //parse the data
+            const profile = JSON.parse(body);
+            
+            //print the data
+            printMessage(username, profile.badges.length, profile.points.JavaScript);
+        })
+
+        
+        
+    })
+}
+
+const users = ["chalkers", "alenaholligan", "jonathangrieve"]  //store usernames in array
+//const users = process.argv.slice(2)  //get usernames from the console
+
+users.forEach( getProfile )

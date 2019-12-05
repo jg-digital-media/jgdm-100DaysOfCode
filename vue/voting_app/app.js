a262d76847945fe3a395a32ae96a19514062c47a
Jonathan Grieve <mail@jonniegrieve.co.uk>
2019-12-05 11:29:05 +0000

    introduce computed properties for identifying leaders

--- vue/voting_app/app.js
+++ vue/voting_app/app.js
@@ -67,6 +67,17 @@ let app = new Vue({
         }
     },
 
+    computed: {
+        //code to display winning names
+        winnerString: function() {
+            let scores = this.frameworks.map(f => f.votes)
+            let highscore = Math.max.apply(Math, scores)
+            let bestList = this.frameworks.filter(f => f.votes == highscore)
+            let bestNames = bestList.map(f => f.name)
+            return bestNames.join(', ')
+        }
+    },
+
     //call load function
     created: function() {
         this.load();

157134a13d237719f2fe6ad2e3e9a6951e65a332
Jonathan Grieve <mail@jonniegrieve.co.uk>
2019-12-18 16:02:14 +0000

    remove value binding function

--- nodeJS/dynamic_site/render.js
+++ nodeJS/dynamic_site/render.js
@@ -1,12 +1,12 @@
 var fs = require("fs");
 
-function mergeValues(values, content) {
+/* function mergeValues(values, content) {
     for(key in values) {
         content = content.replace ("{{" + key + "}}", values[key]);
     }
 
     return content;
-}
+} */
 
 function view(templateName, values, response) {
     var fileContents = fs.readFileSync('./views/' + templateName + '.html');

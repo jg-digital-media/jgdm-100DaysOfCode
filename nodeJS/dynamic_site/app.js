fbc5f8ced0126fedf9e12a5beaa09c963fb4a278
Jonathan Grieve <mail@jonniegrieve.co.uk>
2020-01-13 16:18:20 +0000

    create simple node server

---
+++ nodeJS/dynamic_site/app.js
@@ -0,0 +1,18 @@
+
+//Create a webserver
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((request, response) => {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  response.write("This is before the end");
+  response.end('Hello World');
+  response.write("This is after the end");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});

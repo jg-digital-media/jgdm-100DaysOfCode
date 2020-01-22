4bbef7f42e1118bc3ef686578ae0bbbc927266f4
Jonathan Grieve <mail@jonniegrieve.co.uk>
2020-01-22 12:52:27 +0000

    basic express app

--- nodeJS/express_site/app.js
+++ nodeJS/express_site/app.js
@@ -1,2 +1,14 @@
-const express = "require";
+const express = require('express');
 
+//express function
+const app = express();
+
+//set up the development server listen method - port number
+app.listen(3000);
+
+///served a hello world application
+app.get('/', (request, response) => {
+    
+   //basic response with the send method
+    response.send('I love Treehouse');
+});

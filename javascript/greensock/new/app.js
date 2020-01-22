3448d15f23f1e7d05e946220b3e3d4da2d57ec38
Jonathan Grieve <mail@jonniegrieve.co.uk>
2020-01-22 14:48:14 +0000

    gunbarrel image

--- javascript/greensock/new/app.js
+++ javascript/greensock/new/app.js
@@ -9,14 +9,15 @@ circle6 =  document.getElementsByClassName("circle6");
 circle7 =  document.getElementsByClassName("circle7");
 circle8 =  document.getElementsByClassName("circle8");
 
-bigCrcl = document.getElementsByClassName("");
+gunbarrel = document.getElementsByClassName("gunbarrel");
 
 //animating one timeline
-var bond = gsap.timeline({delay: 2})
-
+let scene1 = gsap.timeline({delay: 2})
+let scene2 = gsap.timeline({delay: 2.5})
+let scene3 = gsap.timeline({delay: 6.5})
 
 //chain gunbarrel circles
-bond.to(box1, {
+scene1.to(box1, {
     duration: 3.6,
     x: 500,
     ease: "none",
@@ -33,60 +34,86 @@ bond.to(box1, {
     delay: 1,
 })*/
 
-let cl = gsap.timeline({delay: 2.5})
 
-cl.to(circle1, {
+
+scene2.to(circle1, {
     opacity: 1,
     duration: 0.1,
     visibility: "visible",
     delay: 0.1,
 }), "-=1"
 
-cl.to(circle2, {
+scene2.to(circle2, {
     opacity: 1,
     duration: 0.1,
     visibility: "visible",
     delay: 0.3,
 }), "-=2"
 
-cl.to(circle3, {
+scene2.to(circle3, {
     opacity: 1,
     duration: 0.1,
     visibility: "visible",
     delay: 0.3,
 }), "-=2"
 
-cl.to(circle4, {
+scene2.to(circle4, {
     opacity: 1,
     duration: 0.1,
     visibility: "visible",
     delay: 0.3,
 }), "-=2"
 
-cl.to(circle5, {
+scene2.to(circle5, {
     opacity: 1,
     duration: 0.1,
     visibility: "visible",
     delay: 0.3,
 }), "-=2"
 
-cl.to(circle6, {
+scene2.to(circle6, {
     opacity: 1,
     duration: 0.1,
     visibility: "visible",
     delay: 0.3,
 }), "-=2"
 
-cl.to(circle7, {
+scene2.to(circle7, {
     opacity: 1,
     duration: 0.1,
     visibility: "visible",
     delay: 0.3,
 }), "-=2"
 
-cl.to(circle8, {
+scene2.to(circle8, {
     opacity: 1,
     duration: 0.1,
     visibility: "visible",
     delay: 0.3,
 }), "-=2"
+
+
+scene2.to(circle8, {
+    opacity: 1,
+    duration: 0.1,
+    visibility: "visible",
+    delay: 0.3,
+}), "-=2"
+
+scene2.to([circle1, circle2, circle3, circle4, circle5, circle6, circle7, circle8], {
+    opacity: 0,
+    duration: 0,
+    visibility: "hidden",
+    delay: 0.5,
+}), "-=2"
+
+
+
+scene3.to(gunbarrel, {
+    opacity: 1,
+    duration: 2.5,
+    visibility: "visible",
+    left: 0,
+    delay: 0.0,
+    ease: "none"
+}), "-=0"

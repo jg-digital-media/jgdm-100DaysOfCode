0cc35b5ee07df2518aab6d6c870bff2300f3e314
jg-digital-media <mail@jonniegrieve.co.uk>
2020-02-10 15:55:14 +0000

    source file for names list

---
+++ nodeJS/express_site/data.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const routes = express.Router();
+
+//list data
+const names = [
+    "Jack Bauer",
+    "Jill Green",
+    "John Joe",
+    "Jonah Whale",
+    "Jamaal LH",
+    "Joe Fisher"
+]
+
+module.exports = routes;
+module.exports = names;

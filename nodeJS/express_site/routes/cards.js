48183741333750ff08fc5b7e7e017df88dbc0ad2
jg-digital-media <mail@jonniegrieve.co.uk>
2020-02-11 16:10:59 +0000

    card template - query strings

--- nodeJS/express_site/routes/cards.js
+++ nodeJS/express_site/routes/cards.js
@@ -2,16 +2,18 @@ const express = require("express");
 const routes = express.Router();
 
 const { data } = require("../data/flashcardData.json");
-//const { cards } = data;
+const { cards } = data;
 
  //serve the cards route
- routes.get('/', (req, res) => {
-     
-    //basic response with the send method
-    res.render('cards', { prompt: "who is buried in Grant's Tomb?"});
-     
-    //res.end();
- });
+ routes.get('/:id', (req, res) => {
+   const { side } = req.query;
+   const { id } = req.params;
+   const text = cards[id][side];
+   const { hint } = cards[id];
+
+   const templateData = { text, hint };
+   res.render('card', templateData);
+});
 
 
  //export cards routes to app.js

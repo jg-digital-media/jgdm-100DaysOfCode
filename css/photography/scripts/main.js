52ea21fedf47ba2c5568cf7af700aa6af77160fc
jg-digital-media <mail@jonniegrieve.co.uk>
2020-02-13 11:44:10 +0000

    implement slick slider

--- css/photography/scripts/main.js
+++ css/photography/scripts/main.js
@@ -1,1 +1,13 @@
 console.log("connected!");
+
+
+$(document).ready(function(){
+    //animal slider
+    $(".animal-slider").slick({
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            dots: true              
+    });
+
+});

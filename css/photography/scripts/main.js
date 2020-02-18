7906db9f3bbb931661c2cd0efe2ddf2778e3dd42
Jonathan Grieve <mail@jonniegrieve.co.uk>
2020-02-18 14:43:50 +0000

    implement responsive menu

--- css/photography/scripts/main.js
+++ css/photography/scripts/main.js
@@ -25,3 +25,17 @@ $(document).ready(function(){
     
 
 });
+
+
+//nav menu
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    //document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    //document.getElementById("main").style.marginLeft= "0";
+  }

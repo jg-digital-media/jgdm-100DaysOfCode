3b41056ab0c19c1a16ba34cd478b6d24e177947d
Jonathan Grieve <mail@jonniegrieve.co.uk>
2020-03-27 12:37:41 +0000

    toggle add currency list state

--- javascript/currency_converter/app.js
+++ javascript/currency_converter/app.js
@@ -1,1 +1,15 @@
 console.log("file connected");
+
+
+//global variables
+
+const addCurrencyBtn = document.querySelector(".add-currency-btn");
+// event listeners
+
+addCurrencyBtn.addEventListener("click", addCurrencyBtnClick);
+
+//functions
+function addCurrencyBtnClick(event) {
+    addCurrencyBtn.classList.toggle("open");
+}
+

c2b3786375000762d1ad84f53eb90c7aea82ca5b
Jonathan Grieve <mail@jonniegrieve.co.uk>
2020-03-27 13:37:26 +0000

    hook up addcurrency data

--- javascript/currency_converter/app.js
+++ javascript/currency_converter/app.js
@@ -1,15 +1,55 @@
 console.log("file connected");
 
 
-//global variables
-
+// global variables
+const addCurrencyList = document.querySelector(".add-currency-list");
 const addCurrencyBtn = document.querySelector(".add-currency-btn");
+
 // event listeners
+addEventListener("click", addCurrencyBtnClick);
 
-addCurrencyBtn.addEventListener("click", addCurrencyBtnClick);
 
-//functions
+// functions
 function addCurrencyBtnClick(event) {
     addCurrencyBtn.classList.toggle("open");
 }
 
+let currencies = [
+
+    {
+        name: "US Dollar",
+        abbreviation: "USD",
+        symbol: "\u0024",
+        flagURL: "http://www.geonames.org/flags/x/us.gif"
+      },
+      {
+        name: "British Pound",
+        abbreviation: "GBP",
+        symbol: "\u00A3",
+        flagURL: "http://www.geonames.org/flags/x/uk.gif"
+      },
+      {
+        name: "Japanese Yen",
+        abbreviation: "JPY",
+        symbol: "\u00A5",
+        flagURL: "http://www.geonames.org/flags/x/jp.gif"
+      },
+      /*paste new currencies here*/ 
+]
+
+
+// populateAddCurrencyList
+
+function populateAddCurrencyList() {
+    for(let i=0; i<currencies.length; i++) {
+        addCurrencyList.insertAdjacentHTML(
+            "beforeend",
+            `<li data-currency="${currencies[i].abbreviation}" class="">
+                        <img src="${currencies[i].flagURL}"  class="flag"><span>${currencies[i].abbreviation} - ${currencies[i].name}</span>
+            </li>` 
+        );
+    }
+	
+}
+
+populateAddCurrencyList();

5e8fd265b0fe82192c0cf04e6d1d0f31893c58bf
Jonathan Grieve <mail@jonniegrieve.co.uk>
2020-04-24 15:45:55 +0100

    create a simple REST API

--- nodeJS/express_restapi/app.js
+++ nodeJS/express_restapi/app.js
@@ -4,7 +4,7 @@ const app = express();
 app.listen(3000, () => console.log('Quote API listening on port 3000!'));
 
 //create an exoress get Route handler
-app.get('/greetings', (res, req)=> {
+app.get('/greetings', (req, res)=> {
 
     res.json({greeting: "Hello world"});
 

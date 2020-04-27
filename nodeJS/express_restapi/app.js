89d34c604a4968ec1e9ecf0d65f6bd05157c378c
Jonathan Grieve <mail@jonniegrieve.co.uk>
2020-04-27 15:53:55 +0100

    get one quote, get all quotes

--- nodeJS/express_restapi/app.js
+++ nodeJS/express_restapi/app.js
@@ -3,12 +3,45 @@ const app = express();
 
 app.listen(3000, () => console.log('Quote API listening on port 3000!'));
 
-//create an exoress get Route handler
-app.get('/greetings', (req, res)=> {
+//Create an Exoress get Route Handler
+app.get('/quotes', (req, res)=> {
 
-    res.json({greeting: "Hello world"});
+    res.json({data});
 
 
 } );
 
+app.get('/quotes/:id', (req, res) => {
 
+    //res.json({data});
+
+    //compare range of id's to that in the array 
+    const quote = data.quotes.find(quote => quote.id == req.params.id);
+
+    //send quote back to client as json
+    res.json(quote);
+
+    //404 page for incorrect quite id's.
+
+
+} );
+
+const data = {
+    quotes: [
+      {
+        id: 8721,
+        quote: "We must accept finite disappointment, but we must never lose infinite hope.",
+        author: "Martin Luther King"
+      },
+      {
+        id: 5779,
+        quote: "Use what you’ve been through as fuel, believe in yourself and be unstoppable!",
+        author: "Yvonne Pierre"
+      },
+      {
+        id: 3406,
+        quote: "To succeed, you have to do something and be very bad at it for a while. You have to look bad before you can look really good.",
+        author: "Barbara DeAngelis"
+      }
+    ]
+  }

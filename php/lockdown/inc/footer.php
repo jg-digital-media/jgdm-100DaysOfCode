08be37d2a0e6bc4c924dc049619750cb54474640
Jonathan Grieve <mail@jonniegrieve.co.uk>
2020-05-13 13:23:55 +0100

    setup php navigation

--- php/lockdown/inc/footer.php
+++ php/lockdown/inc/footer.php
@@ -1,1 +1,10 @@
-<?php ?>
+    
+    <footer>
+
+        <p>&copy; Jonnie Grieve Digital Media 2020</p>
+    </footer>
+    
+    <script src="js/app.js"></script>
+</body>
+</html>
+
--- php/lockdown/inc/header.php
+++ php/lockdown/inc/header.php
@@ -1,1 +1,40 @@
-<?php  ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Page Meta Tags -->
+    <meta name="description" content="<?php echo $meta_description;  ?>">
+    <meta name="keywords" content="<?php echo $meta_keywords;  ?>"> 
+    <meta name="image" content="<?php echo $meta_image;  ?>">
+
+    <!-- Facebook OpenGraph GL -->
+    <meta property="og:title" content="<?php echo $fb_title; ?>">
+    <meta property="og:description" content="<?php echo $fb_description; ?>">
+    <meta property="og:image" content="<?php echo $fb_image; ?>">
+    <meta property="og:url" content="<?php echo $fb_url; ?>">
+
+    <!-- Styles -->
+    <link rel="stylesheet" type="text/css" href="style.css" />
+
+    <!-- Canonical link -->
+    <link rel="canonical" href="<?php echo $canonical;  ?>">
+    <title><?php echo $title; ?></title>
+</head>
+<body>
+
+<header>
+    <h1> <?php echo $title; ?> </h1>
+</header>
+
+    <nav>
+        <ul>
+            <li><a href="index.php" title="Home" <?php if($selected == "home") { echo 'class="selected"'; } ?>>Lockdown</a></li>
+            <li><a href="dene.php" title="Castle Eden Dene" <?php if($selected == "dene") { echo 'class="selected"'; }?>>Castle Eden Dene</a></li>
+            <li><a href="railwayline.php" title="Railway Line" <?php if($selected == "railwayline") { echo 'class="selected"'; } ?>>Railway Line</a></li>
+            <li><a href="coastal.php" title="Coastal" <?php if($selected == "coastal") { echo 'class="selected"'; }?>>Coastal</a></li>
+            <li><a href="routes.php" title="Routes" <?php if($selected == "routes") { echo 'class="selected"'; }?>>Routes</a></li>
+        </ul>
+
+    </nav>

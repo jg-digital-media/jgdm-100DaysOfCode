98c23d9a0899621d5813166afb9d6bf5e7ed3ed6
Jonathan Grieve <mail@jonniegrieve.co.uk>
2020-05-14 15:24:18 +0100

    dummy content route slider

--- php/lockdown/index.php
+++ php/lockdown/index.php
@@ -21,9 +21,9 @@
 
 <?php require_once "inc/header.php"; ?>
 
-<section>
+<section class="lockdown">
 
-    <article class="lockdown">
+    <article class="lockdown-intro">
 
         <p>In March 2020 life changed for me and everyone else on the this planet. Due to the Covid-19 pandemic we all had to make changes to our daily lives and our routines. This left me feeling strange and disoriented at what was happening and the loss of some of the freedoms that we enjoy. Loss of freedom with good reasons might I had.  I did discover however that gettng outside was the best way to make myself feel better about life. So I got out and I got walking. And I feel so lucky that I've had so many safe and green spaces to go while the UK was in lockdown. So I created this website as something nice to rememvwer this strange period by.</p>
 
@@ -40,36 +40,38 @@
     <article class="route-list">
 
         <ul>
-            Dene
+            <h3>Dene</h3>
             <li><a href="" class="route-list-item" title="">One</a></li>
             <li><a href="" class="route-list-item" title="">Two</a></li>
             <li><a href="" class="route-list-item" title="">Three</a></li>
 
-            Railway Line
+            <h3>Railway Line</h3>
             <li><a href="" class="route-list-item" title="">One</a></li>
             <li><a href="" class="route-list-item" title="">Two</a></li>
             <li><a href="" class="route-list-item" title="">Three</a></li>
 
-            Coastal
+            <h3>Coastal</h3>
             <li><a href="" class="route-list-item" title="">One</a></li>
             <li><a href="" class="route-list-item" title="">Two</a></li>
             <li><a href="" class="route-list-item" title="">Three</a></li>
         </ul>
 
     </article>
-
-    <article class="route-slider">
-
-        <img src="img/home.jpg" alt="" title="" />
-        <img src="img/home.jpg" alt="" title="" />
-        <img src="img/home.jpg" alt="" title="" />
-        <img src="img/home.jpg" alt="" title="" />
-        <img src="img/home.jpg" alt="" title="" />
-        <img src="img/home.jpg" alt="" title="" />
-        <img src="img/home.jpg" alt="" title="" />
-
-    </article>    
+ 
 
 </section>
+
+
+<article class="route-slider">
+
+    <a href="#" title="" class=""><img src="img/home.jpg" alt="" /></a>
+    <a href="#" title="" class=""><img src="img/home.jpg" alt="" /></a>
+    <a href="#" title="" class=""><img src="img/home.jpg" alt="" /></a>
+    <a href="#" title="" class=""><img src="img/home.jpg" alt="" /></a>
+    <a href="#" title="" class=""><img src="img/home.jpg" alt="" /></a>
+    <a href="#" title="" class=""><img src="img/home.jpg" alt="" /></a>
+    <a href="#" title="" class=""><img src="img/home.jpg" alt="" /></a>
+
+</article>   
 
 <?php require_once "inc/footer.php"; ?>

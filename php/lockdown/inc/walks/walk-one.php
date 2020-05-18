74fc3f201b12c3137cfa9806e13121d8df53dfcf
Jonathan Grieve <mail@jonniegrieve.co.uk>
2020-05-18 13:40:39 +0100

    test lightbox for walk one

--- php/lockdown/inc/walks/walk-one.php
+++ php/lockdown/inc/walks/walk-one.php
@@ -33,11 +33,25 @@
 
                     <div class="lightboxslider">
 
-                    <img src="http://jg-photography.s3.eu-west-2.amazonaws.com/lockdown_walks/walk-dene-one/IMG_20200507_135445.jpg" data-lightbox="walk-one" data-title="This is a caption" class="trail-image" />
-                    <!--<img src="http://jg-photography.s3.eu-west-2.amazonaws.com/lockdown_walks/walk-dene-one/IMG_20200509_132614.jpg" data-lightbox="walk-one" data-title="This is a caption" class="trail-image" />
-                    <img src="http://jg-photography.s3.eu-west-2.amazonaws.com/lockdown_walks/walk-dene-one/IMG_20200509_132614.jpg" data-lightbox="walk-one" data-title="This is a caption" class="trail-image" />
-                    <img src="http://jg-photography.s3.eu-west-2.amazonaws.com/lockdown_walks/walk-dene-one/IMG_20200509_132614.jpg" data-lightbox="walk-one" data-title="This is a caption" class="trail-image" />
-                    <img src="http://jg-photography.s3.eu-west-2.amazonaws.com/lockdown_walks/walk-dene-one/IMG_20200509_132614.jpg" data-lightbox="walk-one" data-title="This is a caption" class="trail-image" /> -->
+                        <a href="img/walks/walk-dene-one/IMG_20200507_135445.jpg" href="img/walks/walk-dene-one/IMG_20200507_135535.jpg" data-lightbox="walk-one" data-title="This is a caption" class="lightbox-link"> 
+                            <img src="img/walks/walk-dene-one/IMG_20200507_135445.jpg" class="trail-image" />
+                        
+                        </a>
+
+                        <a href="img/walks/walk-dene-one/IMG_20200507_135535.jpg" data-lightbox="walk-one" data-title="This is a caption 2" class="lightbox-link"> 
+                            <!-- <img src="img/walks/walk-dene-one/IMG_20200507_135445.jpg" class="trail-image" /> --> 
+                        </a>
+
+                        <a href="img/walks/walk-dene-one/IMG_20200507_143151.jpg" data-lightbox="walk-one" data-title="This is a caption 3" class="lightbox-link"> 
+                            <!-- <img src="img/walks/walk-dene-one/IMG_20200507_135445.jpg" class="trail-image" /> --> 
+                        </a>
+
+
+                        <!--<a href="" data-lightbox="walk-one" data-title="This is a caption 2"> <img src="http://jg-photography.s3.eu-west-2.amazonaws.com/lockdown_walks/walk-dene-one/IMG_20200507_135445.jpg" data-lightbox="walk-one" data-title="This is a caption" class="trail-image" /></a>
+                        <a href=""><img src="http://jg-photography.s3.eu-west-2.amazonaws.com/lockdown_walks/walk-dene-one/IMG_20200509_132614.jpg" data-lightbox="walk-one" data-title="This is a caption" class="trail-image" />
+                        <a href=""><img src="http://jg-photography.s3.eu-west-2.amazonaws.com/lockdown_walks/walk-dene-one/IMG_20200509_132614.jpg" data-lightbox="walk-one" data-title="This is a caption" class="trail-image" />
+                        <a href=""><img src="http://jg-photography.s3.eu-west-2.amazonaws.com/lockdown_walks/walk-dene-one/IMG_20200509_132614.jpg" data-lightbox="walk-one" data-title="This is a caption" class="trail-image" />
+                        <a href=""><img src="http://jg-photography.s3.eu-west-2.amazonaws.com/lockdown_walks/walk-dene-one/IMG_20200509_132614.jpg" data-lightbox="walk-one" data-title="This is a caption" class="trail-image" /> -->
 
                     </div>
                         

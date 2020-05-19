94d35b8fffb92de6ba310f00ca09d48618f67940
Jonathan Grieve <mail@jonniegrieve.co.uk>
2020-05-19 13:26:55 +0100

    update first trail URLS

--- php/lockdown/inc/walks/walk-three.php
+++ php/lockdown/inc/walks/walk-three.php
@@ -33,100 +33,36 @@
                     <div class="lightbox-slider">
 
                         <!-- TEST FILE PATHS -->
-                        <!-- <a href="img/walks/walk-dene-one/IMG_20200507_135445.jpg" href="img/walks/walk-dene-one/IMG_20200507_135535.jpg" href="img/walks/walk-dene-one/IMG_20200507_143151.jpg" data-lightbox="walk-one" data-title="This is a caption" class="lightbox-link"> 
-                                <div class="follow-trail">Follow the Trail</div>                        
-                        </a>
-
-                        <a href="img/walks/walk-dene-one/IMG_20200507_135535.jpg" data-lightbox="walk-one" data-title="This is a caption 2" class="lightbox-link"> 
-                        </a>
-
-                        <a href="img/walks/walk-dene-one/IMG_20200507_143151.jpg" data-lightbox="walk-one" data-title="This is a caption 3" class="lightbox-link"> 
-                        </a> -->
-
-                        <!-- LOCAL FILE PATHS -->                            
-
-                        <!-- <a href="img/walks/walk-dene-one/IMG_20200507_135445.jpg" data-lightbox="walk-one" data-title="This is a caption" class="lightbox-link">
-                            <div class="follow-trail">Follow the Trail</div>
-                        </a>
-
-                        <a href="img/walks/walk-dene-one/IMG_20200507_135445.jpg" data-lightbox="walk-one" data-title="This is a caption 2" class="lightbox-link"> </a>
-
-                        <a href="img/walks/walk-dene-one/IMG_20200507_135445.jpg" data-lightbox="walk-one" data-title="This is a caption 3" class="lightbox-link"> </a>
-
-                        <a href="img/walks/walk-dene-one/IMG_20200507_135445.jpg" data-lightbox="walk-one" data-title="This is a caption 4" class="lightbox-link"> </a>
-
-                        <a href="img/walks/walk-dene-one/IMG_20200507_135445.jpg" data-lightbox="walk-one" data-title="This is a caption 5" class="lightbox-link"> </a> -->
-
-
 
                         <!-- walks project website URLS -->
 
-                        <a href="" data-lightbox="walk-three" data-title="This is a caption 1 route three" class="lightbox-link"> 
-                            <div class="follow-trail">Follow the Trail</div> </a>
+                        <a href="https://projects.jonniegrieve.co.uk/walks/img/walk-dene-three/IMG_20200507_135445.jpg" data-lightbox="walk-three" data-title="This is a caption 1 route three" class="lightbox-link"> 
+                            <div class="follow-trail">Follow the Trail</div>
+                        </a>
 
-                        <a href="" data-lightbox="walk-three" data-title="This is a caption 2 route three" class="lightbox-link"></a>
-                        <a href="" data-lightbox="walk-three" data-title="This is a caption 3 route three" class="lightbox-link"></a>
-                        <a href="" data-lightbox="walk-three" data-title="This is a caption 3 route three" class="lightbox-link"></a>
-                        <a href="" data-lightbox="walk-three" data-title="This is a caption 3 route three" class="lightbox-link"></a>
-                        <a href="" data-lightbox="walk-three" data-title="This is a caption 3 route three" class="lightbox-link"></a>
-                        <a href="" data-lightbox="walk-three" data-title="This is a caption 3 route three" class="lightbox-link"></a>
-                        <a href="" data-lightbox="walk-three" data-title="This is a caption 3 route three" class="lightbox-link"></a>
-                        <a href="" data-lightbox="walk-three" data-title="This is a caption 3 route three" class="lightbox-link"></a>
-                        <a href="" data-lightbox="walk-three" data-title="This is a caption 3 route three" class="lightbox-link"></a>
-                       
+                        <a href="https://projects.jonniegrieve.co.uk/walks/img/walk-dene-three/IMG_20200507_135535.jpg" data-lightbox="walk-three" data-title="This is a caption 2 route three" class="lightbox-link"></a>
+                        <a href="https://projects.jonniegrieve.co.uk/walks/img/walk-dene-three/IMG_20200507_143151.jpg" data-lightbox="walk-three" data-title="This is a caption 3 route three" class="lightbox-link"></a>
+                        <a href="https://projects.jonniegrieve.co.uk/walks/img/walk-dene-three/IMG_20200507_150105.jpg" data-lightbox="walk-three" data-title="This is a caption 3 route three" class="lightbox-link"></a>
+                        <a href="https://projects.jonniegrieve.co.uk/walks/img/walk-dene-three/IMG_20200507_150121.jpg" data-lightbox="walk-three" data-title="This is a caption 3 route three" class="lightbox-link"></a>
+                        <a href="https://projects.jonniegrieve.co.uk/walks/img/walk-dene-three/IMG_20200507_150407.jpg" data-lightbox="walk-three" data-title="This is a caption 3 route three" class="lightbox-link"></a>
+                        <a href="https://projects.jonniegrieve.co.uk/walks/img/walk-dene-three/IMG_20200507_150929.jpg" data-lightbox="walk-three" data-title="This is a caption 3 route three" class="lightbox-link"></a>
+                        <a href="https://projects.jonniegrieve.co.uk/walks/img/walk-dene-three/IMG_20200507_151151.jpg" data-lightbox="walk-three" data-title="This is a caption 3 route three" class="lightbox-link"></a>
+                        <a href="https://projects.jonniegrieve.co.uk/walks/img/walk-dene-three/IMG_20200507_151207.jpg" data-lightbox="walk-three" data-title="This is a caption 3 route three" class="lightbox-link"></a>
+                        <a href="https://projects.jonniegrieve.co.uk/walks/img/walk-dene-three/IMG_20200509_134032.jpg" data-lightbox="walk-three" data-title="This is a caption 3 route three" class="lightbox-link"></a> 
+                        <a href="https://projects.jonniegrieve.co.uk/walks/img/walk-dene-three/IMG_20200509_135132.jpg" data-lightbox="walk-three" data-title="This is a caption 3 route three" class="lightbox-link"></a> 
+                        <a href="https://projects.jonniegrieve.co.uk/walks/img/walk-dene-three/IMG_20200509_135327.jpg" data-lightbox="walk-three" data-title="This is a caption 3 route three" class="lightbox-link"></a>
+                        <a href="https://projects.jonniegrieve.co.uk/walks/img/walk-dene-three/IMG_20200509_135339.jpg" data-lightbox="walk-three" data-title="This is a caption 3 route three" class="lightbox-link"></a>
+                        <a href="https://projects.jonniegrieve.co.uk/walks/img/walk-dene-three/IMG_20200509_135431.jpg" data-lightbox="walk-three" data-title="This is a caption 3 route three" class="lightbox-link"></a>
+                        <a href="https://projects.jonniegrieve.co.uk/walks/img/walk-dene-three/IMG_20200509_140835.jpg" data-lightbox="walk-three" data-title="This is a caption 3 route three" class="lightbox-link"></a>                       
 
                         <!-- walks directories
 
-                        img/walks/walk-dene-five-yew-tree
-                        img/walks/walk-dene-four-horden
-                        img/walks/walk-dene-one
-                        img/walks/walk-dene-seven-hesleden-route
-                        img/walks/walk-dene-six-yew-tree-loop
                         img/walks/walk-dene-three
-                        img/walks/walk-dene-two
-                        img/walks/walk-eight-hart-to-haswell-right
-                        img/walks/walk-eleven-bleachery-dene
-                        img/walks/walk-four-horden
-                        img/walks/walk-fourteen
-                        img/walks/walk-nine-hart-to-haswell-left
-                        img/walks/walk-ten-bleachery-dene-left
-                        img/walks/walk-thirteen
-                        img/walks/walk-twelve-to-station-town
-
-
-                        https://projects.jonniegrieve.co.uk/walks/img/walk-dene-five-yew-tree
-                        https://projects.jonniegrieve.co.uk/walks/img/walk-dene-four-horden
-                        https://projects.jonniegrieve.co.uk/walks/img/walk-dene-one
-                        https://projects.jonniegrieve.co.uk/walks/img/walk-dene-seven-hesleden-route
-                        https://projects.jonniegrieve.co.uk/walks/img/walk-dene-six-yew-tree-loop
                         https://projects.jonniegrieve.co.uk/walks/img/walk-dene-three
-                        https://projects.jonniegrieve.co.uk/walks/img/walk-dene-two
-                        https://projects.jonniegrieve.co.uk/walks/img/walk-eight-hart-to-haswell-right
-                        https://projects.jonniegrieve.co.uk/walks/img/walk-eleven-bleachery-dene
-                        https://projects.jonniegrieve.co.uk/walks/img/walk-four-horden
-                        https://projects.jonniegrieve.co.uk/walks/img/walk-fourteen
-                        https://projects.jonniegrieve.co.uk/walks/img/walk-nine-hart-to-haswell-left
-                        https://projects.jonniegrieve.co.uk/walks/img/walk-ten-bleachery-dene-left
-                        https://projects.jonniegrieve.co.uk/walks/img/walk-thirteen
-                        https://projects.jonniegrieve.co.uk/walks/img/walk-twelve-to-station-town
-
-                        walk-dene-five-yew-tree
-                        walk-dene-four-horden
-                        walk-dene-one
-                        walk-dene-seven-hesleden-route
-                        walk-dene-six-yew-tree-loop
                         walk-dene-three
-                        walk-dene-two
-                        walk-eight-hart-to-haswell-right
-                        walk-eleven-bleachery-dene
-                        walk-four-horden
-                        walk-fourteen
-                        walk-nine-hart-to-haswell-left
-                        walk-ten-bleachery-dene-left
-                        walk-thirteen
-                        walk-twelve-to-station-town
 
                         --> 
+
                     </div> 
                         
                 </div>

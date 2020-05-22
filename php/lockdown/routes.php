4e2e67cda090331acfeed475b841c25cf01ca8a0
Jonathan Grieve <mail@jonniegrieve.co.uk>
2020-05-22 16:47:28 +0100

    constant website title headings

--- php/lockdown/routes.php
+++ php/lockdown/routes.php
@@ -1,7 +1,7 @@
 <?php 
 
-    $website_title = "Lockdown Page";
-    $pageTitle = "Route List";
+    $website_title = "Jaunts and Journey's in Lockdown Castle Eden";
+    $pageTitle = "Walking Trails in the Castle Eden area";
     $trailType= null;
 
 
@@ -24,19 +24,51 @@
 
     <section class="routes">
         
-        <h2>Main content goes here!</h2>
+        <h2>Walking Trails in the Castle Eden area</h2>
 
         <ul class="route-list">
 
-            <li><a href="inc/walk-templates/walk-one.php" title=""><strong>Route 1</strong - Route Description></a></li>
+            <li><a href="inc/walk-templates/walk-one.php" title="Red Squirrel Trail #1"><strong>Route 1</strong> - Red Squirrel Trail #1</a></li>
 
-            <li><a href="#" title=""><strong>Route 2</strong - Route Description></a></li>
+            <li><a href="inc/walk-templates/walk-two.php" title="Red Squirrel Trail #2"><strong>Route 2</strong> - Red Squirrel Trail #2</a></li>
 
-            <li><a href="#" title=""><strong>Route 3</strong - Route Description></a></li>
+            <li><a href="inc/walk-templates/walk-three.php" title="Walk Dene: Three"><strong>Route 3</strong> - Walk Dene: Three</a></li>
 
-            <li><a href="#" title=""><strong>Route 4</strong - Route Description></a></li>
+            <li><a href="inc/walk-templates/walk-four.php" title="Walk Trail: Horden Route"><strong>Route 4</strong> - Walk Trail: Horden Route</a></li>
 
-            <li><a href="#" title=""><strong>Route 5</strong - Route Description></a></li>
+            <li><a href="inc/walk-templates/walk-five.php" title="Walk Dene 5: Yew Tree Route"><strong>Route 5</strong> - Walk Dene 5: Yew Tree Route</a></li>
+
+            <li><a href="inc/walk-templates/walk-six.php" title="Walk Dene 6: Yew Tree Loop"><strong>Route 6</strong> - Walk Dene 6: Yew Tree Loop</a></li>
+
+            <li><a href="inc/walk-templates/walk-seven.php" title="Walk Dene Seven: Hesleden Route"><strong>Route 7</strong> - Walk Dene Seven: Hesleden Route</a></li>
+
+            <li><a href="inc/walk-templates/walk-eight.php" title="Walk 8:  Hart to Haswell (Right)"><strong>Route 8</strong> - Walk 8:  Hart to Haswell (Right)</a></li>
+
+            <li><a href="inc/walk-templates/walk-nine.php" title="Walk 9: Hart to Haswell (Left"><strong>Route 9</strong> - Walk 9: Hart to Haswell (Left)</a></li>
+
+            <li><a href="inc/walk-templates/walk-ten.php" title="Walk 10: Bleachery Dene: Left"><strong>Route 10</strong> - Walk 10: Bleachery Dene: Left</a></li>
+
+            <li><a href="inc/walk-templates/walk-eleven.php" title="Walk 11: Bleachery Dene"><strong>Route 11</strong> - Walk 11: Bleachery Dene</a></li>
+
+            <li><a href="inc/walk-templates/walk-twelve.php" title="Walk 12: To Station Town"><strong>Route 12</strong> - Walk 12: To Station Town</a></li>
+
+            <li><a href="inc/walk-templates/walk-thirteen.php" title="Walk: 13 Hart to Haswell Cross-field Route"><strong>Route 13</strong> - Walk: 13 Hart to Haswell Cross-field Route</a></li>
+
+             <!--
+            <li><a href="inc/walk-templates/walk-fourteen.php" title="Walk 14"><strong>Route 14</strong> - Walk 14</a></li>
+
+            <li><a href="inc/walk-templates/walk-fifteen.php" title="Route Description"><strong>Route 5</strong> - Route Description</a></li>
+
+            <li><a href="inc/walk-templates/walk-sixteen.php" title="Route Description"><strong>Route 5</strong> - Route Description</a></li>
+
+            <li><a href="inc/walk-templates/walk-seventeen.php" title="Route Description"><strong>Route 5</strong> - Route Description</a></li>
+
+            <li><a href="inc/walk-templates/walk-eighteen.php" title="Route Description"><strong>Route 5</strong> - Route Description</a></li>
+
+            <li><a href="inc/walk-templates/walk-nineteen.php" title="Route Description"><strong>Route 5</strong> - Route Description</a></li>
+
+            <li><a href="inc/walk-templates/walk-twenty.php" title="Route Description"><strong>Route 5</strong> - Route Description</a></li>
+            -->
 
         </ul>
 

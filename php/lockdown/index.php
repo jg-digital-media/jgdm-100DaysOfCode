1983abe97d98ff6bff60247d06d8c6ae7d720a00
Jonathan Grieve <mail@jonniegrieve.co.uk>
2020-05-22 10:06:17 +0100

    homepage content update

--- php/lockdown/index.php
+++ php/lockdown/index.php
@@ -25,9 +25,15 @@
 
     <article class="lockdown-intro">
 
-        <p>In March 2020 life changed for me and everyone else on the this planet. Due to the Covid-19 pandemic we all had to make changes to our daily lives and our routines. This left me feeling strange and disoriented at what was happening and the loss of some of the freedoms that we enjoy. Loss of freedom with good reasons might I had.  I did discover however that gettng outside was the best way to make myself feel better about life. So I got out and I got walking. And I feel so lucky that I've had so many safe and green spaces to go while the UK was in lockdown. So I created this website as something nice to rememvwer this strange period by.</p>
+        <p>In March 2020 life changed for me and everyone else on the planet, possibly forever </p> 
+        
+        <p>Due to the Covid-19 pandemic we all had to make changes to our daily lives and our routines. This left me feeling strange and disoriented at what was happening and the loss of some of the freedoms that we enjoy. Loss of freedom with good reasons might I had. </p>
+        
+        <p>This prompted me to change some things I discovered that outside was the best way to make myself feel better about life. So I got out and I got walking. I discovered a number of walking trails in and around the area I live in. </p>
+        
+        <p>I feel so lucky that I've had so many safe and green spaces to go to while the UK was in Lockdown. So I created this website as something nice to remember this strange period by.</p>
 
-        <a href="routes.php" class="main-btn" title="Go to Routes page" target="self">Button - Routes</a>
+        <a href="routes.php" class="main-btn" title="Go to Routes page" target="self">Walk Trail List</a>
 
     </article>
 
@@ -40,26 +46,27 @@
     <article class="route-list">
 
         <ul>
-            <h3>Dene</h3>
+            <h3>Featured Trails - Castle Eden Dene</h3>
 
-            <li><a href="inc/walk-templates/walk-one.php" class="route-list-item" title="Route 1">One</li>
-            <li><a href="" class="route-list-item" title="">Two</a></li>
+            <li><a href="inc/walk-templates/walk-one.php" class="route-list-item" title="Castle Eden Dene Trail: 1">Castle Eden Dene Trail: 1</li>
+            <li><a href="inc/walk-templates/walk-two.php" class="route-list-item" title="Castle Eden Dene Trail: 2">Castle Eden Dene Trail: 2</a></li>
+            <li><a href="inc/walk-templates/walk-three.php" class="route-list-item" title="Castle Eden Dene Trail: 3">Castle Eden Dene Trail: 3</a></li>
+            <li><a href="inc/walk-templates/walk-four.php" class="route-list-item" title="Horden Trail: Route 4">Horden Trail: Route 4</a></li>
+
+            <h3>Featured Trails: Hart to Haswell Trail</h3>
+
+            <li><a href="inc/walk-templates/walk-seven.php" class="route-list-item" title="Hesleden Trail">Helseden Trail</a></li>
+            <li><a href="inc/walk-templates/walk-eight.php" class="route-list-item" title="Walk 8: Haswell to Hart">Walk 8: Hart to Haswell</a></li>
+            <li><a href="inc/walk-templates/walk-nine.php" class="route-list-item" title="Walk 9: Hart to Haswell">Walk 9: Haswell to Hart</a></li>
+
+
+            
+            <h3>Featured Trails: Coastal</h3>
+
+            <li>Coming Soon</li>
+            <!--<li><a href="" class="route-list-item" title="">Two</a></li>
             <li><a href="" class="route-list-item" title="">Three</a></li>
-            <li><a href="" class="route-list-item" title="">Four</a></li>
-
-            <h3>Railway Line</h3>
-
-            <li>One</li>
-            <li><a href="" class="route-list-item" title="">Two</a></li>
-            <li><a href="" class="route-list-item" title="">Three</a></li>
-            <li><a href="" class="route-list-item" title="">Four</a></li>
-
-            <h3>Coastal</h3>
-
-            <li>One</li>
-            <li><a href="" class="route-list-item" title="">Two</a></li>
-            <li><a href="" class="route-list-item" title="">Three</a></li>
-            <li><a href="" class="route-list-item" title="">Four</a></li>
+            <li><a href="" class="route-list-item" title="">Four</a></li>-->
         </ul>
 
     </article>
@@ -70,14 +77,14 @@
 
 <article class="slider-container">
 
-    <h3>Select</h3>
+    <h3>Select a Route</h3>
 
     <div class="route-slider">
 
         <div> <a href="inc/walk-templates/walk-one.php" title="Walk Dene: 1" class="route-slider-link"> <img src="img/walk_homepage/walk-dene-one.jpg" alt="Walk Dene: One" class="route-slider-image" /> </a> </div>
         <div> <a href="inc/walk-templates/walk-two.php" title="Walk Dene: Two" class="route-slider-link"> <img src="img/walk_homepage/walk-dene-two.jpg" alt="Walk Dene: Two" class="route-slider-image" /> </a> </div> 
         <div> <a href="inc/walk-templates/walk-three.php" title="Walk Dene: Three" class="route-slider-link"> <img src="img/walk_homepage/walk-dene-three.jpg" alt="Walk Dene: Three" class="route-slider-image" /> </a> </div> 
-        <div> <a href="inc/walk-templates/walk-four.php" title="Walk Trail: Horden Route" class="route-slider-link"> <img src="img/walk_homepage/walk-four-horden.jpg" alt="Walk Trail: Horden Route" class="route-slider-image" /> </a> </div> 
+        <div> <a href="inc/walk-templates/walk-four.php" title="Walk Trail: Horden Route" class="route-slider-link"> <img src="img/walk_homepage/walk-dene-four-horden.jpg" alt="Walk Trail: Horden Route" class="route-slider-image" /> </a> </div> 
         <div> <a href="inc/walk-templates/walk-five.php" title="Walk Dene 5: Yew Tree Route" class="route-slider-link"> <img src="img/walk_homepage/walk-dene-five-yew-tree.jpg" alt="Walk Dene 5: Yew Tree Route" class="route-slider-image" /> </a> </div>
         <div> <a href="inc/walk-templates/walk-six.php" title="Walk Dene 6: Yew Tree Loop" class="route-slider-link"> <img src="img/walk_homepage/walk-dene-six-yew-tree-loop.jpg" alt="Walk Dene 6: Yew Tree Loop" class="route-slider-image" /> </a> </div>
         <div> <a href="inc/walk-templates/walk-seven.php" title="Walk Dene Seven: Hesleden Route" class="route-slider-link"> <img src="img/walk_homepage/walk-dene-seven-hesleden-route.jpg" alt="Walk Dene Seven: Hesleden Route" class="route-slider-image" /> </a> </div>
@@ -86,7 +93,7 @@
         <div> <a href="inc/walk-templates/walk-ten.php" title="Walk 10: Bleachery Dene: Left" class="route-slider-link"> <img src="img/walk_homepage/walk-ten-bleachery-dene-left.jpg" alt="Walk 10: Bleachery Dene: Left" class="route-slider-image" /> </a> </div> 
         <div> <a href="inc/walk-templates/walk-eleven.php" title="Walk 11: Bleachery Dene" class="route-slider-link"> <img src="img/walk_homepage/walk-eleven-bleachery-dene.jpg" alt="Walk 11: Bleachery Dene" class="route-slider-image" /> </a> </div> 
         <div> <a href="inc/walk-templates/walk-twelve.php" title="Walk 12: To Station Town" class="route-slider-link"> <img src="img/walk_homepage/walk-twelve-to-station-town.jpg" alt="Walk 12: To Station Town" class="route-slider-image" /> </a> </div> 
-        <div> <a href="inc/walk-templates/walk-thirteen.php" title="Walk 13" class="route-slider-link"> <img src="img/walk_homepage/walk-thirteen.jpg" alt="Walk 13" class="route-slider-image" /> </a> </div> 
+        <div> <a href="inc/walk-templates/walk-thirteen.php" title="Walk: 13 Hart to Haswell Cross-field Route" class="route-slider-link"> <img src="img/walk_homepage/walk-thirteen.jpg" alt="Walk: 13 Hart to Haswell Cross-field Route" class="route-slider-image" /> </a> </div> 
         <div> <a href="inc/walk-templates/walk-fourteen.php" title="Walk 14" class="route-slider-link"> <img src="img/walk_homepage/walk-fourteen.jpg" alt="Walk 14" class="route-slider-image" /> </a> </div> 
 
         <!--

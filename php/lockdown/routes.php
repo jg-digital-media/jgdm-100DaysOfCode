0b2627d1d1b8fc50151646308865a28200baf1b2
Jonathan Grieve <mail@jonniegrieve.co.uk>
2020-05-25 10:16:28 +0100

    echo route titles list

--- php/lockdown/routes.php
+++ php/lockdown/routes.php
@@ -33,44 +33,44 @@
 
             <li><a href="inc/walk-templates/walk-one.php" title="Red Squirrel Trail #1"><strong>Route 1</strong> - <?php echo $trailTitleOne; ?> </a></li>
 
-            <li><a href="inc/walk-templates/walk-two.php" title="Red Squirrel Trail #2"><strong>Route 2</strong> - Red Squirrel Trail #2</a></li>
+            <li><a href="inc/walk-templates/walk-two.php" title="Red Squirrel Trail #2"><strong>Route 2</strong> - <?php echo $trailTitleTwo; ?></a></li>
 
-            <li><a href="inc/walk-templates/walk-three.php" title="Walk Dene: Three"><strong>Route 3</strong> - Walk Dene: Three</a></li>
+            <li><a href="inc/walk-templates/walk-three.php" title="Walk Dene: Three"><strong>Route 3</strong> - <?php echo $trailTitleThree; ?></a></li>
 
-            <li><a href="inc/walk-templates/walk-four.php" title="Walk Trail: Horden Route"><strong>Route 4</strong> - Walk Trail: Horden Route</a></li>
+            <li><a href="inc/walk-templates/walk-four.php" title="Walk Trail: Horden Route"><strong>Route 4</strong> - <?php echo $trailTitleFour; ?></a></li>
 
-            <li><a href="inc/walk-templates/walk-five.php" title="Walk Dene 5: Yew Tree Route"><strong>Route 5</strong> - Walk Dene 5: Yew Tree Route</a></li>
+            <li><a href="inc/walk-templates/walk-five.php" title="Walk Dene 5: Yew Tree Route"><strong>Route 5</strong> -<?php echo $trailTitleFive; ?></a></li>
 
-            <li><a href="inc/walk-templates/walk-six.php" title="Walk Dene 6: Yew Tree Loop"><strong>Route 6</strong> - Walk Dene 6: Yew Tree Loop</a></li>
+            <li><a href="inc/walk-templates/walk-six.php" title="Walk Dene 6: Yew Tree Loop"><strong>Route 6</strong> - <?php echo $trailTitleSix; ?></a></li>
 
-            <li><a href="inc/walk-templates/walk-seven.php" title="Walk Dene Seven: Hesleden Route"><strong>Route 7</strong> - Walk Dene Seven: Hesleden Route</a></li>
+            <li><a href="inc/walk-templates/walk-seven.php" title="Walk Dene Seven: Hesleden Route"><strong>Route 7</strong> -<?php echo $trailTitleSeven; ?></a></li>
 
-            <li><a href="inc/walk-templates/walk-eight.php" title="Walk 8:  Hart to Haswell (Right)"><strong>Route 8</strong> - Walk 8:  Hart to Haswell (Right)</a></li>
+            <li><a href="inc/walk-templates/walk-eight.php" title="Walk 8:  Hart to Haswell (Right)"><strong>Route 8</strong> - <?php echo $trailTitleEight; ?></a></li>
 
-            <li><a href="inc/walk-templates/walk-nine.php" title="Walk 9: Hart to Haswell (Left"><strong>Route 9</strong> - Walk 9: Hart to Haswell (Left)</a></li>
+            <li><a href="inc/walk-templates/walk-nine.php" title="Walk 9: Hart to Haswell (Left"><strong>Route 9</strong> - <?php echo $trailTitleNine; ?></a></li>
 
-            <li><a href="inc/walk-templates/walk-ten.php" title="Walk 10: Bleachery Dene: Left"><strong>Route 10</strong> - Walk 10: Bleachery Dene: Left</a></li>
+            <li><a href="inc/walk-templates/walk-ten.php" title="Walk 10: Bleachery Dene: Left"><strong>Route 10</strong> - <?php echo $trailTitleTen; ?></a></li>
 
-            <li><a href="inc/walk-templates/walk-eleven.php" title="Walk 11: Bleachery Dene"><strong>Route 11</strong> - Walk 11: Bleachery Dene</a></li>
+            <li><a href="inc/walk-templates/walk-eleven.php" title="Walk 11: Bleachery Dene"><strong>Route 11</strong> - <?php echo $trailTitleEleven; ?></a></li>
 
-            <li><a href="inc/walk-templates/walk-twelve.php" title="Walk 12: To Station Town"><strong>Route 12</strong> - Walk 12: To Station Town</a></li>
+            <li><a href="inc/walk-templates/walk-twelve.php" title="Walk 12: To Station Town"><strong>Route 12</strong> - <?php echo $trailTitleTwelve; ?></a></li>
 
-            <li><a href="inc/walk-templates/walk-thirteen.php" title="Walk: 13 Hart to Haswell Cross-field Route"><strong>Route 13</strong> - Walk: 13 Hart to Haswell Cross-field Route</a></li>
+            <li><a href="inc/walk-templates/walk-thirteen.php" title="Walk: 13 Hart to Haswell Cross-field Route"><strong>Route 13</strong> - <?php echo $trailTitleThirteen; ?></a></li>
 
              <!--
-            <li><a href="inc/walk-templates/walk-fourteen.php" title="Walk 14"><strong>Route 14</strong> - Walk 14</a></li>
+            <li><a href="inc/walk-templates/walk-fourteen.php" title="Walk 14"><strong>Route 14</strong> - <?php echo $trailTitleFourteen; ?></a></li>
 
-            <li><a href="inc/walk-templates/walk-fifteen.php" title="Route Description"><strong>Route 5</strong> - Route Description</a></li>
+            <li><a href="inc/walk-templates/walk-fifteen.php" title="Route Description"><strong>Route 5</strong> - <?php echo $trailTitlFifteen; ?></a></li>
 
-            <li><a href="inc/walk-templates/walk-sixteen.php" title="Route Description"><strong>Route 5</strong> - Route Description</a></li>
+            <li><a href="inc/walk-templates/walk-sixteen.php" title="Route Description"><strong>Route 5</strong> - <?php echo $trailTitleSixteen; ?></a></li>
 
-            <li><a href="inc/walk-templates/walk-seventeen.php" title="Route Description"><strong>Route 5</strong> - Route Description</a></li>
+            <li><a href="inc/walk-templates/walk-seventeen.php" title="Route Description"><strong>Route 5</strong> - <?php echo $trailTitleSeventeen; ?></a></li>
 
-            <li><a href="inc/walk-templates/walk-eighteen.php" title="Route Description"><strong>Route 5</strong> - Route Description</a></li>
+            <li><a href="inc/walk-templates/walk-eighteen.php" title="Route Description"><strong>Route 5</strong> - <?php echo $trailTitleEighteen; ?></a></li>
 
-            <li><a href="inc/walk-templates/walk-nineteen.php" title="Route Description"><strong>Route 5</strong> - Route Description</a></li>
+            <li><a href="inc/walk-templates/walk-nineteen.php" title="Route Description"><strong>Route 5</strong> - <?php echo $trailTitleNineteen; ?></a></li>
 
-            <li><a href="inc/walk-templates/walk-twenty.php" title="Route Description"><strong>Route 5</strong> - Route Description</a></li>
+            <li><a href="inc/walk-templates/walk-twenty.php" title="Route Description"><strong>Route 5</strong> - <?php echo $trailTitleTwenty; ?></a></li>
             -->
 
         </ul>

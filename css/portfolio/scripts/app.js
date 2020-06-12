156aef0cc0d8354bfccef660399238f7dcd95d3d
Jonathan Grieve <mail@jonniegrieve.co.uk>
2020-06-12 16:44:57 +0100

    build complete json data

--- css/portfolio/scripts/app.js
+++ css/portfolio/scripts/app.js
@@ -1,7 +1,42 @@
 //check for jQuery
 //alert("jQuery in place");
 
+
+/**
+ * Random Featured Projects
+ */
+
+jQuery.getJSON('assets/data/project-list.json', function(photoData) { //Ajax call
+    //var itemAll = data.images[Math.floor(Math.random() * data.images.length)]; //select image at random
+
+    //select length of json array
+    let itemAll = photoData.projects.length;
+    console.log(photoData.projects.length);
+
+    for (let i=0; i < itemAll; i++) { 
+
+       //build the complete <img> tag
+      /*  jQuery(`<div class="photo-item">
+
+           <img src="${itemAll.url}" class="thumbnails" alt="${itemAll.alt}" title="${itemAll.alt}" />
+           
+           </div>  <div>${itemAll.caption}</div>
+           
+       `).appendTo('#all-photos'); */
+
+
+       //successful delivery of class and image URL
+            
+        jQuery(`<a href="${photoData.projects[i].project_url}" target="blank"><img src="${photoData.projects[i].img_url}" class="site-images" alt="${photoData.projects[i].img_url}" title="${photoData.project_alt}" tabindex="" /></a>`).appendTo('.show-featured .all');     
+
+   }
+
+});
+
+
 //Vue.js instance
+
+
 
 //testimonials page
 const testimonials = [

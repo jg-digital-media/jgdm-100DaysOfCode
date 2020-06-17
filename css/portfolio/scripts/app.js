faf38a019c8252ba0e54532e1be9069778164075
Jonathan Grieve <mail@jonniegrieve.co.uk>
2020-06-17 11:54:17 +0100

    conditional icons based on status key value

--- css/portfolio/scripts/app.js
+++ css/portfolio/scripts/app.js
@@ -17,28 +17,22 @@ jQuery.getJSON('assets/data/project-list.json', function(photoData) { //Ajax cal
 
     for (let i=0; i < itemAll; i++) { 
 
-       //build the complete <img> tag
-      /*  jQuery(`<div class="photo-item">
+        //build the complete <img> tag
+        /*  jQuery(`<div class="photo-item">
 
-           <img src="${itemAll.url}" class="thumbnails" alt="${itemAll.alt}" title="${itemAll.alt}" />
-           
-           </div>  <div>${itemAll.caption}</div>
-           
-       `).appendTo('#all-photos'); */
-
-
-       //successful delivery of class and image URL
+            <img src="${itemAll.url}" class="thumbnails" alt="${itemAll.alt}" title="${itemAll.alt}" />
             
+            </div>  <div>${itemAll.caption}</div>
+            
+        `).appendTo('#all-photos'); */
+
+
+        //successful delivery of class and image URL            
         jQuery(`<a href="${photoData.projects[i].project_url}" target="blank"><img src="${photoData.projects[i].img_url}" class="site-images" alt="${photoData.projects[i].project_alt}" title="${photoData.projects[i].project_alt}" tabindex="" /></a>`).appendTo('.all');     
 
    }
 
 });
-
-
-//Vue.js instance
-
-
 
 //testimonials page
 const testimonials = [

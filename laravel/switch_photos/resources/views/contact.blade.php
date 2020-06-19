af1057b76ed2862e871227c71597522b96a4c6cf
Jonathan Grieve <mail@jonniegrieve.co.uk>
2020-06-19 16:01:31 +0100

    restructure blade templates

--- laravel/switch_photos/resources/views/contact.blade.php
+++ laravel/switch_photos/resources/views/contact.blade.php
@@ -5,4 +5,8 @@
 
    <div>Content Contact</div>
 
+   <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus quo consequatur repellendus accusantium libero corrupti obcaecati, sunt recusandae repellat error amet autem cumque ea distinctio optio fugiat vero at quae!</p>
+
+   <p>Find out more!</p>
+
 @endsection
--- laravel/switch_photos/resources/views/home.blade.php
+++ laravel/switch_photos/resources/views/home.blade.php
@@ -9,18 +9,17 @@
 
     @include('nav')
 
+    <div>        
 
-    <div>
-        
+        <h1>Website Title</h1>
 
-    <h1>Home Blade Template</h1>
+        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto in quibusdam rem quasi accusantium ex quae incidunt libero earum! Dolore tempora labore eos. Eligendi, blanditiis. Suscipit sequi labore voluptatum ut.</h2>
 
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto in quibusdam rem quasi accusantium ex quae incidunt libero earum! Dolore tempora labore eos. Eligendi, blanditiis. Suscipit sequi labore voluptatum ut.</p>
-
-    <img src="{{asset('clearLogo.png')}}" alt="Image" />
-    <img src="clearLogo.png" alt="Image" />
+        <img src="{{asset('clearLogo.png')}}" alt="Image" />
+        <img src="clearLogo.png" alt="Image" />
 
     
+        <!-- Unique content goes here! -->
         @yield('content')
 
     </div>

bcd484e6ea223ab417105da2ade86ffbdd60a4ba
Jonathan Grieve <mail@jonniegrieve.co.uk>
2020-06-23 10:40:51 +0100

    display social and logo images

--- laravel/switch_photos/resources/views/head.blade.php
+++ laravel/switch_photos/resources/views/head.blade.php
@@ -5,4 +5,30 @@
     <title>Home Blade Template</title>
 
     <link href="{{asset('style.css')}}" rel="stylesheet">
+
+    <!-- Meta Descriptions -->
+
+
+    <!-- Canonical -->
+
+
+    <!-- Google Font
+    <link href="https://fonts.googleapis.com/css?family=Merriweather|Odibee+Sans|Quicksand&display=swap" rel="stylesheet">-->
+
+    <!-- Favicon -->
+    <link rel="icon" href="favicon.png" type="image/png">
 </head>
+
+<header class=".header">
+
+    <div class="logo-container">
+        <img src="{{ asset('img/main-logo.png')}}" alt="Main Logo" title="Main logo" />
+    </div>
+
+    <div class="website-title">
+        <h1>Web Title</h1>
+    </div>
+
+    @include ('nav')
+
+</header>

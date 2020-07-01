f313f14a77e6bea84b60a8b7f0f42ffa52f8d1a8
Jonathan Grieve <mail@jonniegrieve.co.uk>
2020-07-01 16:49:26 +0100

    fix image filepaths

--- laravel/switch_photos/resources/views/all.blade.php
+++ laravel/switch_photos/resources/views/all.blade.php
@@ -10,21 +10,21 @@
 
 <article class="category-heading">
 
-    h2>Content: Home</h2>
+    <h2>Content: Home</h2>
 
 <article>
 
 <article class = "main-content">
 
     <!-- dynamic image content - to be replaced. -->
-    <img class="img zelda-botw" src="img/game-screenshout-placeholder.png"  alt="#"  title="#" />
-    <img class="img zelda-botw" src="img/game-screenshout-placeholder.png"  alt="#"  title="#" />
-    <img class="img zelda-botw" src="img/game-screenshout-placeholder.png"  alt="#"  title="#" />
-    <img class="img zelda-botw" src="img/game-screenshout-placeholder.png"  alt="#"  title="#" />
-    <img class="img mario" src="img/game-screenshout-placeholder.png"  alt="#"  title="#" />
-    <img class="img mario" src="img/game-screenshout-placeholder.png"  alt="#"  title="#" />
-    <img class="img mario" src="img/game-screenshout-placeholder.png"  alt="#"  title="#" />
-    <img class="img mario" src="img/game-screenshout-placeholder.png"  alt="#"  title="#" />
+    <img class="img zelda-botw" src="img/game-screenshot-placeholder.png"  alt="#"  title="#" />
+    <img class="img zelda-botw" src="img/game-screenshot-placeholder.png"  alt="#"  title="#" />
+    <img class="img zelda-botw" src="img/game-screenshot-placeholder.png"  alt="#"  title="#" />
+    <img class="img zelda-botw" src="img/game-screenshot-placeholder.png"  alt="#"  title="#" />
+    <img class="img mario" src="img/game-screenshot-placeholder.png"  alt="#"  title="#" />
+    <img class="img mario" src="img/game-screenshot-placeholder.png"  alt="#"  title="#" />
+    <img class="img mario" src="img/game-screenshot-placeholder.png"  alt="#"  title="#" />
+    <img class="img mario" src="img/game-screenshot-placeholder.png"  alt="#"  title="#" />
 
 </article>
 

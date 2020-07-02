a001767369d3d9bb145f7c100318dd77286f9aa6
Jonathan Grieve <mail@jonniegrieve.co.uk>
2020-07-02 09:38:47 +0100

    add project review icon

--- css/project_status/scripts/app.js
+++ css/project_status/scripts/app.js
@@ -11,12 +11,17 @@ jQuery.getJSON('../../../assets/data/project-list.json', function(photoData) {
         //jQuery(`<a href="${photoData.projects[i].project_url}" target="blank"><img src="${photoData.projects[i].img_url}" class="site-images" alt="${photoData.projects[i].project_alt}" title="${photoData.projects[i].project_alt}" tabindex="" /></a>`).appendTo('.project-status-data');
 
         if(photoData.projects[i].status === true) {
-            jQuery(`<li> <span class="complete"> &nbsp; </span> </li>`).appendTo('#project-status');
-        } else {
+            jQuery(`<li> <span class="complete"> &nbsp; </span>  </li>`).appendTo('#project-status');
+
+        } else if (photoData.projects[i].status === false) {
             jQuery(`<li> <span class="incomplete"> &nbsp; </span> </li>`).appendTo('#project-status');
 
+        } else {
+            jQuery(`<li> <span class="under-review"> &nbsp; </span> </li>`).appendTo('#project-status');
         }
-        
+
         jQuery(`<li> ${photoData.projects[i].project_name} <strong>(${photoData.projects[i].project_type})</strong> </li>`).appendTo('#project-names');
+
     }
+    
 });

6441910f72b2d01e30e37e52362d855a8011d985
Jonathan Grieve <mail@jonniegrieve.co.uk>
2020-07-02 15:39:25 +0100

    select class attributes with DOM

--- laravel/switch_photos/public/app.js
+++ laravel/switch_photos/public/app.js
@@ -1,1 +1,28 @@
-console.log("js connected")
+console.log("js connected");
+
+/**/
+    //select photo groups  - change vars
+    const game = document.querySelectorAll(".zelda-botw");
+    const game = document.querySelectorAll(".zelda-linksawakening");
+    const game = document.querySelectorAll(".fifa-one");
+    const game = document.querySelectorAll(".mario-odyssey");
+    const game = document.querySelectorAll(".animal-crossing");
+    const game = document.querySelectorAll(".super-mario-maker");
+    const game = document.querySelectorAll(".donkey-kong-tropic");
+    const game = document.querySelectorAll(".mario-bros");
+    const game = document.querySelectorAll(".mario-bros-two");
+    const game = document.querySelectorAll(".mario-bros-three");
+    const game = document.querySelectorAll(".mario-bros-four");
+    const game = document.querySelectorAll(".mario-world");
+    const game = document.querySelectorAll(".mario-dulux");
+    const game = document.querySelectorAll(".zelda-past");
+    const game = document.querySelectorAll(".mega-man-two");
+    const game = document.querySelectorAll(".world-war-two");
+    const game = document.querySelectorAll(".rac-rally-champ");
+    const game = document.querySelectorAll(".fifa-18");
+    const game = document.querySelectorAll(".asterix-game");
+    const game = document.querySelectorAll(".mario-kart-duluxe");
+    const game = document.querySelectorAll(".sonic-mania");
+    const game = document.querySelectorAll(".football-manage");
+
+

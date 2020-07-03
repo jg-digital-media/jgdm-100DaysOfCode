2850523317b47c5cf027e01b1181728a5282c12e
Jonathan Grieve <mail@jonniegrieve.co.uk>
2020-07-03 13:17:04 +0100

    select DOM elements for image toggling

--- laravel/switch_photos/public/app.js
+++ laravel/switch_photos/public/app.js
@@ -1,28 +1,30 @@
 console.log("js connected");
 
-/**/
-    //select photo groups  - change vars
-    const game = document.querySelectorAll(".zelda-botw");
-    const game = document.querySelectorAll(".zelda-linksawakening");
-    const game = document.querySelectorAll(".fifa-one");
-    const game = document.querySelectorAll(".mario-odyssey");
-    const game = document.querySelectorAll(".animal-crossing");
-    const game = document.querySelectorAll(".super-mario-maker");
-    const game = document.querySelectorAll(".donkey-kong-tropic");
-    const game = document.querySelectorAll(".mario-bros");
-    const game = document.querySelectorAll(".mario-bros-two");
-    const game = document.querySelectorAll(".mario-bros-three");
-    const game = document.querySelectorAll(".mario-bros-four");
-    const game = document.querySelectorAll(".mario-world");
-    const game = document.querySelectorAll(".mario-dulux");
-    const game = document.querySelectorAll(".zelda-past");
-    const game = document.querySelectorAll(".mega-man-two");
-    const game = document.querySelectorAll(".world-war-two");
-    const game = document.querySelectorAll(".rac-rally-champ");
-    const game = document.querySelectorAll(".fifa-18");
-    const game = document.querySelectorAll(".asterix-game");
-    const game = document.querySelectorAll(".mario-kart-duluxe");
-    const game = document.querySelectorAll(".sonic-mania");
-    const game = document.querySelectorAll(".football-manage");
+//select photo groups  - change vars
+const game_cat_botw = document.querySelectorAll(".zelda-botw")[0];
 
+//Select checkboxes
+const toggleZeldaBreathOfWild = document.getElementById("zelda-one");
 
+//Event Listeners
+game_cat_botw.addEventListener("click", function(e) {
+
+    if( toggleZeldaBreathOfWild.checked == false ) {
+
+        toggleZeldaBreathOfWild.checked = true;
+        //toggleZeldaBreathOfWild.checkbox.checked = checked;
+        game_cat_botw.style.display = "inline-block";
+            
+    } else if( toggleZeldaBreathOfWild.checked == true ) {
+
+        toggleZeldaBreathOfWild.checked = false;
+        //toggleZeldaBreathOfWild.checkbox.checked = unchecked;
+        game_cat_botw.style.display = "none";
+
+    }
+
+});
+
+/*
+
+*/

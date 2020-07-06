c44d630bc2c36fd597e6660b54ab91c12a958f42
Jonathan Grieve <mail@jonniegrieve.co.uk>
2020-07-06 16:34:34 +0100

    begin looping of first category of photos

--- laravel/switch_photos/public/js/app.js
+++ laravel/switch_photos/public/js/app.js
@@ -1,18 +1,14 @@
 console.log("js connected");
 
 //select photo groups  - change vars
-let game_cat_botw = document.querySelectorAll(".zelda-botw")[0];
-//console.log( game_cat_botw.length );
-
+let game_cat_botw = document.querySelectorAll(".zelda_botw")[0];
 let game_cat_lAwakening = document.querySelectorAll(".zelda-linksawakening")[0];
 let game_cat_fifaOne = document.querySelectorAll(".fifa-twenty")[0];
-
 
 //Select checkboxes
 let toggleZeldaBreathOfWild = document.getElementById("zelda-one");
 let toggleZeldaLinksAwakening = document.getElementById("zelda-two");
 let toggleFifaOne = document.getElementById("fifa-one");
-
 
 //Event Listeners
 game_cat_botw.addEventListener("click", function(e) {
@@ -32,16 +28,25 @@ game_cat_botw.addEventListener("click", function(e) {
 });
 
 
+
 // functions
-function toggleImage() {
-    if (game_cat_botw.style.display === "inline-block") {
-        game_cat_botw.style.display = "none";
-    } else if ( game_cat_botw.style.display = "none" ) {
-        game_cat_botw.style.display = "inline-block";
+function toggleImage(category) {
+
+    let cat_one_len = category.zelda_botw[i].length;
+
+    for(let i=0; i < cat_one_len; i++); {    
+
+        let game_cat_botw = document.querySelectorAll(".zelda_botw")[0];
+        
+        if (game_cat_botw.style.display[i] === "inline-block") {
+            game_cat_botw.style.display[i] = "none";
+        } else if ( game_cat_botw.style.display[i] = "none" ) {
+            game_cat_botw.style.display[i] = "inline-block";
+        }
     }
 }
 
-function toggleSecondImage() {
+/* function toggleSecondImage() {
     if (game_cat_lAwakening.style.display === "inline-block") {
         game_cat_lAwakening.style.display = "none";
     } else if ( game_cat_lAwakening.style.display = "none" ) {
@@ -54,15 +59,15 @@ function toggleThirdImage() {
         game_cat_fifaOne.style.display = "none";
     } else if ( game_cat_fifaOne.style.display = "none" ) {
         game_cat_fifaOne.style.display = "inline-block";
-    }
+     }
 }
 
+*/
 
 
 /** 
  * Get JSON data.
  */
-
 jQuery.getJSON('data/all-photos-classes.json', function(catOne_data) {
     //let getData;
 
@@ -71,7 +76,7 @@ jQuery.getJSON('data/all-photos-classes.json', function(catOne_data) {
         //get all thumbnails in one category
         //jQuery('<div class="photo-item"> <img src="' + photoData.pets[i].thumb_url +'" loading="lazy" data-src= "'  + photoData.pets[i].thumb_url + '" class="' + 'thumbnails ' + photoData.pets[i].class + '" /></div>').appendTo('.category-photos');
 
-        jQuery(`<img class="img ${ catOne_data.zelda-botw[i].img_class }" src="${ catOne_data.zelda-botw[i].img_url }" alt="${ catOne_data.zelda-botw[i].img_alt }" title="${ catOne_data.zelda-botw[i].img_alt }" /> `).appendTo('.category-photos');
+        jQuery(`<img class="img ${ catOne_data.zelda_botw[i].img_class }" src="${ catOne_data.zelda_botw[i].img_url }" alt="${ catOne_data.zelda_botw[i].img_alt }" title="${ catOne_data.zelda_botw[i].img_alt }" /> `).appendTo('.category-photos');
     }
 
 });
--- laravel/switch_photos/resources/views/home.blade.php
+++ laravel/switch_photos/resources/views/home.blade.php
@@ -18,30 +18,11 @@
             <article class = "category-photos">
 
                 <!-- dynamic image content - to be replaced. -->
-                <img class="img zelda-botw" src="img/game-screenshot-placeholder.png" alt="#" title="#" />
-                <img class="img zelda-botw" src="img/game-screenshot-placeholder.png" alt="#" title="#" />
+                <img class="img zelda_botw" src="img/game-screenshot-placeholder.png" alt="#" title="#" />
+                <img class="img zelda_botw" src="img/game-screenshot-placeholder.png" alt="#" title="#" />
                 <img class="img zelda-linksawakening" src="img/game-screenshot-placeholder.png" alt="#" title="#" />
                 <img class="img fifa-twenty" src="img/game-screenshot-placeholder.png" alt="#" title="#" />
-                <img class="img fifa-twenty" src="img/game-screenshot-placeholder.png" alt="#" title="#" />
-                <img class="img mario-odyssey" src="img/game-screenshot-placeholder.png" alt="#" title="#" />
-                <img class="img animal-crossing" src="img/game-screenshot-placeholder.png" alt="#" title="#" />
-                <img class="img super-mario-maker" src="img/game-screenshot-placeholder.png" alt="#" title="#" />
-                <img class="img donkey-kong-tropic" src="img/game-screenshot-placeholder.png" alt="#" title="#" />
-                <img class="img mario-bros" src="img/game-screenshot-placeholder.png" alt="#" title="#" />
-                <img class="img mario-bros-two" src="img/game-screenshot-placeholder.png" alt="#" title="#" />
-                <img class="img mario-bros-three" src="img/game-screenshot-placeholder.png" alt="#" title="#" />
-                <img class="img mario-bros-four" src="img/game-screenshot-placeholder.png" alt="#" title="#" />
-                <img class="img mario-world" src="img/game-screenshot-placeholder.png" alt="#" title="#" />
-                <img class="img mario-duluxe" src="img/game-screenshot-placeholder.png" alt="#" title="#" />
-                <img class="img zelda-past" src="img/game-screenshot-placeholder.png" alt="#" title="#" />
-                <img class="img mega-man-two" src="img/game-screenshot-placeholder.png" alt="#" title="#" />
-                <img class="img world-war-two" src="img/game-screenshot-placeholder.png" alt="#" title="#" />
-                <img class="img rac-rally-champ" src="img/game-screenshot-placeholder.png" alt="#" title="#" />
-                <img class="img fifa-18" src="img/game-screenshot-placeholder.png" alt="#" title="#" />
-                <img class="img asterix-game" src="img/game-screenshot-placeholder.png"  alt="#" title="#" />
-                <img class="img mario-kart-duluxe" src="img/game-screenshot-placeholder.png" alt="#" title="#" />
-                <img class="img sonic-mania" src="img/game-screenshot-placeholder.png" alt="#" title="#" />
-                <img class="img football-manager" src="img/game-screenshot-placeholder.png" alt="#" title="#" />
+                <img class="img fifa-twenty" src="img/game-screenshot-placeholder.png" alt="#" title="#" />            
            
             </article> 
 

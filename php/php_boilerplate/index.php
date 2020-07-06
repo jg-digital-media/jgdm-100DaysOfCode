3bd7ba8c3fa6a165e526d23b4a2277dbde24e2a1
Jonathan Grieve <mail@jonniegrieve.co.uk>
2020-07-06 15:53:45 +0100

    new php boilerplate project

---
+++ php/php_boilerplate/index.php
@@ -0,0 +1,56 @@
+<?php  ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Jonathan Grieve | Amateur Photography</title>
+
+    <!-- Styles  -->
+    <link rel="stylesheet" type="text/css" href ="styles/style.css" />
+
+    <!-- Slick Styling-->
+    <link rel="stylesheet" type="text/css" href ="assets/slick.css" />
+    <link rel="stylesheet" type="text/css" href ="assets/slick/slick-theme.css" />
+
+    <!-- Lightbox-->
+    <link href="assets/lightbox/dist/css/lightbox.css" rel="stylesheet" />
+
+    <!-- Google Font-->
+    <link href="https://fonts.googleapis.com/css?family=Merriweather|Odibee+Sans|Quicksand&display=swap" rel="stylesheet">
+
+    <!-- Favicon -->
+    <link rel="icon" href="favicon.png" type="image/png">
+
+    <!-- meta tags -->
+    <meta name="description" content="My name is Jonathan and this is my website for my amateur photography. I hope you enjoy my photos!">
+    <meta name="keywords" content="photography, website, pets, animals and nature, buildings, people, others"> 
+    <meta name="image" content="https://projects.jonniegrieve.co.uk/photography/boo-preview.png">
+
+    <!-- FACEBOOK: Open Graph -->
+    <meta property="og:title" content="Jonathan Grieve | Amateur Photography">
+    <meta property="og:description" content="My name is Jonathan and this is my website for my amateur photography.  I hope you enjoy my photos!">
+    <meta property="og:image" content="https://projects.jonniegrieve.co.uk/photography/boo-preview.png">
+    <meta property="og:url" content="https://projects.jonniegrieve.co.uk/photography/index.html">
+
+    <!-- TWITTER: Open Graph -->
+    <meta name="twitter:title" content="Jonathan Grieve | Amateur Photography">
+    <meta name="twitter:description" content="My name is Jonathan and this is my website for my amateur photography.  I hope you enjoy my photos!">
+    <meta name="twitter:image" content="https://projects.jonniegrieve.co.uk/photography/boo-preview.png">
+    <meta name="twitter:card" content="jonnie_grieve_photography">
+
+    <!-- Canonical link -->
+    <link rel="canonical" href="https://projects.jonniegrieve.co.uk/photography/main.html">
+
+</head>
+        <body>
+
+        <h1>PHP Boilerplate</h2>
+
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor odit qui ducimus adipisci ratione consequatur officiis, hic pariatur mollitia voluptatibus soluta deserunt eveniet sed delectus nihil dignissimos eligendi iste. Dolor!</p>
+            
+</body>
+</html>

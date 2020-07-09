86842076bd5932b828a1c170bfd6413559e28ec6
Jonathan Grieve <mail@jonniegrieve.co.uk>
2020-07-09 09:55:02 +0100

    log text based on checkbox click

--- laravel/switch_photos/public/js/app.js
+++ laravel/switch_photos/public/js/app.js
@@ -1,5 +1,5 @@
 /**
- * Date: 07/07/2020
+ * Date: 09/07/2020
  * Authored by: @jg_digitalMedia
  * 
  * Links:
@@ -44,100 +44,16 @@ jQuery.getJSON("data/all-photos-classes.json", function( catOne_data ) {
 
 function toggleImage(category) {
     
-    var game_cat_botw = document.querySelectorAll(".zelda_botw");
+    game_cat_botw = document.querySelectorAll(".zelda_botw");
 
-    jQuery.getJSON("data/all-photos-classes.json", function( catOne_data ) {
+    if( toggleZeldaBreathOfWild.checked == true ) {
 
-        if( toggleZeldaBreathOfWild.checked == true ) {
-            //game_cat_botw.classList.add("zelda_botw");
-            game_cat_botw.style.display = "inline-block";
+        console.log("images on");
 
-        }
+    } else {
 
-    });
-
-}
-
-
-//Event Listeners
-/* game_cat_botw.addEventListener("click", function(e) {
-
-    if( toggleZeldaBreathOfWild.checked == false ) {
-
-        toggleZeldaBreathOfWild.checked = true;
-        game_cat_botw.style.display = "inline-block";
-            
-    } else if( toggleZeldaBreathOfWild.checked == true ) {
-
-        toggleZeldaBreathOfWild.checked = false;
-        game_cat_botw.style.display = "none";
+        console.log("images off");
 
     }
 
-}); */
-
-
-
-// functions
-/*  function toggleImage(category) {
-
-    let cat_one_len = category.zelda_botw[i].length;
-
-    for(let i=0; i < cat_one_len; i++); {    
-
-        let game_cat_botw = document.querySelectorAll(".zelda_botw")[0];
-        
-        if (game_cat_botw.style.display[i] === "inline-block") {
-            game_cat_botw.style.display[i] = "none";
-        } else if ( game_cat_botw.style.display[i] = "none" ) {
-            game_cat_botw.style.display[i] = "inline-block";
-        }
-   }
 }
-
-function toggleSecondImage() {
-    if (game_cat_lAwakening.style.display === "inline-block") {
-        game_cat_lAwakening.style.display = "none";
-    } else if ( game_cat_lAwakening.style.display = "none" ) {
-        game_cat_lAwakening.style.display = "inline-block";
-    }
-}
-
-function toggleThirdImage() {
-    if (game_cat_fifaOne.style.display === "inline-block") {
-        game_cat_fifaOne.style.display = "none";
-    } else if ( game_cat_fifaOne.style.display = "none" ) {
-        game_cat_fifaOne.style.display = "inline-block";
-     }
-}
-
-*/
-
-
-/**
- * code
- * 
- 
-catOne_data.zelda_botw.length
-
-// conditional logic here.. access element
-                if ( element.style.display === "inline-block" ) {
-                    element.style.display = "none";
-
-                } else if ( element.style.display === "none" ) {
-                    element.style.display = "inline-block";
-                                    
-                    jQuery(`<img class="img ${ catOne_data.zelda_botw[i].img_class }" src="${ catOne_data.zelda_botw[i].img_url }" alt="${ catOne_data.zelda_botw[i].img_alt }" title="${ catOne_data.zelda_botw[i].img_alt }" style="display: inline-block;" /> `).appendTo('.category-photos');
-
-} 
-
-//console.log("Iterate: " + "\n")
-            /* if( game_cat_botw.style.display === "inline-block" ) {
-
-                game_cat_botw.style.display = "none";
-
-            } else if( game_cat_botw.style.display === "none" ) {
-
-                game_cat_botw.style.display = "inline-block";
-                jQuery(`<img class="img ${ catOne_data.zelda_botw[i].img_class }" src="${ catOne_data.zelda_botw[i].img_url }" alt="${ catOne_data.zelda_botw[i].img_alt }" title="${ catOne_data.zelda_botw[i].img_alt }" />"`).appendTo(".category-photos");
-            */

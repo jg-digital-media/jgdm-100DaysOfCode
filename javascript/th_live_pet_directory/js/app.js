6b3f4c45d6f3cb099aec9e92d6d15ddb60e6cfaf
Jonathan Grieve <mail@jonniegrieve.co.uk>
2020-07-10 15:11:01 +0100

    functional pet directory

--- javascript/th_live_pet_directory/js/app.js
+++ javascript/th_live_pet_directory/js/app.js
@@ -1,13 +1,54 @@
 const petList = document.querySelector('#pets');
 const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+const btn = document.querySelector('#btn-remove');
+const form = document.querySelector('form');
+
+let html = "";
 
 // TODO 1: Insert pet data into the page when it loads
+pets.forEach( pet => {
+    html += `    
+        <li>
+            ${ pet.name } the ${ pet.breed }
+                <img src=img/${ pet.photo } alt=${ pet.breed }
+            <input type="checkbox">
+        </li>
+    `;
+});
 
 
 // TODO 2: Remove all selected pets upon clicking the "remove" button
+btn.addEventListener('click', () => {
+    //contains a collection of child elements
+    const pets = petList.children;
+
+    //for of loop  - loop through a pet of pet list
+    for ( let pet of pets) {
+        if( pet.classList.contains('selected') ) {
+        pet.remove();        
+        }
+    }
+});
 
 
 // TODO 3: Create and insert pets on form submit
+function createPet() {
+    const petName = document.querySelector( '#pet-name' );
+    const petPhoto = document.querySelector( '#pet-photo' );
+
+    //create petList item
+    const newPet = `
+        <li>
+            ${ petName.value }
+            <img src="img/${ petPhoto.value } alt=${ petName.value }>
+            <input type="checkbox">
+        </li>
+    `;
+    //insert content into DOM
+    petList.insertAdjacentHTML( 'beforeend', newPet );
+    
+    
+}
 
 
 // Use event delegation to handle all checkbox changes from an ancestor node
@@ -18,6 +59,7 @@ petList.addEventListener('change', (e) => {
   }
 });
 
+
 // Show/hide a pet image within the pet list
 petList.addEventListener('click', (e) => {
   let target = e.target;
@@ -25,4 +67,5 @@ petList.addEventListener('click', (e) => {
   if (target.tagName === 'LI') {
     petImg.classList.toggle('show');
   }
+
 });

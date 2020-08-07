2c94acb4cadcdbe02f364b2483733ce670d44f47
jg-digital-media <mail@jonniegrieve.co.uk>
2020-08-07 16:47:32 +0100

    fetch request specific user

--- css/landing/fetch/photo_api/photo.js
+++ css/landing/fetch/photo_api/photo.js
@@ -1,5 +1,27 @@
 console.log("photo.js connected");
 
-fetch()
-    .then()
-    .then()
+const photos_list = document.getElementById("photos-list");
+
+function fetchData(url) {
+    return fetch(url)
+           .then(res => res.json())
+}
+
+
+//fetch request - list of names
+fetchData( 'https://api.unsplash.com/users/webdesignerjon/photos' )
+    .then(data => getPhotos(data.message)), {
+        mode: "no-cors"
+    };
+
+    //display photos to browser.
+function getPhotos(data) {
+  
+    const print_names = data.map( item  => `
+          
+          <li>${item}</li>
+          
+    `).join("");
+
+    list.innerHTML = print_names;
+};

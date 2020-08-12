3e2e5a75100bbb7f609b09835fa4f95db29e3ab3
jg-digital-media <mail@jonniegrieve.co.uk>
2020-08-12 16:11:25 +0100

    fillable file references

--- laravel/photos_restapi/app/Owner.php
+++ laravel/photos_restapi/app/Owner.php
@@ -6,8 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Owner extends Model
 {
-    //define relationships
     protected $table = 'owner';
+
+    protected $fillable = [
+        'name',
+        'copyright'
+    ];
+    
+    //define relationships
     public function photos() {
         return $this->hasMany(Photos::class);    
     }

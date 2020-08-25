2b66cfa0ba0e6e82171ecf2cc34fd910e02ad9d2
jg-digital-media <mail@jonniegrieve.co.uk>
2020-08-25 16:42:47 +0100

    search photos method returns okay 200

--- api/basic/app.js
+++ api/basic/app.js
@@ -5,17 +5,38 @@ console.log("js connected!")
 Client ID's  
 
 #1:  c1e47c9589dd4c170d631aabd17cb23af8ec8451cf1c2f5e50af82e4e59f417
-
+#2:  zkiYj0naBQBgRYCTJwxBDiFic1xncn-b70hftLJVzn8
 */
 
 
+function get_collection_data() {
+
+    let client_id = "zkiYj0naBQBgRYCTJwxBDiFic1xncn-b70hftLJVzn8";    
+    let username = document.getElementById("search").value;
+    
+    let url = "https://api.unsplash.com/users/:" + 
+    username +
+    "/collections";
+
+    fetch(url)
+        .then(function (data) {
+              console.log(data);
+    });
+    console.log("url retrieved: " + url )
+
+    ///users/:username/collections
+}
+
 function search_photos() {
-    let client_id = "c1e47c9589dd4c170d631aabd17cb23af8ec8451cf1c2f5e50af82e4e59f417";
+    let client_id = "zkiYj0naBQBgRYCTJwxBDiFic1xncn-b70hftLJVzn8";
     let query = document.getElementById("search").value;
-    let url = "https://api.unsplash.con/search/photos/?client_id=" +
+
+
+    let url = "https://api.unsplash.com/search/photos/?client_id=" +
     client_id +
     "&query=" +
     query;
+    console.log(url);
 
     //make a request to the api
     

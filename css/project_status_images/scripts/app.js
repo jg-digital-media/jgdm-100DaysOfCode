403a94ff1ebaa22a9688c3fe28c6b349ae627ae5
jg-digital-media <mail@jonniegrieve.co.uk>
2020-09-25 13:37:07 +0100

    new data retrieval markup with jquery

--- css/project_status_images/scripts/app.js
+++ css/project_status_images/scripts/app.js
@@ -17,23 +17,17 @@ jQuery.getJSON('data/project-list.json', function(photoData) {
 
     for (let i=0; i < itemAll; i++) { 
         
-        //jQuery(`<a href="${photoData.projects[i].project_url}" target="blank"><img src="${photoData.projects[i].img_url}" class="site-images" alt="${photoData.projects[i].project_alt}" title="${photoData.projects[i].project_alt}" tabindex="" /></a>`).appendTo('.project-status-data');
+        jQuery(`
+            <li>
+                <a href="${photoData.projects[i].project_url}" target="blank">
+                   <img src="${photoData.projects[i].img_url}" class="site-images" alt="${photoData.projects[i].project_alt}" title="${photoData.projects[i].project_alt}" tabindex="" />
+                   
+                </a> 
 
-        if(photoData.projects[i].status === true) {
-            jQuery(`<li> <span class="complete"> &nbsp; </span>  </li>`).appendTo('#project-status');
-
-        } else if (photoData.projects[i].status === false) {
-            jQuery(`<li> <span class="incomplete"> &nbsp; </span> </li>`).appendTo('#project-status');
-
-        } else if (photoData.projects[i].status === "upcoming") {
-            jQuery(`<li> <span class="upcoming"> &nbsp; </span> </li>`).appendTo('#project-status');
-
-        } else {
-            jQuery(`<li> <span class="under-review"> &nbsp; </span> </li>`).appendTo('#project-status');
-        }
-
-        jQuery(`<li> ${photoData.projects[i].project_name} <strong>(${photoData.projects[i].project_type})</strong> </li>`).appendTo('#project-names');
-
-    }
-    
+                <div class="project_info">
+                    <span class="${photoData.projects[i].project_status }"></span>
+                    <div id="project_name">${photoData.projects[i].project_name }</div>
+                </div>
+           </li>`).appendTo('.project-status-data');     
+    }   
 });

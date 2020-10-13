e329f6170c9a53896fbfe4d28bae31fe56a0bdad
Jonathan Grieve <mail@jonniegrieve.co.uk>
2020-10-13 15:55:58 +0100

    update footer timestamp

--- php/php_website/inc/footer.php
+++ php/php_website/inc/footer.php
@@ -1,6 +1,6 @@
 
     <footer>
-        <div>&copy; 2019 TV Lib by <a href="https://www.jonniegrieve.co.uk" target="blank" class="footer-link">Jonnie Grieve Digital Media</a></div>
+        <div>&copy; 2020 TV Lib by <a href="https://www.jonniegrieve.co.uk" target="blank" class="footer-link">Jonnie Grieve Digital Media</a></div>
     </footer>
 
 

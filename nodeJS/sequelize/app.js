9e688750be73cd4dd711f0209718d338eebb1805
jg-digital-media <mail@jonniegrieve.co.uk>
2020-11-04 10:10:56 +0000

    test a database connection

--- nodeJS/sequelize/app.js
+++ nodeJS/sequelize/app.js
@@ -1,1 +1,26 @@
 console.log("app.js");
+
+//import sequelize
+const Sequelize = require('sequelize');
+
+//sequelize constructor function
+const sequelize = new Sequelize({
+
+    dialect: 'sqlite',
+    storage: 'filename.db'
+
+});
+
+// async IIFE - connect to database
+(async () => {
+
+    try {  
+        await sequelize.authenticate();
+        console.log("Connection to the database successful");
+
+     } catch ( error ) { 
+         
+        console.error("Error Connecting to the Database: ", error);
+     }
+   
+    }) ();

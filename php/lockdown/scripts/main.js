d12e3c3ad813b0eb6b4f0063cb988d07e38b698d
jg-digital-media <mail@jonniegrieve.co.uk>
2020-11-18 14:58:47 +0000

    make slider draggable

--- php/lockdown/scripts/main.js
+++ php/lockdown/scripts/main.js
@@ -9,8 +9,8 @@ $(document).ready(function(){
         infinite: true,
         slidesToShow: 8,
         slidesToScroll: 5,
-        dots: true,
-        draggable: false
+        dots: false,
+        draggable: true
     });
 
 });

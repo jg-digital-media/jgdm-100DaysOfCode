1b0e577175ebf059b54626b83b3df4b468096dba
jg-digital-media <mail@jonniegrieve.co.uk>
2021-01-18 15:13:48 +0000

    test database connection

--- nodeJS/sequelize_relationships/app.js
+++ nodeJS/sequelize_relationships/app.js
@@ -1,5 +1,8 @@
 'use strict';
 
+
+// declare sequelize and models variables and initialize them to the 
+// sequelize and models objects imported from the ./db module
 const { sequelize, models } = require('./db');
 
 // Get references to our models.
@@ -19,15 +22,19 @@ let theIncredibles;
 
 console.log('Testing the connection to the database...');
 
+
+//async/await syntax for DB Testing
 (async () => {
   try {
     // Test the connection to the database
+    await sequelize.authenticate();
     console.log('Connection to the database successful!');
 
     // Sync the models
     console.log('Synchronizing the models with the database...');
 
     // Add People to the Database
+    await sequelize.sync({ force: true });
     console.log('Adding people to the database...');
   
     // Update the global variables for the people instances

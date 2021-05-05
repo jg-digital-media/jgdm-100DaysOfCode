edd5d5f44871f085d473fc0c1363c16395285921
Jonathan Grieve <mail@jonniegrieve.co.uk>
2021-05-05 11:04:54 +0100

    simulate car run

--- python/oop/classes/Car_instance.py
+++ python/oop/classes/Car_instance.py
@@ -3,15 +3,61 @@ class Car:
     wheels = 4
     doors = 2
     engine = True
+    is_moving = False
+    gas = 150
 
     def __init__(self, model, year, make="Ford"):
         self.make = make
         self.model = model
         self.year = year
 
+    def stop(self):
+        if self.is_moving:
+            print('string car stopped')
+            self.is_moving = False
+        else:
+            print('string car already stopped')
+
+    def go(self, speed):
+
+        if self.use_gas():
+            if not self.is_moving:
+                print("string car moving")
+                self.is_moving = True
+            print('The car is going ', speed )
+        else:
+            print('You have run out of gas')
+            self.stop()
+
+    def use_gas(self):
+        self.gas -= 50
+
+        if self.gas <= 0:
+            return False
+        else: 
+            return True
+
 car_one = Car("Model T", 1908)
 car_two = Car("Phantom", 2020, "Rolls Royce")
 
+
+#print values
+print( car_one.doors ) # class attributes
+print( car_two.doors ) # class attributes
+
+car_one.doors = 4  # change value of doors attribute in one instance
+Car.doors = 6  # changes value of doors attribute in every instance
+
+print( car_one.doors ) # class attributes
+print( car_one.doors ) # class attributes
+print( Car.doors )
+
+car_one.doors = 6  # instance class change
+Car.doors = 3    # class change
+
+print( car_one.doors )
+print( car_one.doors )
+print( Car.doors ) 
 
 #instance attributes are accessed by Dot notation.  They can only be accessed on an instance
 
@@ -23,13 +69,26 @@ car_one.year = 2015
 print(car_one.year)
 print(car_two.year)
 
-# ------> Default attributes
 
-# best placed at the end of instance attribute list
+
+car_one.go(44)
+print(car_one.gas)
+car_one.stop()
+
+car_one.go(34)
+print(car_one.gas)
+car_one.stop()
+
+
+car_one.go(3)
+
+
 
 
 """
+# ------> Default attributes
 
+# best placed at the end of instance attribute list
 
     def __init__(self, make, model, year):
         self.make = make

d6e687774aaf5662da062c6a590813c8b9fc3eea
Jonathan Grieve <mail@jonniegrieve.co.uk>
2021-05-07 16:54:16 +0100

    test cards class

--- python/oop/classes/memory_game/Cards.py
+++ python/oop/classes/memory_game/Cards.py
@@ -1,2 +1,23 @@
-class Cards: 
-    pass
+class Card:
+    def __init__(self, word, location):
+        self.word = word  
+        self.location = location
+        self.matched = False
+
+    def __eq__(self, other):
+        return self.word == other.word
+
+    def __str__(self):
+        return self.word
+
+
+if __name__ == '__main__':
+    card1 = Card('egg', 'A1')
+    card2 = Card('egg', 'B1')
+    card3 = Card('hut', 'D4')
+
+print(card1 == card2)
+print(card1 == card3)
+print(card2 == card3)
+print(card1 == card2)
+print(card1)

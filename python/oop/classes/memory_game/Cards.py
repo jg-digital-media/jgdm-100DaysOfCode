30095489c569878f407a4754d32690c5d5c1aa19
Jonathan Grieve <mail@jonniegrieve.co.uk>
2021-05-12 14:29:52 +0100

    fix print method indentation

--- python/oop/classes/memory_game/Cards.py
+++ python/oop/classes/memory_game/Cards.py
@@ -1,14 +1,14 @@
 class Card:
-    def __init__(self, word, location):
-        self.word = word  
+    def __init__(self, card, location):
+        self.card = card  
         self.location = location
         self.matched = False
 
     def __eq__(self, other):
-        return self.word == other.word
+        return self.card == other.card
 
     def __str__(self):
-        return self.word
+        return self.card
 
 
 if __name__ == '__main__':
@@ -16,9 +16,8 @@ if __name__ == '__main__':
     card2 = Card('egg', 'B1')
     card3 = Card('hut', 'D4')
 
-""" print(card1 == card2)
-print(card1 == card3)
-print(card2 == card3)
-print(card1 == card2)
-print(card1)
- """
+    print(card1 == card2)
+    print(card1 == card3)
+    print(card2 == card3)
+    print(card1 == card2)
+    print(card1)

e9df38f9ded56b6f9351db7f33b7aa36b5770953
Jonathan Grieve <mail@jonniegrieve.co.uk>
2021-05-13 16:27:13 +0100

    Add remaining methods to the game

--- python/oop/classes/memory_game/Game.py
+++ python/oop/classes/memory_game/Game.py
@@ -32,7 +32,7 @@ class Game:
         row = []
         for column in self.columns:
             for card in self.cards:
-                if card.location == "{} {}".format(column, num):
+                if card.location == "{}, {}".format(column, num):
                     if card.matched:
                         row.append(str(card))
                     else:
@@ -40,7 +40,7 @@ class Game:
         return row
 
 
-    """ def create_grid(self):
+    def create_grid(self):
 
         # | A | B | C | D |
         header = ' | ' + ' | '.join(self.columns) + ' |'
@@ -50,21 +50,67 @@ class Game:
             print_row = "{}| ".format(row)        
             get_row = self.create_row(row)
             print_row += ' | '.join(get_row) + ' |'
-            print(print_row) """
+            print(print_row)
     
+
+    def check_match(self, loc1, loc2):
+        cards = []
+
+        for card in self.cards:
+            if card.location == loc1 or card.location == loc2:
+                cards.append(card)
+            if cards[0] == cards[1]:
+                cards[0].matched = True
+                cards[1].matched = True
+                return True
+            else:
+                for card in cards:
+                    print("{}:{}".format(card.location, card))
+                return False
+
+
+    def check_win(self):
+        for card in self.cards:
+            if card.matched == False:
+                return False
+            return True
+
+
+    def check_location(self, time):
+        while True:
+            guess = input("What's the location of your {} card? ".format(time))
+            if guess.upper() in self.locations:
+                return guess.upper()
+            else:
+                print("Please enter a valid location: E.g. A1")
+
+                
+    """ def start_game(self):
+        game_running = True
+        print('\nMemory Game\n\n')
+        self.set_cards()
+        while game_running:
+            self.create_grid()
+            guess1 = self.check_location('first')
+            guess2 = self.check_location('second')
+            if self.check_match(guess1, guess2):
+                if self.check_win():
+                    print('Congrats! You guessed them all and won the game')
+                    self.create_grid()
+                    game_running = False
+            else:
+                input('Those cards are not a match. Press enter to continue')
+        print('Game over') """
     
 # dunder main
 if __name__ == '__main__':
     game = Game() 
-    game.set_cards()
-    print(game.create_row(1))
-    print(game.create_row(2))
-    print(game.create_row(3))
-    print(game.create_row(4))
+    # game.start_game()
+    game.create_grid()
+
     game.cards[0].matched = True
     game.cards[1].matched = True
     game.cards[2].matched = True
     game.cards[3].matched = True
 
-    # new game grid
-    #game.create_grid()
+    # new game grid    

5600cd52c6572e61f7f0dd75284daf89a6b27313
Jonathan Grieve <mail@jonniegrieve.co.uk>
2021-05-18 12:57:40 +0100

    move to flask basics for separation

--- python/flask/flask_basics/simple_app.py
+++ python/flask/flask_basics/simple_app.py
@@ -11,12 +11,32 @@ app = Flask (__name__)
 def hello(name="Treehouse"):
      return 'Hello {}'.format(name)
 
+
+""" 
+
+    # return a simple route as plain text
+    def add(num1, num2):
+    return '{}+{}={}'.format(num1, num2, num1+num2) """
+
 #add route for a URL calculator
 @app.route('/add/<int:num1>/<int:num2>')
 @app.route('/add/<float:num1>/<float:num2>')
 @app.route('/add/<int:num1>/<float:num2>')
 @app.route('/add/<float:num1>/<int:num2>')
+
+# return a html string
 def add(num1, num2):
-    return '{}+{}={}'.format(num1, num2, num1+num2)
+    return """ 
+    
+    <!doctype html>
+        <head>
+            <title>Python Flask</title>
+        </head>
+
+        <body> <h1>{} * {} = {}</h1>
+        </body>
+    </html>
+    
+    """.format(num1, num2, num1+num2)
 
 app.run(debug=True, port=8000, host='0.0.0.0')

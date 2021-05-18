f47042b4b3c9ae4d157c5ab89866b0180c0ea4a5
Jonathan Grieve <mail@jonniegrieve.co.uk>
2021-05-18 10:19:15 +0100

    simple app multiple routes

---
+++ python/flask/flask_project/simple_app.py
@@ -0,0 +1,22 @@
+# import flask class
+from flask import Flask
+
+
+# use whatever the current namespace is as instance name
+app = Flask (__name__)  
+
+# multi routes
+@app.route('/')
+@app.route('/<name>')
+def hello(name="Treehouse"):
+     return 'Hello {}'.format(name)
+
+#add route for a URL calculator
+@app.route('/add/<int:num1>/<int:num2>')
+@app.route('/add/<float:num1>/<float:num2>')
+@app.route('/add/<int:num1>/<float:num2>')
+@app.route('/add/<float:num1>/<int:num2>')
+def add(num1, num2):
+    return '{}+{}={}'.format(num1, num2, num1+num2)
+
+app.run(debug=True, port=8000, host='0.0.0.0')

307d99da619d979931929d85c0da68a443b23867
Jonathan Grieve <mail@jonniegrieve.co.uk>
2021-05-18 10:19:45 +0100

    named routes

--- python/flask/flask_project/app.py
+++ python/flask/flask_project/app.py
@@ -5,9 +5,10 @@ from flask import Flask
 
 app = Flask (__name__)  # use whatever the current namespace is
 
-
-@app.route("/")
-def index():
-    return "Hello, World!"
+# multi routes
+@app.route('/')
+@app.route('/<name>')
+def hello(name="Treehouse"):
+     return 'Hello {}'.format(name)
 
 app.run(debug=True, port=8000, host='0.0.0.0')

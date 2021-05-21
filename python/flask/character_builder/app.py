f79d7b323cf9078d7990c7fd5475e144305ecadd
jg-digital-media <mail@jonniegrieve.co.uk>
2021-05-21 21:26:18 +0100

    app entry point

--- python/flask/character_builder/app.py
+++ python/flask/character_builder/app.py
@@ -1,1 +1,10 @@
-# char
+# imports
+from flask import Flask
+
+# Run the Flask App.
+app = Flask(__name__)
+
+
+
+# run local server in browser and debugging tools
+app.run(debug=True, host='0.0.0.0', port=8000)

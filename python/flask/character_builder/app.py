a54166727ef44f75729dc85ea631bf9369cf1f2b
jg-digital-media <mail@jonniegrieve.co.uk>
2021-05-22 15:27:09 +0100

    simulate saving data via a route

--- python/flask/character_builder/app.py
+++ python/flask/character_builder/app.py
@@ -11,5 +11,12 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+# Save data to save route with POST 
+
+@app.route('/save', methods=["POST"])
+def save():
+    return "Saved!"
+
+
 # run local server in browser and debugging tools
 app.run(debug=True, host='0.0.0.0', port=8000)

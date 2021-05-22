8c060782344810fed15841ec071f4d02b1df9339
jg-digital-media <mail@jonniegrieve.co.uk>
2021-05-22 16:33:45 +0100

    work with cookie data

--- python/flask/character_builder/app.py
+++ python/flask/character_builder/app.py
@@ -1,21 +1,40 @@
 # imports
-from flask import Flask, render_template, redirect, url_for, request
+import json
+
+from flask import (Flask, render_template, redirect, 
+url_for, request, make_response)
 
 # Run the Flask App.
 app = Flask(__name__)
+
+
+# Save cookie Data
+
+def get_saved_data():
+    try:
+        data = json.loads(request.cookies.get('bear_character'))
+
+    except TypeError:
+        data = {}
+
+    return data
 
 # Index route
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+
+    # call the get_saved_data function to retrieve data
+    data = get_saved_data()
+    return render_template("index.html", saves=data)
 
 # Save data to save route with POST 
 
 @app.route('/save', methods=["POST"])
 def save():
-    import pdb; pdb.set_trace()
-    return redirect( url_for("index") )
+    response = make_response(redirect(url_for("index")))
+    response.set_cookie("bear_character", json.dumps(dict(request.form.items())))
+    return response
 
 
 # run local server in browser and debugging tools

81d3d12c90fec9923bfc25695a559d4fa4743cae
jg-digital-media <mail@jonniegrieve.co.uk>
2021-05-22 15:17:24 +0100

    render home template

--- python/flask/character_builder/app.py
+++ python/flask/character_builder/app.py
@@ -1,10 +1,15 @@
 # imports
 from flask import Flask
+from flask import render_template
 
 # Run the Flask App.
 app = Flask(__name__)
 
+# Index route
 
+@app.route('/')
+def index():
+    return render_template("index.html")
 
 # run local server in browser and debugging tools
 app.run(debug=True, host='0.0.0.0', port=8000)

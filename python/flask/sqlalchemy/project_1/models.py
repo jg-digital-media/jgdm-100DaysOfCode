23479fc3337c00b4c3f9f5923d78d0f05a58ff32
Jonathan Grieve <mail@jonniegrieve.co.uk>
2021-05-28 15:45:24 +0100

    create first model users

--- python/flask/sqlalchemy/project_1/models.py
+++ python/flask/sqlalchemy/project_1/models.py
@@ -1,1 +1,33 @@
 # models.py
+
+from sqlalchemy import create_engine, Column, Integer, String
+
+from sqlalchemy.ext.declarative import declarative_base
+
+# create db
+engine = create_engine('sqlite:///users.db', echo=True)
+
+# This base, maps our model(s) to the database.
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    fullname = Column(String)
+    nickname = Column(String)
+
+    def __repr__(self):
+        return f'<User(name={self.name}, fullname={self.fullname}, nickname="{self.nickname})>'
+
+# -----> Create the Table
+
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
+
+
+# Run the file to create the DB
+
+

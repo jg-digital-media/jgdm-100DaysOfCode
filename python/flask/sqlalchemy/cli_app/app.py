9176143f21c4d4cdb26304e4dbfa5b415aa0b85d
jg-digital-media <mail@jonniegrieve.co.uk>
2021-06-18 15:20:52 +0100

    first time db creation

--- python/flask/sqlalchemy/cli_app/app.py
+++ python/flask/sqlalchemy/cli_app/app.py
@@ -1,4 +1,13 @@
 # import models
+from models import (Base, session, Media, engine)
+
+
+# Create the Database
+if __name__  == '__main__':
+   Base.metadata.create_all(engine)
+
+
+
 # main menu - add, search, analysis, exit, view
 # add books to the database 
 # edit books
--- python/flask/sqlalchemy/cli_app/models.py
+++ python/flask/sqlalchemy/cli_app/models.py
@@ -1,3 +1,33 @@
-# create a database
-# create a model
-# title, author, date published, price
+## create a database
+
+# Declare the imports
+from sqlalchemy import (create_engine, Column, Integer, String, Date)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+## create a model
+
+# Setup DB for creation
+engine = create_engine('sqlite:///media.db', echo=False)
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+
+# Setup schema  media_ title, author, date published, price
+
+class Media(Base):
+    __tablename__ = 'media'
+    
+    id= Column(Integer, primary_key=True)
+    media_title = Column('Media Title', String)
+    media_type = Column('Media Type', String)
+    author = Column('Author/Creator', String)
+    published_date = Column('Published', Date)
+    price = Column('Price', Integer)
+
+    # Display string of data in console - formatted string
+    def __repr__(self):
+        return f'Title: {self.title} Media Title: {self.title} Media Type : {self.title} Author: {self.title} Published Date: {self.title} Price: {self.price}' 
+

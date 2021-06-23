c637fc3c5fc8dbaeb79728806eeca12ac06cbc0e
Jonathan Grieve <mail@jonniegrieve.co.uk>
2021-06-23 14:25:26 +0100

    wrap up main menu error handler

--- python/flask/sqlalchemy/cli_app/app.py
+++ python/flask/sqlalchemy/cli_app/app.py
@@ -6,14 +6,26 @@ from models import (Base, session, Media, engine)
 def menu():
     while True:
         print('''
-        MEDIA LIBRARY MENU
-        1) Add Itrm
-        2) View all books
-        3) Search item
-        4) Media item
-        5) Exit the Application ''')
+            \nMEDIA LIBRARY MENU
+            \r1) Add Itrm
+            \r2) View all books
+            \r3) Search item
+            \r4) Media item
+            \r5) Exit the Application ''') 
 
-        input('What would you like to do? ')
+        # store a variable for the menu coice
+        choice = input('What would you like to do? ')
+        if choice in ['1','2','3','4','5']:
+            return choice
+        else:
+            input('''
+                    \rError: please try one of the choices above
+                    \rA number from 1 to 5.
+                    \rPress Enter to Try Again.''')
+
+
+
+
 
 # Create the Database
 if __name__  == '__main__':

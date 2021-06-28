1614fe62fa093f4430e96e3b86aca1ebd015dd48
Jonathan Grieve <mail@jonniegrieve.co.uk>
2021-06-28 14:40:31 +0100

    minor edits problems with smart nav scroll

--- css/experiments/6/app.js
+++ css/experiments/6/app.js
@@ -1,33 +1,34 @@
+const nav = document.querySelector('nav');
+
 
 //sets a default scroll position
 let previousScrollPosition = 0;
 
 const isScrollingDown = () => {
+  let scrolledPosition = supportPageOffset
+    ? window.pageYOffset
+    : isCSS1Compat
+    ? document.documentElement.scrollTop
+    : document.body.scrollTop;
+  let isScrollDown;
 
-    // detched scrolling activity with vanilla js
-
-    let currentScrolledPosition = window.scrollY || window.pageYOffset;
-    let scrollingDown;  
-  
-    if (currentScrolledPosition > previousScrollPosition) {
-        scrollingDown = true;
-    } else {
-        scrollingDown = false;
-    }
-
-    previousScrollPosition = currentScrolledPosition;
-    return scrollingDown; 
-  };
-
-  const nav = document.querySelector('nav');
-  
-
-  const handleNavScroll = () => {
-    if (isScrollingDown()) {
-      nav.classList.add('scroll-down');
-      nav.classList.remove('scroll-up')
-    } else {
-      nav.classList.add('scroll-up');
-      nav.classList.remove('scroll-down')
-    }
+  if (scrolledPosition > previousScrollPosition) {
+    isScrollDown = true;
+  } else {
+    isScrollDown = false;
   }
+  previousScrollPosition = scrolledPosition;
+  return isScrollDown;
+};
+
+
+
+const handleNavScroll = () => {
+  if (isScrollingDown()) {
+    nav.classList.add('scroll-down');
+    nav.classList.remove('scroll-up')
+  } else {
+    nav.classList.add('scroll-up');
+    nav.classList.remove('scroll-down')
+  }
+}

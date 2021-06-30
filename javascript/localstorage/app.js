83d6d1bb4234db99a69595f010bc7d023f3eed8d
Jonathan Grieve <mail@jonniegrieve.co.uk>
2021-06-30 15:10:17 +0100

    using local storage example

---
+++ javascript/localstorage/app.js
@@ -0,0 +1,13 @@
+console.log("using localstorage");
+
+
+// Store something in local storage - setItem
+// localStorage.setItem('keyName', 'keyValue')
+localStorage.setItem('name', 'Jack');
+
+// retrieve a value from localstorage
+localStorage.getItem('name');
+
+// remove an item from localstorae
+removeItem('name') 
+

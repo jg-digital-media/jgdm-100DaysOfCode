7fc78404876ed632c33828041f5816414bf26b9c
Jonathan Grieve <mail@jonniegrieve.co.uk>
2021-07-12 16:30:26 +0100

    operate the application menu

--- python/flask/sqlalchemy/cli_app/app.py
+++ python/flask/sqlalchemy/cli_app/app.py
@@ -2,7 +2,7 @@
 from models import (Base, session, Media, engine)
 
 
-
+# To display the menu options to the console
 def menu():
     while True:
         print('''
@@ -19,14 +19,45 @@ def menu():
             return choice
         else:
             input('''
-                    \rError: please try one of the choices above
+                    \rError: Please try again with one of the choices above
                     \rA number from 1 to 5.
                     \rPress Enter to Try Again. ''')
+
+
+# %o keep the application running until user exit
+def app():
+
+    app_running = True
+
+    while app_running:
+
+        choice = menu()
+
+        if choice == '1':
+            #add menu item
+            pass
+
+        elif choice == '2':
+            # view book 
+            pass
+
+        elif choice == '3':
+            # view book 
+            pass
+
+        elif choice == '4':
+            # analyse - media item details
+            pass
+
+        else:             
+            # default option to exit the application 
+            print('\n\nGOODBYE')
+            app_running = False
 
 # Create the Database
 if __name__  == '__main__':
     Base.metadata.create_all(engine)
     
     # call app functions
-    menu()
+    app()
 

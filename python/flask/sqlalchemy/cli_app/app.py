8026f8d116aecc26286ef9114ad033e547f177ae
Jonathan Grieve <mail@jonniegrieve.co.uk>
2021-07-13 12:35:38 +0100

    test date string on function call

--- python/flask/sqlalchemy/cli_app/app.py
+++ python/flask/sqlalchemy/cli_app/app.py
@@ -27,8 +27,45 @@ def menu():
                     \rA number from 1 to 5.
                     \rPress Enter to Try Again. ''')
 
+def clean_date(date_str):
 
-# %o keep the application running until user exit
+    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+    # split and convert date format into usable data
+    split_date = date_str.split(" ")
+    print(split_date)
+
+
+    """  month = int(months.index(split_date[0]) + 1)
+    day = int(split_date[1].split('')[0])
+    year = int(split_date[2])
+    print(day) """
+
+def clean_price():
+    pass
+
+
+# import 
+def import_csv():
+    with open('media_list.csv') as csvfile:
+        data = csv.reader(csvfile)
+
+        # display data in the console
+        for row in data:
+            print(row)
+            """ media_title = row[0]
+            media_type = row[1]
+            artist = row[2]
+            genre = row[3]
+            # published_date = clean_date(row[4])
+            # price = clean_price(row[5])
+
+            
+            new_media = Media(Title=media_title, Type=media_type, Artist=artist, Genre=genre, Date=published_date, Price=price)
+            session.add(new_media) """
+        session.commit()
+
+# To keep the application running until user exit
 def app():
 
     app_running = True
@@ -63,5 +100,7 @@ if __name__  == '__main__':
     Base.metadata.create_all(engine)
     
     # call app functions
-    app()
+    clean_date("October 23, 2021")
+    # import_csv()
+    # app()
 

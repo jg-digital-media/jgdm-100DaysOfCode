48f949874dfa11a7ed77c9562bebb8724fab7e95
jg-digital-media <mail@jonniegrieve.co.uk>
2021-08-18 15:45:32 +0100

    basic flask page

---
+++ python/flask/flask_sql/app.py
@@ -0,0 +1,12 @@
+from flask import Flask 
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "A message from the main app route"
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000, host='127.0.0.1')

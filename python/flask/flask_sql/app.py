e06a4aac07006f65a1d881ca48d2b6351221c05e
jg-digital-media <mail@jonniegrieve.co.uk>
2021-08-18 15:53:01 +0100

    add data route

--- python/flask/flask_sql/app.py
+++ python/flask/flask_sql/app.py
@@ -8,5 +8,9 @@ app = Flask(__name__)
 def index():
     return "A message from the main app route"
 
+@app.route('/data')
+def data():
+    return "This is the data page"
+
 if __name__ == '__main__':
     app.run(debug=True, port=8000, host='127.0.0.1')

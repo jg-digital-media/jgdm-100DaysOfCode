53bc48439075921fe1ce8e8320e29b4903477e20
jg-digital-media <mail@jonniegrieve.co.uk>
2021-08-20 17:26:48 +0100

    add class name

--- javascript/dom/dom_two/app.js
+++ javascript/dom/dom_two/app.js
@@ -6,6 +6,7 @@ btn_update.addEventListener("click", function(){
 
     
     const get_heading = document.querySelector("h2");
+    get_heading.className = "grow";
     const change_text = document.getElementById("js_change_text");
     get_heading.textContent = change_text.value;
     change_text.value = "";

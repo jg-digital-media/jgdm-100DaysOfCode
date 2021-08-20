22dcfae8bc61f23348448c7810b829433fdfce5c
jg-digital-media <mail@jonniegrieve.co.uk>
2021-08-20 16:52:38 +0100

    change text content of heading

---
+++ javascript/dom/dom_two/app.js
@@ -0,0 +1,12 @@
+
+const btn_update = document.getElementById("update_btn");
+
+
+btn_update.addEventListener("click", function(){
+
+    
+    const get_heading = document.querySelector("h2");
+    const change_text = document.getElementById("js_change_text");
+   get_heading.textContent = change_text.value;
+})
+

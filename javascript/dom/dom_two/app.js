ce9a1ec0e5bb6cee097f8f0fff6cefec2754ef3d
jg-digital-media <mail@jonniegrieve.co.uk>
2021-08-20 16:55:41 +0100

    remove text from input on submit

--- javascript/dom/dom_two/app.js
+++ javascript/dom/dom_two/app.js
@@ -7,6 +7,7 @@ btn_update.addEventListener("click", function(){
     
     const get_heading = document.querySelector("h2");
     const change_text = document.getElementById("js_change_text");
-   get_heading.textContent = change_text.value;
+    get_heading.textContent = change_text.value;
+    change_text.value = "";
 })
 

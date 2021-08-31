b874acaf070021dd0c2a1403e39c78b0aa83bc2e
Jonathan Grieve <mail@jonniegrieve.co.uk>
2021-08-31 11:41:29 +0100

    add code file localstorage

---
+++ javascript/dom/dom_two/localstorage.js
@@ -0,0 +1,21 @@
+
+/* getItem() on local storage to hold list data */
+/* function getNewDataItems() {
+  const store_list_data = localStorage.getItem('new_tasks');
+  return [];
+} */
+
+/* inspect JSON data of local storage key value pairs */
+/* function getRecentSearches() {
+  const searches = localStorage.getItem('new_tasks');
+  if (searches) {
+    return JSON.parse(searches);
+  }
+  return [];
+} */
+
+/* clear the local storage data on button click */
+/* clearButton.addEventListener('click', () => {
+  localStorage.removeItem('new_tasks');
+  recentSearchList.innerHTML = '';
+}); */

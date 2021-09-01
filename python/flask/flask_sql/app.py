5cda3fb0429da13a5f5474477f1d538f2ca3ae82
Jonathan Grieve <mail@jonniegrieve.co.uk>
2021-09-01 14:37:31 +0100

    degfine db schema

--- python/flask/flask_sql/app.py
+++ python/flask/flask_sql/app.py
@@ -1,21 +1,19 @@
 from flask import Flask, render_template
 
 
-
-app = Flask(__name__)
+# import Database schema from models.py
+from models import db, Roster, app
 
 @app.route('/')
 def index():
 
-    return render_template("index.html")
-    
+    return render_template("index.html")    
 
 
 @app.route('/two')
 def two():
 
-    return render_template("two.html")
-    
+    return render_template("two.html")    
 
 
 @app.route('/three')
@@ -27,13 +25,14 @@ def three():
 @app.route('/form', methods=["GET", "POST"])
 def main_form():
 
-    return render_template("form.html")
-    
+    return render_template("form.html")    
 
 
 @app.route('/data')
 def data():
     return "This is the data page"
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000, host='127.0.0.1')
---
+++ python/flask/flask_sql/models.py
@@ -0,0 +1,24 @@
+ # Declare imports for the app
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+
+
+# Connect to SQLAlchemy
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/roster.db'
+db = SQLAlchemy(app)
+
+
+# Create Database Model - 3 fields + ID as primary key
+class Roster(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column("Name", db.String(80), unique=True, nullable=False)
+    age = db.Column("Age", db.Integer(120), unique=True, nullable=False)
+    joined = db.Column('Joined', db.DateTime, default=datetime.datetime.now)
+
+    def __repr__(self):
+        return f'''<Roster %r> ({self.name} 
+        Age: {self.age}
+        Joined: {self.joined }
+       '''

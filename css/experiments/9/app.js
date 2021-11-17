b9a8c0d0d3bb41db8496d4266787f07cefcf47f5
Jonathan Grieve <mail@jonniegrieve.co.uk>
2021-11-17 16:53:18 +0000

    sets up initial collapsible content functionality

---
+++ css/experiments/9/app.js
@@ -0,0 +1,26 @@
+console.log("app.js connected! #9")
+
+
+console.log("app.js connected! #9");
+
+var coll = document.getElementsByClassName("collapse");
+var i;
+
+
+
+for (i = 0; i < coll.length; i++) {
+
+  coll[i].addEventListener("click", function() {
+
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+
+    if (content.style.maxHeight) {
+
+      content.style.maxHeight = null;
+    } else {
+
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}

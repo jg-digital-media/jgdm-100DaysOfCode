ba40c25f1c997b6c203c705282d23f77719ab66a
Jonathan Grieve <mail@jonniegrieve.co.uk>
2022-01-25 15:00:38 +0000

    style route selector thumbnails

--- php/lockdown/sections/route-slider.php
+++ php/lockdown/sections/route-slider.php
@@ -3,25 +3,85 @@
 
     <div class="route-slider">
 
-        <div> <a href="inc/walk-templates/dene/walk-one.php" title="Red Squirrel Trail #1" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-dene-one.jpg" alt="Red Squirrel Trail #1" class="route-slider-image" /> </a> </div>
-        <div> <a href="inc/walk-templates/dene/walk-two.php" title="Red Squirrel Trail #2" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-dene-two.jpg" alt="Red Squirrel Trail #2" class="route-slider-image" /> </a> </div> 
-        <div> <a href="inc/walk-templates/dene/walk-three.php" title="Walk Dene: Three" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-dene-three.jpg" alt="Walk Dene: Three" class="route-slider-image" /> </a> </div> 
-        <div> <a href="inc/walk-templates/dene/walk-four.php" title="Walk Trail: Horden Route" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-dene-four-horden.jpg" alt="Walk Trail: Horden Route" class="route-slider-image" /> </a> </div> 
-        <div> <a href="inc/walk-templates/dene/walk-five.php" title="Walk Dene 5: Yew Tree Route" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-dene-five-yew-tree.jpg" alt="Walk Dene 5: Yew Tree Route" class="route-slider-image" /> </a> </div>
-        <div> <a href="inc/walk-templates/dene/walk-six.php" title="Walk Dene 6: Yew Tree Loop" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-dene-six-yew-tree-loop.jpg" alt="Walk Dene 6: Yew Tree Loop" class="route-slider-image" /> </a> </div>
-        <div> <a href="inc/walk-templates/railway/walk-seven.php" title="Walk Dene Seven: Hesleden Route" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-dene-seven-hesleden-route.jpg" alt="Walk Dene Seven: Hesleden Route" class="route-slider-image" /> </a> </div>
-        <div> <a href="inc/walk-templates/railway/walk-eight.php" title="Walk 8:  Hart to Haswell (Right)" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-eight-hart-to-haswell-right.jpg" alt="Walk 8:  Hart to Haswell (Right)" class="route-slider-image" /> </a> </div>
-        <div> <a href="inc/walk-templates/railway/walk-nine.php" title="Walk 9: Hart to Haswell (Left)" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-nine-hart-to-haswell-left.jpg" alt="Walk 9: Hart to Haswell (Left)" class="route-slider-image" /> </a> </div>
-        <div> <a href="inc/walk-templates/railway/walk-ten.php" title="Walk 10: Bleachery Dene: Left" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-ten-bleachery-dene-left.jpg" alt="Walk 10: Bleachery Dene: Left" class="route-slider-image" /> </a> </div> 
-        <div> <a href="inc/walk-templates/railway/walk-eleven.php" title="Walk 11: Bleachery Dene" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-eleven-bleachery-dene.jpg" alt="Walk 11: Bleachery Dene" class="route-slider-image" /> </a> </div> 
-        <div> <a href="inc/walk-templates/railway/walk-twelve.php" title="Walk 12: To Station Town" class="route-slider-link" target="blank" target="blank"> <img src="img/walk_homepage/walk-twelve-to-station-town.jpg" alt="Walk 12: To Station Town" class="route-slider-image" /> </a> </div> 
-        <div> <a href="inc/walk-templates/railway/walk-thirteen.php" title="Walk: 13 Hart to Haswell Cross-field Route" class="route-slider-link"> <img src="img/walk_homepage/walk-thirteen.jpg" alt="Walk: 13 Hart to Haswell Cross-field Route" class="route-slider-image" /> </a> </div> 
-        <div> <a href="inc/walk-templates/coastal/walk-fourteen.php" title="Walk 14" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-fourteen.jpg" alt="Walk 14" class="route-slider-image" /> </a> </div>
-        <div> <a href="inc/walk-templates/coastal/walk-fifteen.php" title="Walk 15: Crimdon Route 1" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-fifteen.jpg" alt="Walk 15: Crimdon Route 1" class="route-slider-image" /> </a> </div> 
-        <div> <a href="inc/walk-templates/coastal/walk-sixteen.php" title="Walk 16: Crimdon Route 2" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-sixteen.jpg" alt="Walk 16: Crimdon Route 2" class="route-slider-image" /> </a> </div> 
-        <div> <a href="inc/walk-templates/coastal/walk-seventeen.php" title="Blackhall Rocks" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-seventeen.jpg" alt="Blackhall Rocks" class="route-slider-image" /> </a> </div> 
-        <div> <a href="inc/walk-templates/coastal/walk-eighteen.php" title="Limekiln Gill Trail" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-eighteen.jpg" alt="Limekiln Gill Trail" class="route-slider-image" /> </a> </div> 
-        <div> <a href="inc/walk-templates/coastal/walk-nineteen.php" title="Walk 19" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-nineteen.jpg" alt="Walk 19" class="route-slider-image" /> </a> </div> 
-        <div> <a href="inc/walk-templates/dene/walk-twenty.php" title="Walk 20" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-twenty.jpg" alt="Walk 20" class="route-slider-image" /> </a> </div> 
+        <!-- <div>One</div>
+        <div>Two</div>
+        <div>Three</div>
+        <div>Four</div>
+        <div>One</div>
+        <div>Two</div>
+        <div>Three</div>
+        <div>Eight</div> -->
+
+
+        
+
+        <div><a href="inc/walk-templates/dene/walk-one.php" title="Red Squirrel Trail #1" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-dene-one.jpg" alt="Red Squirrel Trail #1" class="route-slider-image" /> </a> </div>
+        
+        <div><a href="inc/walk-templates/dene/walk-two.php" title="Red Squirrel Trail #2" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-dene-two.jpg" alt="Red Squirrel Trail #2" class="route-slider-image" /> </a> </div> 
+        
+        <div><a href="inc/walk-templates/dene/walk-three.php" title="Walk Dene: Three" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-dene-three.jpg" alt="Walk Dene: Three" class="route-slider-image" /> </a> </div> 
+        
+        <div><a href="inc/walk-templates/dene/walk-four.php" title="Walk Trail: Horden Route" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-dene-four-horden.jpg" alt="Walk Trail: Horden Route" class="route-slider-image" /> </a> </div> 
+        
+        <div><a href="inc/walk-templates/dene/walk-five.php" title="Walk Dene 5: Yew Tree Route" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-dene-five-yew-tree.jpg" alt="Walk Dene 5: Yew Tree Route" class="route-slider-image" /> </a> </div>
+        
+        <div><a href="inc/walk-templates/dene/walk-six.php" title="Walk Dene 6: Yew Tree Loop" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-dene-six-yew-tree-loop.jpg" alt="Walk Dene 6: Yew Tree Loop" class="route-slider-image" /> </a> </div>
+        
+        <div><a href="inc/walk-templates/railway/walk-seven.php" title="Walk Dene Seven: Hesleden Route" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-dene-seven-hesleden-route.jpg" alt="Walk Dene Seven: Hesleden Route" class="route-slider-image" /> </a> </div>
+        
+        <div><a href="inc/walk-templates/railway/walk-eight.php" title="Walk 8:  Hart to Haswell (Right)" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-eight-hart-to-haswell-right.jpg" alt="Walk 8:  Hart to Haswell (Right)" class="route-slider-image" /> </a> </div>
+        
+        <div><a href="inc/walk-templates/railway/walk-nine.php" title="Walk 9: Hart to Haswell (Left)" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-nine-hart-to-haswell-left.jpg" alt="Walk 9: Hart to Haswell (Left)" class="route-slider-image" /> </a> </div>
+        
+        <div><a href="inc/walk-templates/railway/walk-ten.php" title="Walk 10: Bleachery Dene: Left" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-ten-bleachery-dene-left.jpg" alt="Walk 10: Bleachery Dene: Left" class="route-slider-image" /> </a> </div> 
+        
+        <div><a href="inc/walk-templates/railway/walk-eleven.php" title="Walk 11: Bleachery Dene" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-eleven-bleachery-dene.jpg" alt="Walk 11: Bleachery Dene" class="route-slider-image" /> </a> </div> 
+        
+        <div><a href="inc/walk-templates/railway/walk-twelve.php" title="Walk 12: To Station Town" class="route-slider-link" target="blank" target="blank"> <img src="img/walk_homepage/walk-twelve-to-station-town.jpg" alt="Walk 12: To Station Town" class="route-slider-image" /> </a> </div> 
+        
+        <div><a href="inc/walk-templates/railway/walk-thirteen.php" title="Walk: 13 Hart to Haswell Cross-field Route" class="route-slider-link"> <img src="img/walk_homepage/walk-thirteen.jpg" alt="Walk: 13 Hart to Haswell Cross-field Route" class="route-slider-image" /> </a> </div> 
+        
+        <div><a href="inc/walk-templates/coastal/walk-fourteen.php" title="Walk 14" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-fourteen.jpg" alt="Walk 14" class="route-slider-image" /> </a> </div>
+        
+        <div><a href="inc/walk-templates/coastal/walk-fifteen.php" title="Walk 15: Crimdon Route 1" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-fifteen.jpg" alt="Walk 15: Crimdon Route 1" class="route-slider-image" /> </a> </div> 
+        
+        <div><a href="inc/walk-templates/coastal/walk-sixteen.php" title="Walk 16: Crimdon Route 2" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-sixteen.jpg" alt="Walk 16: Crimdon Route 2" class="route-slider-image" /> </a> </div> 
+        
+        <div><a href="inc/walk-templates/coastal/walk-seventeen.php" title="Blackhall Rocks" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-seventeen.jpg" alt="Blackhall Rocks" class="route-slider-image" /> </a> </div> 
+        
+        <div><a href="inc/walk-templates/coastal/walk-eighteen.php" title="Limekiln Gill Trail" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-eighteen.jpg" alt="Limekiln Gill Trail" class="route-slider-image" /> </a> </div> 
+        
+        <div><a href="inc/walk-templates/coastal/walk-nineteen.php" title="Walk 19" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-nineteen.jpg" alt="Walk 19" class="route-slider-image" /> </a> </div> 
+        
+        <div><a href="inc/walk-templates/dene/walk-twenty.php" title="Walk 20" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-twenty.jpg" alt="Walk 20" class="route-slider-image" /> </a> </div> 
+
+      
+
+
+
+        <!--
+            
+        <a href="inc/walk-templates/dene/walk-one.php" title="Red Squirrel Trail #1" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-dene-one.jpg" alt="Red Squirrel Trail #1" class="route-slider-image" /> </a>
+        <a href="inc/walk-templates/dene/walk-two.php" title="Red Squirrel Trail #2" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-dene-two.jpg" alt="Red Squirrel Trail #2" class="route-slider-image" /> </a> 
+        <a href="inc/walk-templates/dene/walk-three.php" title="Walk Dene: Three" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-dene-three.jpg" alt="Walk Dene: Three" class="route-slider-image" /> </a> 
+        <a href="inc/walk-templates/dene/walk-four.php" title="Walk Trail: Horden Route" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-dene-four-horden.jpg" alt="Walk Trail: Horden Route" class="route-slider-image" /> </a> 
+        <a href="inc/walk-templates/dene/walk-five.php" title="Walk Dene 5: Yew Tree Route" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-dene-five-yew-tree.jpg" alt="Walk Dene 5: Yew Tree Route" class="route-slider-image" /> </a>
+        <a href="inc/walk-templates/dene/walk-six.php" title="Walk Dene 6: Yew Tree Loop" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-dene-six-yew-tree-loop.jpg" alt="Walk Dene 6: Yew Tree Loop" class="route-slider-image" /> </a>
+        <a href="inc/walk-templates/railway/walk-seven.php" title="Walk Dene Seven: Hesleden Route" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-dene-seven-hesleden-route.jpg" alt="Walk Dene Seven: Hesleden Route" class="route-slider-image" /> </a>
+        <a href="inc/walk-templates/railway/walk-eight.php" title="Walk 8:  Hart to Haswell (Right)" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-eight-hart-to-haswell-right.jpg" alt="Walk 8:  Hart to Haswell (Right)" class="route-slider-image" /> </a>
+        <a href="inc/walk-templates/railway/walk-nine.php" title="Walk 9: Hart to Haswell (Left)" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-nine-hart-to-haswell-left.jpg" alt="Walk 9: Hart to Haswell (Left)" class="route-slider-image" /> </a>
+        <a href="inc/walk-templates/railway/walk-ten.php" title="Walk 10: Bleachery Dene: Left" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-ten-bleachery-dene-left.jpg" alt="Walk 10: Bleachery Dene: Left" class="route-slider-image" /> </a> 
+        <a href="inc/walk-templates/railway/walk-eleven.php" title="Walk 11: Bleachery Dene" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-eleven-bleachery-dene.jpg" alt="Walk 11: Bleachery Dene" class="route-slider-image" /> </a> 
+        <a href="inc/walk-templates/railway/walk-twelve.php" title="Walk 12: To Station Town" class="route-slider-link" target="blank" target="blank"> <img src="img/walk_homepage/walk-twelve-to-station-town.jpg" alt="Walk 12: To Station Town" class="route-slider-image" /> </a> 
+        <a href="inc/walk-templates/railway/walk-thirteen.php" title="Walk: 13 Hart to Haswell Cross-field Route" class="route-slider-link"> <img src="img/walk_homepage/walk-thirteen.jpg" alt="Walk: 13 Hart to Haswell Cross-field Route" class="route-slider-image" /> </a> 
+        <a href="inc/walk-templates/coastal/walk-fourteen.php" title="Walk 14" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-fourteen.jpg" alt="Walk 14" class="route-slider-image" /> </a> </div>
+        <a href="inc/walk-templates/coastal/walk-fifteen.php" title="Walk 15: Crimdon Route 1" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-fifteen.jpg" alt="Walk 15: Crimdon Route 1" class="route-slider-image" /> </a> 
+        <a href="inc/walk-templates/coastal/walk-sixteen.php" title="Walk 16: Crimdon Route 2" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-sixteen.jpg" alt="Walk 16: Crimdon Route 2" class="route-slider-image" /> </a> 
+        <a href="inc/walk-templates/coastal/walk-seventeen.php" title="Blackhall Rocks" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-seventeen.jpg" alt="Blackhall Rocks" class="route-slider-image" /> </a> 
+        <a href="inc/walk-templates/coastal/walk-eighteen.php" title="Limekiln Gill Trail" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-eighteen.jpg" alt="Limekiln Gill Trail" class="route-slider-image" /> </a> 
+        <a href="inc/walk-templates/coastal/walk-nineteen.php" title="Walk 19" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-nineteen.jpg" alt="Walk 19" class="route-slider-image" /> </a> 
+        <a href="inc/walk-templates/dene/walk-twenty.php" title="Walk 20" class="route-slider-link" target="blank"> <img src="img/walk_homepage/walk-twenty.jpg" alt="Walk 20" class="route-slider-image" /> </a> 
+    
+        --> 
 
     </div>

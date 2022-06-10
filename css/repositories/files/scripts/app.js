9934f2bbe2a1abf3b0a14968340819c20743589e
Jonathan Grieve <mail@jonniegrieve.co.uk>
2022-06-10 09:05:57 +0100

    attempt copy clone function

--- css/repositories/files/scripts/app.js
+++ css/repositories/files/scripts/app.js
@@ -1,46 +1,58 @@
 
-
+// script connected
 console.log("app.js connected");
 
+const clone_messasge = "git clone";
+const copy_clone_command = document.querySelectorAll("copy_clone_command");
 
 
-/*  //select project count
-    let project_count = document.getElementById("project_count");
-    let count = "( " + photoData.projects.length + " )";
-
-    project_count.textContent = count; */
-
+// Get the json data
 jQuery.getJSON('files/data/repositories.json', function(photoData) {
 
-    
-
-
     let itemAll = photoData.length;
-    console.log(photoData.length);
+    console.log( photoData.length );
 
     
     for (let i=0; i < itemAll; i++) { 
         
-        // Successful delivery of class and image URL    - uses data-src and/or src attributes for image element        
-        /* jQuery(`<a href="${photoData.projects[i].repo_name}" target="blank"> Data </a>`
-        ).appendTo('.list-js');  */ 
-        
+        // store message on jQuery method
         jQuery(
 
             `<div class="repo_item">
 
-
                 <img src="${ photoData[i].repo_img }" title="${ photoData[i].repo_alt }" alt="${ photoData[i].repo_alt }" />
-                <a href="${ photoData[i].repo_url }" target="blank"> ${ photoData[i].repo_name } </a>
+                <a href="${ photoData[i].repo_url }" class="href_repo_url" target="blank"> ${ photoData[i].repo_name } </a>
 
                 <span class="${ photoData[i].repo_type }"> (${ photoData[i].repo_type }) </span> | <span class="${ photoData[i].repo_status} "> (${ photoData[i].repo_status }) </span>
-                <!-- > <a href="#" class="clone_command" id="js-clone">clone command</a>  -->
+                
+                <!-- Copy clone button -->
+                <a href="#" class="copy_clone_command" id="js-clone" onclick="copy_the_text()">clone command</a>
             
             </div>`
 
-        ).appendTo('.list-js');  
+        ).appendTo('.list-js');                 
 
-    }
+    }    
 
+  
 });
 
+
+function copy_the_text() {
+
+    // Get the text field 
+    var copyText = document.querySelectorAll(".href_repo_url");
+
+    // Select the text field 
+    clone_message.select();
+    clone_message.setSelectionRange(0, 99999); /* For mobile devices 
+
+    //  Copy the text inside the text field */
+    clone_message.clipboard.writeText(copyText.value);
+    
+    // Alert the copied text 
+    alert("Copied the text: " + copyText.value);
+
+}    
+
+copy_the_text();

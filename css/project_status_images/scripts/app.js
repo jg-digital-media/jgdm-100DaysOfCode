b15ca0c86e3fc985b38aeb508b55f54734c3a05a
Jonathan Grieve <mail@jonniegrieve.co.uk>
2022-06-16 13:39:18 +0100

    fix correct project count

--- css/project_status_images/scripts/app.js
+++ css/project_status_images/scripts/app.js
@@ -26,7 +26,7 @@ jQuery.getJSON('../../css/portfolio/data/project-list.json', function(photoData)
     for (let i=0; i < itemAll; i++) { 
         
         jQuery(`
-        
+
             <li>
                 <a href="${photoData.projects[i].project_url}" target="blank">
                    <img ${photoData.projects[i].img_type}="${photoData.projects[i].img_url}" src="${photoData.projects[i].img_url}" class="site-images lazy" alt="${photoData.projects[i].project_alt}" title="${photoData.projects[i].project_alt}" tabindex="" />
@@ -43,21 +43,6 @@ jQuery.getJSON('../../css/portfolio/data/project-list.json', function(photoData)
 
 });
 
-
-jQuery.getJSON('../../css/portfolio/data/repositories.json', function(repoData) { 
-    
-    let itemAll = repoData.repositories.length;
-    console.log( repoData.repositories.length );
-    
-    // display data count to browser
-    project_count.textContent = "( " +  repoData.repositories.length  + " )";
-
-    for (let i=0; i < itemAll; i++) { 
-
-        jQuery(`1.... 3....`).appendTo(`.repository-status-data`);
-
-    }
-});
 
 
 

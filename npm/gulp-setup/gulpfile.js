74773bcd94f9b411c5ba6f6432795004617b1321
Jonathan Grieve <mail@jonniegrieve.co.uk>
2022-06-27 16:15:39 +0100

    start the gulpfile taskfunnergit add npm/gulp-setup/gulpfile.js

---
+++ npm/gulp-setup/gulpfile.js
@@ -0,0 +1,39 @@
+
+const { src, dest, series, watch } = require('gulp');
+
+
+// Get the required NPM pckages and store to variales 
+
+// Styles
+const scss = require('gulp-sass')(require('sass'));
+
+const autoPrefixer = require('gulp-autoprefixer');
+
+const cssMinify = require('gulp-clean-css');
+
+// Scripts
+const jsMinify = require('gulp-terser');
+
+// use styles and scripts
+
+function styles() {
+
+    return src('./frontend/src/styles/**/*.scss')
+
+    .pipe(scss())
+    .pipe(autoPrefixer('last 2 versions'))
+    .pipe(cssMinify)
+    .pipe(dest('./frontend/dist/styles/'))
+}
+
+function scripts() {
+
+    return src('.frontend/src/srcipts/**/*.js')
+    
+    .pipe(jsMinify())
+    .pipe(dest('./frontend/dist/scripts/'));
+
+
+
+}
+

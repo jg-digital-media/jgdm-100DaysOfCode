90d2fdb0cee316224d76fa8b9dfb980e689fe8d8
Jonathan Grieve <mail@jonniegrieve.co.uk>
2022-06-28 16:55:11 +0100

    reformat task runner file

--- npm/gulp-setup/gulpfile.js
+++ npm/gulp-setup/gulpfile.js
@@ -5,47 +5,47 @@ const { src, dest, series, watch } = require('gulp');
 // Get the required NPM pckages and store to variales 
 
 // Styles
-const scss = require('gulp-sass')(require('sass'));
-
+const gulp_sass = require('gulp-sass') (require('sass'));
 const autoPrefixer = require('gulp-autoprefixer');
-
 const cssMinify = require('gulp-clean-css');
 
-// Scripts
-const jsMinify = require('gulp-terser');
-
-// use styles and scripts
-
+// use styles
 function styles() {
 
     return src('./frontend/src/styles/**/*.scss')
 
-    .pipe(scss())
+    .pipe(gulp_sass())
     .pipe(autoPrefixer('last 2 versions'))
     .pipe(cssMinify())
     .pipe(dest('./frontend/dist/styles/'))
 }
 
+// Scripts
+const jsMinify = require('gulp-terser');
+
 function scripts() {
 
-    return src('.frontend/src/srcipts/**/*.js')
+    return src('.frontend/src/scripts/**/*.js')
     
     .pipe(jsMinify())
-    .pipe(dest('./frontend/dist/scripts/'));
-
-
+    .pipe(dest('./frontend/dist/scripts/'))
 
 }
 
+
+// Watch task
 function watchTask() {
-
-    watch( [ 
+ 
+    watch(        
+    [ 
         './frontend/src/styles/**/*.scss', 
-        './frontend/src/scripts/**/*.js'], series(styles, scripts)
-    );
+        './frontend/src/scripts/**/*.js'
+    ], 
 
-}
+    series(styles, scripts)
+    )
 
+} 
 
 exports.default = series(styles, scripts, watchTask);
 

55b406a4c4db7698c57fae3597f27a8141fca38b
Jonathan Grieve <mail@jonniegrieve.co.uk>
2022-07-04 13:16:34 +0100

    commented code for getting data dnyamically

--- css/sass_project/scripts/app.js
+++ css/sass_project/scripts/app.js
@@ -3,3 +3,20 @@ $(document).ready(function() {
         arrows: false,
     });
 });
+
+
+
+/*
+
+jQuery.getJSON('../../../data/project-list.json', function(photoData) { 
+    
+    let itemAll = photoData.projects.length;
+    console.log(photoData.projects.length);
+
+    for (let i=0; i < itemAll; i++) { 
+
+        <img class="slide" src="" alt="Games Yard  First created in 2004, this website was created to be an informational site for playing various popular games." />
+
+    }
+    
+});*/

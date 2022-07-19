8604f4cecd18ae45e5e6e154b0e46f74205a9cc7
jg-digital-media <mail@jonniegrieve.co.uk>
2022-07-19 16:41:34 +0100

    display ValueError when text is input

--- python/tkinter/temp.py
+++ python/tkinter/temp.py
@@ -5,12 +5,18 @@ import tkinter as tk
 
 # Functions
 
+# The fahrenheit to celsiys calculator
 def fahrenheit_to_celsius():
 
-
-    fa = ent_temperature.get()
-    ce = (5/9) * (float( fa ) - 32)
-    lbl_result["text"] = f"{round(ce, 2)} \N{DEGREE CELSIUS}"
+    # raise value error when text is entered
+    try:
+        fa = ent_temperature.get()
+    
+        ce = (5/9) * (float( fa ) - 32)
+        lbl_result["text"] = f"{round(ce, 2)} \N{DEGREE CELSIUS}"
+    except  ValueError:
+        print("ssss")
+        lbl_result["text"] = "Error: Please enter a number"
 
 window = tk.Tk()
 window.title("Temperature Converter")
@@ -20,6 +26,7 @@ window.resizable(width=False, height=False)
 frm_entry = tk.Frame(master=window)
 ent_temperature = tk.Entry(master=frm_entry, width=10)
 lbl_temp = tk.Label(master=frm_entry, text="\N{DEGREE FAHRENHEIT}")
+lbl_title = tk.Label(master=window, text = "Temperature Calculator")
 
 ent_temperature.grid(row=0, column=0, sticky="e")
 lbl_temp.grid(row=0, column=1, sticky="w")
@@ -31,10 +38,14 @@ btn_convert = tk.Button(
 
 )
 
+
+
 lbl_result = tk.Label(master=window, text="\N{DEGREE CELSIUS}")
 
-frm_entry.grid(row=0, column=0, padx=10)
-btn_convert.grid(row=0, column=1, pady=10)
-lbl_result.grid(row=0, column=2, padx=10)
+
+lbl_title.grid(row=0, column=1, padx=0)
+frm_entry.grid(row=1, column=0, padx=10)
+btn_convert.grid(row=1, column=1, pady=10)
+lbl_result.grid(row=1, column=2, padx=10)
 
 window.mainloop()

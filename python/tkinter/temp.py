342f6872c6e53ab692f56ef3648b13ce63a46fde
Jonathan Grieve <mail@jonniegrieve.co.uk>
2022-07-19 12:11:42 +0100

    finish temperature calculator

---
+++ python/tkinter/temp.py
@@ -0,0 +1,40 @@
+# Temp.py - Temperature Calculator
+# Src: https://realpython.com/python-gui-tkinter/#building-a-temperature-converter-example-app
+
+import tkinter as tk
+
+# Functions
+
+def fahrenheit_to_celsius():
+
+
+    fa = ent_temperature.get()
+    ce = (5/9) * (float( fa ) - 32)
+    lbl_result["text"] = f"{round(ce, 2)} \N{DEGREE CELSIUS}"
+
+window = tk.Tk()
+window.title("Temperature Converter")
+window.resizable(width=False, height=False)
+
+
+frm_entry = tk.Frame(master=window)
+ent_temperature = tk.Entry(master=frm_entry, width=10)
+lbl_temp = tk.Label(master=frm_entry, text="\N{DEGREE FAHRENHEIT}")
+
+ent_temperature.grid(row=0, column=0, sticky="e")
+lbl_temp.grid(row=0, column=1, sticky="w")
+
+btn_convert = tk.Button(
+    master=window,
+    text= "\N{RIGHTWARDS BLACK ARROW}",
+    command=fahrenheit_to_celsius
+
+)
+
+lbl_result = tk.Label(master=window, text="\N{DEGREE CELSIUS}")
+
+frm_entry.grid(row=0, column=0, padx=10)
+btn_convert.grid(row=0, column=1, pady=10)
+lbl_result.grid(row=0, column=2, padx=10)
+
+window.mainloop()

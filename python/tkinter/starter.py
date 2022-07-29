7c9c83bbc9be3bac478f9b38355c9c31148deefb
jg-digital-media <mail@jonniegrieve.co.uk>
2022-07-29 17:05:21 +0100

    tkinter starter file

---
+++ python/tkinter/starter.py
@@ -0,0 +1,23 @@
+
+
+"""
+starter file.  Requires Python and a command line system
+
+Run: python starter.py
+
+"""
+
+# import the tkinter module
+import tkinter
+
+# import the ttk submodule
+from tkinter import ttk
+
+# creates a root window for the app/gui
+root = tkinter.Tk()
+
+"""Code your program"""
+
+
+# Run the tkinter event loop (listening for events)
+root.mainloop()

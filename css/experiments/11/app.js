e0422781a6114bd0ccd94f6496d2778d1b895dcf
Jonathan Grieve <mail@jonniegrieve.co.uk>
2022-10-28 12:49:47 +0100

    javascript accordian starting example

--- css/experiments/11/app.js
+++ css/experiments/11/app.js
@@ -1,1 +1,24 @@
 console.log("app.js connected");
+
+// Toggle accordian content
+
+let accordians = document.querySelectorAll('.accordian');
+
+accordians.forEach(accordian => {
+    
+    accordian.addEventListener('click', e => {
+        
+        let accordianBtn = accordian.querySelector('button');
+        accordian.classList.toggle('active');
+        
+        if(accordianBtn.textContent === '+') {
+            accordianBtn.textContent === '-';
+        } else {
+            accordianBtn.textContent = '+';
+        }
+        
+        
+    })
+    
+})
+

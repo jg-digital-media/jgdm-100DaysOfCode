3a4e3c2a23de69ce3867b76328be057bd6735607
Jonathan Grieve <mail@jonniegrieve.co.uk>
2022-10-28 11:46:02 +0100

    move css toggle

--- css/css-only-toggle/app.js
+++
@@ -1,9 +0,0 @@
-console.log("app.js connected");
-
-//toggle dark mode
-let toggle_btn = document.getElementById("dark-mode"); 
-
-toggle_btn.addEventListener("click", () =>  {
-    
-    document.body.classList.toggle("dark__mode");
-});

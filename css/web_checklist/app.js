0aa500f385a1bd440630186406850890f8f2bcaf
jg-digital-media <mail@jonniegrieve.co.uk>
2022-11-21 14:07:10 +0000

    select checklist item

--- css/web_checklist/app.js
+++ css/web_checklist/app.js
@@ -1,1 +1,9 @@
 console.log("Check: app.js connected - 21/11/2022 - 10:32")
+
+let checklist_item = document.getElementsByClassName(".checklist-item");
+
+function myFunction() {
+    /* var element = document.getElementById("myDIV"); */
+    /* element.classList.toggle("mystyle"); */
+  }
+

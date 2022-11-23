6c516a938b53e4c48f343c9e55f7c47d48bd119e
jg-digital-media <mail@jonniegrieve.co.uk>
2022-11-23 17:02:03 +0000

    experiment with fixes for focus styling on checkbox tick

--- css/web_checklist/app.js
+++ css/web_checklist/app.js
@@ -2,22 +2,47 @@ console.log("Check: app.js connected - 22/11/2022 - 15:36")
 
 let checklist_item = document.getElementsByClassName("checklist-item");
 let checkboxes = document.querySelectorAll(".checkbox-style");
-let checklist_labels = document.querySelectorAll("label");
+let checklist_labels = document.querySelectorAll(".checkbox_labels");
 
 
-const toggleChk = document.querySelectorAll('.checklist-item').forEach(checkboxes => {
+/* const toggleChk = document.querySelectorAll('.checklist-item').forEach( checkbox_btn  => {
 
-  checkboxes.addEventListener('click', function() {
+  checkbox_btn.addEventListener('click', function() {
 
-    checkboxes.classList.toggle('selected');
-  });
+    checkbox_btn.classList.toggle('selected');
+    //checkbox_btn.click();
+  }); 
+
+}); */
+
+const toggleLbl = document.querySelectorAll(".checklist-item").forEach( (item_labels, checkbox_btn, checklist) => {
+
+  item_labels.addEventListener("click", function() {
+
+  
+    item_labels.classList.toggle('selected');
+    //checkbox_btn.classList.toggle('selected');
+
+    checkbox_btn.previousElementSibling;
+    checkbox_btn.classList.toggle('selected');
+    //checkboxes.checked = checkboxes.checked;
+    /* 
+      if( checkboxes.checked == true ) {
+
+        checkboxes.checked = !checkboxes.checked;
+        
+      } 
+    */
 
 });
 
-function toggle_bg_checkbox() {
+  
+});  
+
+/* function toggle_bg_checkbox() {
 
   // checklist_item.classList.toggle("selected");  
-}
+} */
 
 /* function toggle_bg_checkbox() {
 

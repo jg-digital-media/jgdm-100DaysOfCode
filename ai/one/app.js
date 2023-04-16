6084add1615b76cc9539f549f16f80880db2a3b0
jg-digital-media <mail@jonniegrieve.co.uk>
2023-04-16 20:43:30 +0100

    first project - sticky header

---
+++ ai/one/app.js
@@ -0,0 +1,14 @@
+console.log("app.js connected!");
+
+window.onscroll = function() {myFunction()};
+
+var header = document.querySelector("header");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}

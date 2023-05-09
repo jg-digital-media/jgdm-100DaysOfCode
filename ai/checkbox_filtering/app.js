df102e201233716ecc84b99eb0eb433940e19645
Jonathan Grieve <mail@jonniegrieve.co.uk>
2023-05-09 16:58:38 +0100

    centralise checkbox filters

--- ai/checkbox_filtering/app.js
+++ ai/checkbox_filtering/app.js
@@ -1,20 +1,67 @@
 
 //
-console.log("app.js connected - 09-05-2023 - 14:07");
+console.log("app.js connected - 09-05-2023 - 15:55");
 
-    const toggleCheckbox = document.getElementById('toggleCheckbox');
-    const toggleElements = document.getElementsByClassName('toggle-element');
+    const toggleCheckboxJavaScript = document.getElementById('toggleCheckboxJavaScript');
+    const toggleCheckboxHTML = document.getElementById('toggleCheckboxHTML');
+    const toggleCheckboxCSS = document.getElementById('toggleCheckboxCSS');
 
-    toggleCheckbox.addEventListener('change', function() {
-      const isChecked = toggleCheckbox.checked;
+    const toggleElements = document.getElementsByClassName('javascript');
+    const toggleHtmlElements = document.getElementsByClassName('html');
+    const toggleCssElements = document.getElementsByClassName('css');
 
-      for (let i = 0; i < toggleElements.length; i++) {
-        const element = toggleElements[i];
+    // HTML 
+    toggleCheckboxHTML.addEventListener('change', function() {
+        
+        const isChecked = toggleCheckboxHTML.checked;
 
-        if (isChecked) {
-          element.style.display = 'block';
-        } else {
-          element.style.display = 'none';
-        }
+        for (let i = 0; i < toggleHtmlElements.length; i++) {
+            const element = toggleHtmlElements[i];
+
+            if (isChecked) {
+                
+                element.style.display = 'block';
+            } else {
+                
+                element.style.display = 'none';
+            }
+      }
+    });
+
+
+    // CSS
+    toggleCheckboxCSS.addEventListener('change', function() {
+        
+        const isChecked = toggleCheckboxCSS.checked;
+
+        for (let i = 0; i < toggleCssElements.length; i++) {
+            const element = toggleCssElements[i];
+
+            if (isChecked) {
+                
+                element.style.display = 'block';
+            } else {
+                
+                element.style.display = 'none';
+            }
+      }
+    });
+
+
+    // JavaScript
+    toggleCheckboxJavaScript.addEventListener('change', function() {
+        
+        const isChecked = toggleCheckboxJavaScript.checked;
+
+        for (let i = 0; i < toggleElements.length; i++) {
+            const element = toggleElements[i];
+
+            if (isChecked) {
+                
+                element.style.display = 'block';
+            } else {
+                
+                element.style.display = 'none';
+            }
       }
     });
--- ai/checkbox_filtering/index.php
+++ ai/checkbox_filtering/index.php
@@ -3,21 +3,45 @@
 <head>
   <style>
   </style>
+    
+    <link rel="stylesheet" type="text/css" href="styles.css" />
 </head>
 <body>
     
+    <header>
+        
+        <h1>Checkbox Filtering</h1>
     
-    <strong>Input: </strong><code>toggle a list of child elements based on the existence of a HTML class using checkboxes</code>
+    </header>
     
-  <label>
-    <input type="checkbox" id="toggleCheckbox"> Toggle Elements
-  </label>
+    <div id="container">      
+        
+    
+        <span class="input"><strong>Input: </strong><code>toggle a list of child elements based on the existence of a HTML class using checkboxes</code></span>
+    
+        <aside class="filters">
+            
+              <label>
+                <input type="checkbox" id="toggleCheckboxHTML"> HTML
+              </label>
+              <label>
+                <input type="checkbox" id="toggleCheckboxCSS"> CSS
+              </label>
+              <label>
+                <input type="checkbox" id="toggleCheckboxJavaScript"> JavaScript
+              </label>
+            
+        </aside>
+        
+          <div class="toggle-element javascript">Element 1</div>
+          <div class="toggle-element javascript css">Element 2</div>
+          <div class="toggle-element css">Element 3</div>
+          <div class="toggle-element html javascript css ">Element 4</div>
+          <div class="toggle-element html ">Element 5</div>
+          <div class="toggle-element javascript ">Element 6</div>
+        
+        </div>
 
-  <div class="toggle-element">Element 1</div>
-  <div class="toggle-element">Element 2</div>
-  <div class="toggle-element">Element 3</div>
-
-  <script>
-  </script>
+    <script type="text/javascript" src="app.js"></script>
 </body>
 </html>

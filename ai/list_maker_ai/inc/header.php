d8c9b364de33ff12b4707f762a855bd9dc7b9afe
Jonathan Grieve <mail@jonniegrieve.co.uk>
2023-08-24 12:49:39 +0100

    basic version navigation in header

--- ai/list_maker_ai/inc/header.php
+++ ai/list_maker_ai/inc/header.php
@@ -23,6 +23,16 @@
     <header>
         <h1><?php echo $website_title; ?> <span id="versioning">v<?php echo $version; ?></span></h1>
         
+        <ul class="version_list">
+            <li><a href="index-v1.php">v1</a></li>
+            <li><a href="index-v2.php">v2</a></li>
+            <li><a href="index-v3.php">v3</a></li>
+            <li><a href="index-v4.php">v4</a></li>
+            <li><a href="index-v5.php">v5</a></li>
+            <li><a href="index-v6.php">v6.1</a></li>
+            <!--<li><a href="index-v.php">v1</a></li>-->
+        </ul>
+        
     </header>
 
     <?php else: ?>

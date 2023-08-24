2f251bf1b235342d9a84c11dce55a496f0afe3e3
Jonathan Grieve <mail@jonniegrieve.co.uk>
2023-08-24 08:50:22 +0100

    add header element for header v4

--- ai/list_maker_ai/inc/header.php
+++ ai/list_maker_ai/inc/header.php
@@ -17,14 +17,16 @@
 </head>
 <body>
     
-   
     
-     <?php if ( $website_title == "Simple List Maker App" ) : ?>
-    
-    <h1><?php echo $website_title; ?> <span id="versioning">v<?php echo $version; ?></span></h1>
-    
+    <?php if ( $website_title == "Simple List Maker App" ) : ?>
+
+    <header>
+        <h1><?php echo $website_title; ?> <span id="versioning">v<?php echo $version; ?></span></h1>
+        
+    </header>
+
     <?php else: ?>
-    
-     <h1>Task List <span id="versioning">v<?php echo $version; ?></span></h1>
-    
+
+        <h1>Task List <span id="versioning">v<?php echo $version; ?></span></h1>
+
     <?php endif; ?>

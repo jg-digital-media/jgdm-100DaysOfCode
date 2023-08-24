d3240d876a1dcd772b12e8a6787046dc68ff2869
Jonathan Grieve <mail@jonniegrieve.co.uk>
2023-08-24 07:54:13 +0100

    convert project to php

---
+++ ai/list_maker_ai/inc/header.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Task List Maker</title>
+</head>
+<body>
+    <h1>Task List</h1>
---
+++ ai/list_maker_ai/index-v1.php
@@ -0,0 +1,7 @@
+<?php require "inc/header.php"; ?>
+
+    <input type="text" id="taskInput" placeholder="Enter task">
+    <button id="addTaskButton">Add Task</button>
+    <ul id="taskList"></ul>
+
+<?php require "inc/footer.php"; ?>

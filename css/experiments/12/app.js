aadff55f3e1c578d8d0d0976bd31512ed1a7b3cd
Jonathan Grieve <mail@jonniegrieve.co.uk>
2023-09-11 14:52:13 +0100

    set more custom properties

--- css/experiments/12/app.js
+++ css/experiments/12/app.js
@@ -16,18 +16,28 @@ const monochromeColorInput = document.getElementById('monochrome');
 // theme customiser event listeners
 primaryColorInput.addEventListener('click', (event) => {
     
-  document.documentElement.style.setProperty('--primary-color', 'lightblue');
+    document.documentElement.style.setProperty('--primary-color', 'lightblue');    
+    document.documentElement.style.setProperty('--main-color', '#d7fdff');
+    document.documentElement.style.setProperty('--button-bg','#6ff2ff');
+    document.documentElement.style.setProperty('--border-color', '#1313a1');
 });
 
 secondaryColorInput.addEventListener('click', (event) => {
     
-  document.documentElement.style.setProperty('--primary-color', 'lightgreen');
+    document.documentElement.style.setProperty('--primary-color', 'lightgreen');    
+    document.documentElement.style.setProperty('--main-color', '#b3ffe3');
+    document.documentElement.style.setProperty('--button-bg','#2cff6d');
+    document.documentElement.style.setProperty('--border-color', '#13a140');
 });
 
 monochromeColorInput.addEventListener('click', (event) => {
     
-  document.documentElement.style.setProperty('--primary-color', 'gray');
+    document.documentElement.style.setProperty('--primary-color', 'gray');    
+    document.documentElement.style.setProperty('--main-color', '#dbdbdb');
+    document.documentElement.style.setProperty('--button-bg','#d5d5d5');
+    document.documentElement.style.setProperty('--border-color', 'darkgray');
 });
+
 
 console.log(primaryColorInput.style.setProperty);
 console.log(secondaryColorInput.style.setProperty);

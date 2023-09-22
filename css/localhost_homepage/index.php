a151fbe1e36edc1ab34e6f0412a208ebc5c486d2
Jonathan Grieve <mail@jonniegrieve.co.uk>
2023-09-22 12:10:41 +0100

    add a homepage to go on localhost directories

---
+++ css/localhost_homepage/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>JGDM - My Projects on Localhost</title>
+    
+    <style>
+        
+        body {
+            
+            font-family: Arial, sans-serif;
+            background-color: #f0f0f0;
+            text-align: center;
+            padding: 20px;
+        }
+
+        h1 {
+            
+            color: #333;
+        }
+
+        p {
+            
+            font-size: 18px;
+            color: #666;
+        }
+
+        ul {
+            
+            list-style-type: none;
+            padding: 0;
+        }
+
+        li {
+            
+            margin: 10px 0;
+        }
+
+        a {
+            
+            text-decoration: none;
+            color: #007BFF;
+            font-weight: bold;
+            transition: color .3s;
+        }
+
+        a:hover {
+            
+            color: #0056b3;
+        }
+        
+    </style>
+</head>
+<body>
+    
+    <h1>JGDM - My Projects on Localhost</h1>
+    
+    <p>Links to Directories:</p>
+    
+    <ul>
+        <li><a href="http://localhost/jgdm-100DaysOfCode">#100DaysOfCode</a></li>
+        <li><a href="http://localhost/projects">Projects</a></li>
+        <li><a href="http://localhost/server">Server</a></li>
+        <li><a href="http://localhost/repositories">Repositories</a></li>
+        <li><a href="http://localhost/wordpress">WordPress</a></li>
+        <li><a href="http://localhost/jg-digital-media.github.io">GitHub Page</a></li>
+        <li><a href="http://localhost/photography-website">Photography Website</a></li>
+    </ul>
+</body>
+</html>

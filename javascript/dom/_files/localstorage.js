2770b1ff96be61d8bac8a5e89f6614cfaa3e5e86
Jonathan Grieve <mail@jonniegrieve.co.uk>
2023-11-01 16:01:41 +0000

    sort excess files

---
+++ javascript/dom/_files/localstorage.js
@@ -0,0 +1,32 @@
+
+/* getItem() on local storage to hold list data */
+
+
+// # https://dev.to/michaelburrows/how-to-save-data-in-localstorage-using-javascript-994  - Dev To Example
+
+/*
+
+
+
+*/
+
+
+/* function getNewDataItems() {
+  const store_list_data = localStorage.getItem('new_tasks');
+  return [];
+} */
+
+/* inspect JSON data of local storage key value pairs */
+/* function getRecentSearches() {
+  const searches = localStorage.getItem('new_tasks');
+  if (searches) {
+    return JSON.parse(searches);
+  }
+  return [];
+} */
+
+/* clear the local storage data on button click */
+/* clearButton.addEventListener('click', () => {
+  localStorage.removeItem('new_tasks');
+  recentSearchList.innerHTML = '';
+}); */

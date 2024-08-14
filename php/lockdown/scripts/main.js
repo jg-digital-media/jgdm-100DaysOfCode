44a0810ec748336579d2cca179eceeaa2d89c919
Jonathan Grieve <mail@jonniegrieve.co.uk>
2024-08-14 15:15:41 +0100

    add responsive slick breakpoints

--- php/lockdown/scripts/main.js
+++ php/lockdown/scripts/main.js
@@ -1,5 +1,5 @@
 //connection check
-console.log("connected: main script");
+console.log("connected: main script - main.js - 14-08-2024 - 13:38");
 
 
 //Slick Carousel - Homepage Slider
@@ -7,11 +7,43 @@ $(document).ready(function(){
 
     $('.route-slider').slick({
         // infinite: true,
-        slidesToShow: 20,
-        slidesToScroll: 20,
+        slidesToShow: 12,
+        slidesToScroll: 12,
         dots: false,
         draggable: true,
-        swipe: true
+        swipe: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+
+                    slidesToShow: 10,
+                    slidesToScroll: 10,
+                    infinite: true,
+                    //dots: true
+                } 
+            }, {
+                
+                breakpoint: 980,
+                settings: {
+
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
+                    infinite: true,
+                    //dots: true
+                }
+            }, {
+                
+                breakpoint: 680,
+                settings: {
+
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    //dots: true
+                }
+            }
+        ]
     });
 
 });

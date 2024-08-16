0a6cf333f5db901dc67636acbe21f7ef2f69979b
Jonathan Grieve <mail@jonniegrieve.co.uk>
2024-08-16 12:31:03 +0100

    add header and footer

---
+++ php/php_boilerplate/inc/footer.php
@@ -0,0 +1,10 @@
+
+    </main>
+
+    <footer>
+        <p>&copy; <?php echo date('Y') ?> Created by Jonnie Grieve Digital Media <a href="https://www.jonniegrieve.co.uk">https://www.jonniegrieve.co.uk</a>
+    </footer>
+
+    <script type="text/javascript" src="scripts/app.js"></script>
+</body>
+</html>

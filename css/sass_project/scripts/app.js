12a6cae2d6f62fcb47ca719ab106b14732eaf86b
Jonathan Grieve <mail@jonniegrieve.co.uk>
2024-08-19 11:45:32 +0100

    add cdn links

--- css/sass_project/scripts/app.js
+++ css/sass_project/scripts/app.js
@@ -12,22 +12,24 @@
  * // file path to json file - server - ../../../data/sass---project--list.json
  * // absolute path - https://www.jonniegrieve.co.uk/data/project-list.json
  * 
- * Last Update: 13:32 - 09/08/2024
+ * Last Update: 10:42 - 19/08/2024
  */
 
 console.log("connected to app.js")
 
 /* Make the Carousel */ 
 $(document).ready(function() {
+
     $('.js-carousel').slick({
+
         arrows: false,
     });
 });
 
 
 /* Get project list */
-jQuery.getJSON('../../../data/sass---project--list.json', function(photoData) {     
-// jQuery.getJSON('data/sass---project--list.json', function(photoData) { 
+    //jQuery.getJSON('../../../data/sass---project--list.json', function(photoData) {     
+    jQuery.getJSON('data/sass---project--list.json', function(photoData) { 
 
 
     // get the dynamic count and display on screen
@@ -42,7 +44,7 @@ jQuery.getJSON('../../../data/sass---project--list.json', function(photoData) {
 
     for (let i=0; i < itemAll; i++) { 
 
-        jQuery(`<img class="slide" src="${ photoData.projects[i].img_url}" alt="${ photoData.projects[i].img_alt }" />`).appendTo('.js-carousel');
+        jQuery(`<img class="slide" src="${ photoData.projects[i].img_url}" alt="${ photoData.projects[i].project_alt }" />`).appendTo('.js-carousel');
 
     }
     

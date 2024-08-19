e69f5ffd92eef9a61f2931ce4a0597a9c5174eff
Jonathan Grieve <mail@jonniegrieve.co.uk>
2024-08-19 15:06:09 +0100

    delete php auth from repo

--- php/php_auth/procedures/vote.php
+++
@@ -1,9 +0,0 @@
-<?php
-require __DIR__ . '/../inc/bootstrap.php';
-requireAuth();
-
-$user = getAuthenticatedUser();
-
-vote(request()->get('bookId'), request()->get('vote'), $user['id']);
-
-redirect('/jgdm-100daysofcode/php/php_auth/books.php');

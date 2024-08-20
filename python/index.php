1aa72808dfce5274c1f0685313a01d1eee18288d
Jonathan Grieve <mail@jonniegrieve.co.uk>
2024-08-20 11:07:49 +0100

    add home pages for directories

--- python/index.php
+++ python/index.php
@@ -4,23 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
     <title>Python Project List</title>
 
     <style>
     
-    li {
-        margin: 10px 0;
-    }
+        li {
+            margin: 10px 0;
+        }
 
-    a {
-        color: red;
-        
+        a {
+            color: red;
+            
 
-    }
+        }
 
-    a:visited {
-        color: red;
-    }
+        a:visited {
+
+            color: red;
+        }
+
     </style>
 </head>
 <body>
@@ -30,6 +33,12 @@
 <a href="../">Back</a>
 
 <ul>
+    <li>python/basics</li>
+    <li>python/flask</li>
+    <li>python/lists/</li>
+    <li>python/madlibs/</li>
+    <li>python/oop/</li>
+    <li>python/tkinter</li>
     <li><strong></strong> - <a href="#" target="blank">Local</a> </li>
 </ul>
     

ced7ff324572ecf339a159b4cf60b06ebea33246
Jonathan Grieve <mail@jonniegrieve.co.uk>
2024-08-29 12:29:21 +0100

    introduce a project list page

--- regex/index.php
+++ regex/index.php
@@ -1,15 +1,40 @@
-<?php 
-    require "sections/header.php";
-    require "sections/specific.php";
-    require "sections/ranges.php";
-    require "sections/wildcard.php";   
-    require "sections/repeated.php";  
-    require "sections/excluding.php";
-    require "sections/alternation.php";
-    require "sections/groups.php";
-    require "sections/beginning-end.php"; 
-    require "sections/footer.php";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-?>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-N9WB623MVP"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
 
+        gtag('config', 'G-N9WB623MVP');
+    </script>
+
+    <link rel="stylesheet" tyoe="text/css" href="../../style.css">    
+
+    <title>JGDM #100daysOfCode</title>
+
+</head>
+
+<body>
+
+        <!-- TODO: build this list and use this page as homepage -->
+
+        <section>
+      
+      <h1>&quot;100DaysOfCode&quot; Regex Project List <a id="goback" href="../">Back</a> </h1> 
+
+
+        <ul>
+            <li>regex/regex_one - <a href="regex_one/" target="blank">Local</a> </li>
+        </ul>
+ 
+    
+</body>
+</html>
 
---
+++ regex/regex_one/index.php
@@ -0,0 +1,15 @@
+<?php 
+    require "sections/header.php";
+    require "sections/specific.php";
+    require "sections/ranges.php";
+    require "sections/wildcard.php";   
+    require "sections/repeated.php";  
+    require "sections/excluding.php";
+    require "sections/alternation.php";
+    require "sections/groups.php";
+    require "sections/beginning-end.php"; 
+    require "sections/footer.php";
+
+?>
+
+

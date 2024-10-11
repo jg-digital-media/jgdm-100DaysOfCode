8c6388b30f1448540e83bd110e4094bc35b8dfa0
Jonathan Grieve <mail@jonniegrieve.co.uk>
2024-10-11 15:57:38 +0100

    add to repo alternative boilerplate php site

---
+++ php/php_boilerplate_alt/index.php
@@ -0,0 +1,27 @@
+<?php
+
+    //vars 
+    $page_name = "home";
+
+    $meta_description = "";
+    $meta_keywords = "";
+    $meta_image = "";
+
+    $page_url = "";
+    $page_title = "Page Home";
+    $website_title= "PHP Website Starter";
+
+?>
+
+    
+<?php require "inc/header.php";  ?>
+
+    <main>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor odit qui ducimus adipisci ratione consequatur officiis, hic pariatur mollitia voluptatibus soluta deserunt eveniet sed delectus nihil dignissimos eligendi iste. Dolor!</p>
+    </main>
+
+    
+<?php require "inc/footer.php"; ?>
+
+</body>
+</html>

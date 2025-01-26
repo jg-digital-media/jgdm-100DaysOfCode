a6ab846cdd87a48f9d63f6b444009aaabfb95925
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-01-26 19:07:10 +0000

    plan new project

---
+++ ai/work_counter/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Work Counter</title>
+</head>
+<body>
+    
+
+    TODO: Type in Hourly rate into a text box and press enter to store number. [£00.00]
+
+    TODO: Add a new text box or element that contains a timer in minutes and seconds counting up from 0.  [00:00:00]
+
+    TODO: Above the text box/element there should be another element that dynamically shows us the growing number for the fee due based on the timer. It should react to the timer on display. [£00.00]
+    
+    TODO: a link that serves as a button to start the timer. 
+    TODO: a link that serves as a button to pause the timer.
+    TODO: a link that serves as a button to reset the timer.
+
+    <h1>Work Counter - index.php</h1>
+
+</body>
+</html>

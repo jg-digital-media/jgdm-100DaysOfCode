4d9405a08393152a291b797611ed4669e9726f38
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-01-28 14:50:10 +0000

    new page - autism traits

---
+++ css/autism_traits/index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+
+    <!-- Google Fonts-->
+
+    <link rel="stylesheet" type="text/css" href="media/slick/slick-theme.css" />
+    <link rel="stylesheet" type="text/css" href="media/slick/slick.css" />
+
+    
+    <!-- meta tags -->
+    <meta name="description" content="">
+    <meta name="keywords" content=""> 
+    <meta name="image" content="">
+
+    <!-- FACEBOOK: Open Graph -->
+    <meta property="og:title" content="">
+    <meta property="og:description" content="">
+    <meta property="og:image" content="">
+    <meta property="og:url" content="">
+
+    <!-- TWITTER: Open Graph -->
+    <meta name="twitter:title" content="">
+    <meta name="twitter:description" content="">
+    <meta name="twitter:image" content="">
+    <meta name="twitter:card" content="">
+
+    <!-- Favicon -->
+    <link rel="icon" href="favicon.png" type="image/png">
+
+    <!-- Canonical link -->
+    <link rel="canonical" href="">
+
+    <!-- CSS Stylesheets -->
+    <link rel="stylesheet" type="text/css" href="style.css" />
+
+    <title>My Traits in my Autism</title>
+</head>
+<body>
+    
+        <h1>My Traits in my Autism</h1>
+
+
+        <div class="intro">
+            <p>On the 28th January 2025, I received my formal diagnsis of Autism.  It's something I think I've known for a long time but only really started the journey for a year.  It feels great to have this formally confirmed.  On this page I want to lay out how Autism manifests in me. </p>
+        </div>
+
+        <div class="traits">
+            <div class="trait">
+                <h2>I am a person who is very self-conscious</h2>
+                <p>I am very self-conscious and I have a hard time being myself. I tend to be very critical of myself and I don't like to admit that I'm wrong.</p>
+            </div>
+            <div class="trait">
+                <h2>I am a person who is very shy</h2>
+                <p>I am very shy and I tend to be very quiet. I don't like to talk to people and I tend to keep to myself.</p>
+            </div>
+            
+        </div> 
+                
+        <div class="resources">
+            <h2>Resources</h2>
+            <p>I've included some resources below that I found helpful in my journey with Autism.</p>
+            <ul>
+                <li><a href="https://www.autism.org.uk/about-autism/what-is-autism" target="_blank">What is Autism?</a></li>
+                <li><a href="https://www.autism.org.uk/about-autism/autism-and-the-law" target="_blank">Autism and the Law</a></li>
+                <li><a href="https://www.autism.org.uk/support-for-autism/resources-and-support" target="_blank">Resources and Support</a></li>
+                <li><a href="https://www.autism.org.uk/support-for-autism/help-for-parents" target="_blank">Help for Parents</a></li>
+                <li><a href="https://www.autism.org.uk/support-for-autism/help-for-carers" target="_blank">Help for Carers</a></li>
+                <li><a href="https://www.autism.org.uk/support-for-autism/help-for-friends-and-family" target="_blank">Help for Friends and Family</a></li>
+                <li><a href="https://www.autism.org.uk/support-for-autism/help-for-teachers" target="_blank">Help for Teachers</a></li>
+                <li><a href="https://www.autism.org.uk/support-for-autism/help-for-students" target="_blank">Help for Students</a></li>
+                <li><a href="https://www.autism.org.uk/support-for-autism/help-for-health-professionals" target="_blank">Help for Health Professionals</a></li>
+                <li><a href="https://www.autism.org.uk/support-for-autism/help-for-people-with-learning-disabilities" target="_blank">Help for People with Learning Disabilities</a></li>
+            </ul>
+        </div>
+
+        <footer>
+            <p><a href="#top">Back to Top</a> &copy; JGDM: <a href="https://www.jonniegrieve.co.uk">Jonnie Grieve Digital Media</a>.</p>
+        </footer>
+
+
+    <!-- <script src="scripts/modernizr-all.js" type="text/javascript"></script>
+    <script src="scripts/jquery.js" type="text/javascript"></script>
+    <script src="media/slick/slick.js" type="text/javascript"></script>
+    <script src="scripts/app.js" type="text/javascript"></script> -->
+
+</body>
+</html>

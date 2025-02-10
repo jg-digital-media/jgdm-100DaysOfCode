8c9d3af20c22ef53e43d454a2887472458fb2bc9
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-02-10 20:32:46 +0000

    score list template files

---
+++ ai/score_comparator/assets/template-parts/home-scores.php
@@ -0,0 +1,112 @@
+<table>
+    <tr>
+        <th>
+            <h2>Team Name</h2>
+            <div class="score">
+                <p>0</p>
+            </div>
+        </th>
+        <th>v</th>
+        <th>
+            <h2>Opponent</h2>
+            <div class="score">
+                <p>0</p>
+            </div>
+        </th>
+    </tr>
+    <tr>
+        <td>Team Name</td>
+        <td>v</td>
+        <td>AFC Bournemouth</td>
+    </tr>
+    <tr>
+        <td>Team Name</td>
+        <td>v</td>
+        <td>Arsenal</td>
+    </tr>
+    <tr>
+        <td>Team Name</td>
+        <td>v</td>
+        <td>Aston Villa</td>
+    </tr>
+    <tr>
+        <td>Team Name</td>
+        <td>v</td>
+        <td>Brentford</td>
+    </tr>
+    <tr>
+        <td>Team Name</td>
+        <td>v</td>
+        <td>Brighton & Hove Albion</td>
+    </tr>
+    <tr>
+        <td>Team Name</td>
+        <td>v</td>
+        <td>Crystal Palace</td>
+    </tr>
+    <tr>
+        <td>Team Name</td>
+        <td>v</td>
+        <td>Chelsea</td>
+    </tr>
+    <tr>
+        <td>Team Name</td>
+        <td>v</td>
+        <td>Everton</td>
+    </tr>
+    <tr>
+        <td>Team Name</td>
+        <td>v</td>
+        <td>Fulham</td>
+    </tr>
+    <tr>
+        <td>Team Name</td>
+        <td>v</td>
+        <td>Ipswich</td>
+    </tr>
+    <tr>
+        <td>Team Name</td>
+        <td>v</td>
+        <td>Liverpool</td>
+    </tr>
+    <tr>
+        <td>Team Name</td>
+        <td>v</td>
+        <td>Leicester City</td>
+    </tr>
+    <tr>
+        <td>Team Name</td>
+        <td>v</td>
+        <td>Manchester City</td>
+    </tr>
+    <tr>
+        <td>Team Name</td>
+        <td>v</td>
+        <td>Manchester United</td>
+    </tr>
+    <tr>
+        <td>Team Name</td>
+        <td>v</td>
+        <td>Nottingham Forest</td>
+    </tr>
+    <tr>
+        <td>Team Name</td>
+        <td>v</td>
+        <td>Southampton</td>
+    </tr>
+    <tr>
+        <td>Team Name</td>
+        <td>v</td>
+        <td>Tottenham Hotspur</td>
+    </tr>
+    <tr>
+        <td>Team Name</td>
+        <td>v</td>
+        <td>West Ham United</td>
+    </tr>
+    <tr>
+        <td>Team Name</td>
+        <td>v</td>
+        <td>Wolverhampton Wanderers</td>
+    </tr>
+</table>

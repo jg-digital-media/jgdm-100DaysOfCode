de480d730dfe5eb112c5c7caebdb546b5bba452b
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-02-11 15:59:34 +0000

    update home scores table template

--- ai/score_comparator/assets/template-parts/home-scores.php
+++ ai/score_comparator/assets/template-parts/home-scores.php
@@ -1,112 +1,135 @@
 <table>
     <tr>
-        <th>
-            <h2>Team Name</h2>
-            <div class="score">
-                <p>0</p>
-            </div>
-        </th>
-        <th>v</th>
-        <th>
-            <h2>Opponent</h2>
-            <div class="score">
-                <p>0</p>
-            </div>
-        </th>
+        <th id="column---home--team">Home</th>
+        <th id="column---home--score" class="score---home">Score</th>
+        <th id="column---versus">v</th>
+        <th id="column---away--team">Opponent</th>
+        <th id="column---away--score" class="score---away">Score</th>
     </tr>
-    <tr>
-        <td>Team Name</td>
+    <tr class="score---compares--default">
+        <td>Ipswich Town</td>
+        <td>N</td>
         <td>v</td>
         <td>AFC Bournemouth</td>
+        <td>N</td>
     </tr>
-    <tr>
-        <td>Team Name</td>
+    <tr class="score---compares--higher">
+        <td>Ipswich Town</td>
+        <td>0</td>
         <td>v</td>
         <td>Arsenal</td>
+        <td>0</td>
     </tr>
-    <tr>
-        <td>Team Name</td>
+    <tr class="score---compares--higher">
+        <td>Ipswich Town</td>
+        <td>0</td>
         <td>v</td>
         <td>Aston Villa</td>
+        <td>0</td>
     </tr>
-    <tr>
-        <td>Team Name</td>
+    <tr class="score---compares--lower">
+        <td>Ipswich Town</td>
+        <td>0</td>
         <td>v</td>
         <td>Brentford</td>
+        <td>0</td>
     </tr>
-    <tr>
-        <td>Team Name</td>
+    <tr class="score---compares--lower">
+        <td>Ipswich Town</td>
+        <td>0</td>
         <td>v</td>
         <td>Brighton & Hove Albion</td>
+        <td>0</td>
     </tr>
-    <tr>
-        <td>Team Name</td>
+    <tr class="score---compares--equal">
+        <td>Ipswich Town</td>
+        <td>0</td>
         <td>v</td>
         <td>Crystal Palace</td>
+        <td>0</td>
     </tr>
-    <tr>
-        <td>Team Name</td>
+    <tr class="score---compares--higher">
+        <td>Ipswich Town</td>
+        <td>0</td>
         <td>v</td>
         <td>Chelsea</td>
+        <td>0</td>
     </tr>
-    <tr>
-        <td>Team Name</td>
+    <tr class="score---compares--higher">
+        <td>Ipswich Town</td>
+        <td>0</td>
         <td>v</td>
         <td>Everton</td>
+        <td>0</td>
     </tr>
-    <tr>
-        <td>Team Name</td>
+    <tr class="score---compares--higher">
+        <td>Ipswich Town</td>
+        <td>0</td>
         <td>v</td>
         <td>Fulham</td>
+        <td>0</td>
     </tr>
-    <tr>
-        <td>Team Name</td>
-        <td>v</td>
-        <td>Ipswich</td>
-    </tr>
-    <tr>
-        <td>Team Name</td>
+    <tr class="score---compares--default">
+        <td>Ipswich Town</td>
+        <td>N</td>
         <td>v</td>
         <td>Liverpool</td>
+        <td>N</td>
     </tr>
-    <tr>
-        <td>Team Name</td>
+    <tr class="score---compares--equal">
+        <td>Ipswich Town</td>
+        <td>0</td>
         <td>v</td>
         <td>Leicester City</td>
+        <td>0</td>
     </tr>
-    <tr>
-        <td>Team Name</td>
+    <tr class="score---compares--higher">
+        <td>Ipswich Town</td>
+        <td>0</td>
         <td>v</td>
         <td>Manchester City</td>
+        <td>0</td>
     </tr>
-    <tr>
-        <td>Team Name</td>
+    <tr class="score---compares--default">
+        <td>Ipswich Town</td>
+        <td>N</td>
         <td>v</td>
         <td>Manchester United</td>
+        <td>N</td>
     </tr>
-    <tr>
-        <td>Team Name</td>
+    <tr class="score---compares--lower">
+        <td>Ipswich Town</td>
+        <td>0</td>
         <td>v</td>
         <td>Nottingham Forest</td>
+        <td>0</td>
     </tr>
-    <tr>
-        <td>Team Name</td>
+    <tr class="score---compares--lower">
+        <td>Ipswich Town</td>
+        <td>0</td>
         <td>v</td>
         <td>Southampton</td>
+        <td>0</td>
     </tr>
-    <tr>
-        <td>Team Name</td>
+    <tr class="score---compares--lower">
+        <td>Ipswich Town</td>
+        <td>0</td>
         <td>v</td>
         <td>Tottenham Hotspur</td>
+        <td>0</td>
     </tr>
-    <tr>
-        <td>Team Name</td>
+    <tr class="score---compares--higher">
+        <td>Ipswich Town</td>
+        <td>0</td>
         <td>v</td>
         <td>West Ham United</td>
+        <td>0</td>
     </tr>
-    <tr>
-        <td>Team Name</td>
+    <tr class="score---compares--default">
+        <td>Ipswich Town</td>
+        <td>N</td>
         <td>v</td>
         <td>Wolverhampton Wanderers</td>
+        <td>N</td>
     </tr>
 </table>

f2192dac30b037724a51ac9a4e8ba6cbbce767a9
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-02-11 16:45:54 +0000

    add paragraphs for about page

--- ai/score_comparator/about.php
+++ ai/score_comparator/about.php
@@ -7,7 +7,17 @@
 </head>
 <body>
     
-    <p>about.php</p>
+    <h1>About this App</h1>
+
+    <p>Development for this app begun on the 10th February 2024.</p>
     
+    <p>It has been a brainchild of mine for many years. It was an idea I came up with during, ironically a Web Accessibility module for web design at University.</p> 
+
+    <p>However I knew I didn't have the &quot;spoons&quot; to create it on my own. I don't say that to do myself down. When I say I don't have the &quot;spoons&quot; I mean that I don't have the knowledge or skills; the power or the patience in my head to sit down to be able to create it when I think about the enormity, time and effort it would take.</p>
+
+    <p>Then the age of AI came. I say that as if I'm thinking about it lightly, but I don't. ....</p>
+
+    <p>about.php</p>
+
 </body>
 </html>

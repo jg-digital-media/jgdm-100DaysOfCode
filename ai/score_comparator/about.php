4894d5a80aad264582aa15c5b35e59b66400a6cc
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-02-13 16:49:03 +0000

    add note about scripts

--- ai/score_comparator/about.php
+++ ai/score_comparator/about.php
@@ -71,7 +71,7 @@
 
         <p>However I knew I didn't have the &quot;spoons&quot; to create it on my own. I don't say that to do myself down. When I say I don't have the &quot;spoons&quot; I mean that I don't have the knowledge or skills; the power or the patience in my head to sit down to be able to create it when I think about the enormity, time and effort it would take.</p>
 
-        <p>Then the age of AI came. I say that as if I'm thinking about it lightly, but I don't. ....</p>
+        <p>Then the age of AI came. I say that as if I'm thinking about it lightly, but I don't. I have learning difficulties and Autism. So even with AI in my arsenal it still seemed like a daunting task. Especially with an application like this that has many moving parts like this one.  With the AI, my prompting and <code>claude-3.5-sonnnet</code>'s responses, I was closer than even I thought to even making a start.
 
         <?php date_default_timezone_set('Europe/London'); ?>
         <p>I've been working on this app for the last 10 years.</p> 

445b364e215a9c6f202ee28a3ae8f38577cba418
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-02-14 12:59:51 +0000

    add sql development scripts

---
+++ ai/score_comparator/assets/data/script.sql
@@ -0,0 +1,407 @@
+/* Last Update: 14/02/2025 - 12:40 */
+
+-- Create Match Tables
+CREATE TABLE bournemouth_home_matches (
+    id INTEGER PRIMARY KEY,
+    home_team TEXT NOT NULL,
+    home_score INTEGER,
+    away_team TEXT NOT NULL,
+    away_score INTEGER
+);
+
+CREATE TABLE arsenal_home_matches (
+    id INTEGER PRIMARY KEY,
+    home_team TEXT NOT NULL,
+    home_score INTEGER,
+    away_team TEXT NOT NULL,
+    away_score INTEGER
+);
+
+CREATE TABLE astonvilla_home_matches (
+    id INTEGER PRIMARY KEY,
+    home_team TEXT NOT NULL,
+    home_score INTEGER,
+    away_team TEXT NOT NULL,
+    away_score INTEGER
+);
+
+CREATE TABLE brentford_home_matches (
+    id INTEGER PRIMARY KEY,
+    home_team TEXT NOT NULL,
+    home_score INTEGER,
+    away_team TEXT NOT NULL,
+    away_score INTEGER
+);
+
+CREATE TABLE chelsea_home_matches (
+    id INTEGER PRIMARY KEY,
+    home_team TEXT NOT NULL,
+    home_score INTEGER,
+    away_team TEXT NOT NULL,
+    away_score INTEGER
+);
+
+CREATE TABLE crystalpalace_home_matches (
+    id INTEGER PRIMARY KEY,
+    home_team TEXT NOT NULL,
+    home_score INTEGER,
+    away_team TEXT NOT NULL,
+    away_score INTEGER
+);
+
+CREATE TABLE everton_home_matches (
+    id INTEGER PRIMARY KEY,
+    home_team TEXT NOT NULL,
+    home_score INTEGER,
+    away_team TEXT NOT NULL,
+    away_score INTEGER
+);
+
+CREATE TABLE fulham_home_matches (
+    id INTEGER PRIMARY KEY,
+    home_team TEXT NOT NULL,
+    home_score INTEGER,
+    away_team TEXT NOT NULL,
+    away_score INTEGER
+);
+
+CREATE TABLE ipswich_home_matches (
+    id INTEGER PRIMARY KEY,
+    home_team TEXT NOT NULL,
+    home_score INTEGER,
+    away_team TEXT NOT NULL,
+    away_score INTEGER
+);
+
+CREATE TABLE liverpool_home_matches (
+    id INTEGER PRIMARY KEY,
+    home_team TEXT NOT NULL,
+    home_score INTEGER,
+    away_team TEXT NOT NULL,
+    away_score INTEGER
+);
+
+CREATE TABLE leicester_home_matches (
+    id INTEGER PRIMARY KEY,
+    home_team TEXT NOT NULL,
+    home_score INTEGER,
+    away_team TEXT NOT NULL,
+    away_score INTEGER
+);
+
+CREATE TABLE manchestercity_home_matches (
+    id INTEGER PRIMARY KEY,
+    home_team TEXT NOT NULL,
+    home_score INTEGER,
+    away_team TEXT NOT NULL,
+    away_score INTEGER
+);
+
+CREATE TABLE manchesterunited_home_matches (
+    id INTEGER PRIMARY KEY,
+    home_team TEXT NOT NULL,
+    home_score INTEGER,
+    away_team TEXT NOT NULL,
+    away_score INTEGER
+);
+
+CREATE TABLE nottinghamforest_home_matches (
+    id INTEGER PRIMARY KEY,
+    home_team TEXT NOT NULL,
+    home_score INTEGER,
+    away_team TEXT NOT NULL,
+    away_score INTEGER
+);
+
+CREATE TABLE southampton_home_matches (
+    id INTEGER PRIMARY KEY,
+    home_team TEXT NOT NULL,
+    home_score INTEGER,
+    away_team TEXT NOT NULL,
+    away_score INTEGER
+);
+
+CREATE TABLE spurs_home_matches (
+    id INTEGER PRIMARY KEY,
+    home_team TEXT NOT NULL,
+    home_score INTEGER,
+    away_team TEXT NOT NULL,
+    away_score INTEGER
+);
+
+CREATE TABLE westham_home_matches (
+    id INTEGER PRIMARY KEY,
+    home_team TEXT NOT NULL,
+    home_score INTEGER,
+    away_team TEXT NOT NULL,
+    away_score INTEGER
+);
+
+CREATE TABLE wolverhampton_home_matches (
+    id INTEGER PRIMARY KEY,
+    home_team TEXT NOT NULL,
+    home_score INTEGER,
+    away_team TEXT NOT NULL,
+    away_score INTEGER
+);
+
+CREATE TABLE bournemouth_away_matches (    
+    id INTEGER PRIMARY KEY,
+    home_team TEXT NOT NULL,
+    home_score INTEGER,
+    away_team TEXT NOT NULL,
+    away_score INTEGER
+
+);
+
+CREATE TABLE arsenal_away_matches (    
+    id INTEGER PRIMARY KEY,
+    home_team TEXT NOT NULL,
+    home_score INTEGER,
+    away_team TEXT NOT NULL,
+    away_score INTEGER
+
+);
+
+CREATE TABLE astonvilla_away_matches (    
+    id INTEGER PRIMARY KEY,
+    home_team TEXT NOT NULL,
+    home_score INTEGER,
+    away_team TEXT NOT NULL,
+    away_score INTEGER
+
+);
+
+CREATE TABLE brentford_away_matches (    
+    id INTEGER PRIMARY KEY,
+    home_team TEXT NOT NULL,
+    home_score INTEGER,
+    away_team TEXT NOT NULL,
+    away_score INTEGER
+
+);
+
+CREATE TABLE brighton_away_matches (    
+    id INTEGER PRIMARY KEY,
+    home_team TEXT NOT NULL,
+    home_score INTEGER,
+    away_team TEXT NOT NULL,
+    away_score INTEGER
+
+);
+
+CREATE TABLE chelsea_away_matches (    
+    id INTEGER PRIMARY KEY,
+    home_team TEXT NOT NULL,
+    home_score INTEGER,
+    away_team TEXT NOT NULL,
+    away_score INTEGER
+
+);
+
+CREATE TABLE crystalpalace_away_matches (    
+    id INTEGER PRIMARY KEY,
+    home_team TEXT NOT NULL,
+    home_score INTEGER,
+    away_team TEXT NOT NULL,
+    away_score INTEGER
+
+);
+
+CREATE TABLE everton_away_matches (    
+    id INTEGER PRIMARY KEY,
+    home_team TEXT NOT NULL,
+    home_score INTEGER,
+    away_team TEXT NOT NULL,
+    away_score INTEGER
+
+);
+
+CREATE TABLE fulham_away_matches (    
+    id INTEGER PRIMARY KEY,
+    home_team TEXT NOT NULL,
+    home_score INTEGER,
+    away_team TEXT NOT NULL,
+    away_score INTEGER
+
+);
+
+CREATE TABLE ipswich_away_matches (    
+    id INTEGER PRIMARY KEY,
+    home_team TEXT NOT NULL,
+    home_score INTEGER,
+    away_team TEXT NOT NULL,
+    away_score INTEGER
+
+);
+
+CREATE TABLE liverpool_away_matches (    
+    id INTEGER PRIMARY KEY,
+    home_team TEXT NOT NULL,
+    home_score INTEGER,
+    away_team TEXT NOT NULL,
+    away_score INTEGER
+
+);
+
+CREATE TABLE leicester_away_matches (    
+    id INTEGER PRIMARY KEY,
+    home_team TEXT NOT NULL,
+    home_score INTEGER,
+    away_team TEXT NOT NULL,
+    away_score INTEGER
+
+);
+
+CREATE TABLE manchestercity_away_matches (    
+    id INTEGER PRIMARY KEY,
+    home_team TEXT NOT NULL,
+    home_score INTEGER,
+    away_team TEXT NOT NULL,
+    away_score INTEGER
+
+);
+
+CREATE TABLE manchesterunited_away_matches (    
+    id INTEGER PRIMARY KEY,
+    home_team TEXT NOT NULL,
+    home_score INTEGER,
+    away_team TEXT NOT NULL,
+    away_score INTEGER
+
+);
+
+CREATE TABLE nottinghamforest_away_matches (    
+    id INTEGER PRIMARY KEY,
+    home_team TEXT NOT NULL,
+    home_score INTEGER,
+    away_team TEXT NOT NULL,
+    away_score INTEGER
+
+);
+
+CREATE TABLE southampton_away_matches (    
+    id INTEGER PRIMARY KEY,
+    home_team TEXT NOT NULL,
+    home_score INTEGER,
+    away_team TEXT NOT NULL,
+    away_score INTEGER
+
+);
+
+CREATE TABLE spurs_away_matches (    
+    id INTEGER PRIMARY KEY,
+    home_team TEXT NOT NULL,
+    home_score INTEGER,
+    away_team TEXT NOT NULL,
+    away_score INTEGER
+
+);
+
+CREATE TABLE westham_away_matches (    
+    id INTEGER PRIMARY KEY,
+    home_team TEXT NOT NULL,
+    home_score INTEGER,
+    away_team TEXT NOT NULL,
+    away_score INTEGER
+
+);
+
+CREATE TABLE wolverhampton_away_matches (    
+    id INTEGER PRIMARY KEY,
+    home_team TEXT NOT NULL,
+    home_score INTEGER,
+    away_team TEXT NOT NULL,
+    away_score INTEGER
+
+);
+
+-- Create Base Scores Tables
+
+CREATE TABLE base_scores_home (
+
+    id INTEGER PRIMARY KEY,
+    home_team TEXT NOT NULL,
+    away_team TEXT NOT NULL,
+    home_score INTEGER,
+    away_score INTEGER,
+    played INTEGER
+);
+
+CREATE TABLE base_scores_away (
+
+    id INTEGER PRIMARY KEY,
+    away_team TEXT NOT NULL,
+    home_team TEXT NOT NULL,
+    away_score INTEGER,
+    home_score INTEGER,
+    played INTEGER
+);
+
+
+
+-- Base Scores for home reports
+INSERT INTO base_scores_home (home_team, away_team, home_score, away_score, played) VALUES
+    ('AFC Bournemouth', 'Newcastle United', 2, 3, 1),
+    ('Arsenal', 'Newcastle United', 0, 0, 0),
+    ('Aston Villa', 'Newcastle United', 0, 0, 0),
+    ('Brentford', 'Newcastle United', 0, 0, 0),
+    ('Brighton and Hove Albion', 'Newcastle United', 0, 0, 0),
+    ('Chelsea', 'Newcastle United', 0, 0, 0),
+    ('Crystal Palace', 'Newcastle United', 0, 0, 0),
+    ('Everton', 'Newcastle United', 0, 0, 0),
+    ('Fulham', 'Newcastle United', 0, 0, 0),
+    ('Ipswich Town', 'Newcastle United', 0, 0, 0),
+    ('Leicester City', 'Newcastle United', 0, 0, 0),
+    ('Liverpool', 'Newcastle United', 0, 0, 0),
+    ('Manchester City', 'Newcastle United', 0, 0, 0),
+    ('Manchester United', 'Newcastle United', 0, 0, 0),
+    ('Nottingham Forest', 'Newcastle United', 0, 0, 0),
+    ('Southampton', 'Newcastle United', 0, 0, 0),
+    ('Tottenham Hotspur', 'Newcastle United', 0, 0, 0),
+    ('West Ham United', 'Newcastle United', 0, 0, 0),
+    ('Wolverhampton Wanderers', 'Newcastle United', 0, 0, 0);   
+
+-- Base Scores for away reports
+INSERT INTO base_scores_away (away_team, home_team, away_score, home_score, played) VALUES
+    ('Newcastle United', 'AFC Bournemouth', 3, 2, 1),
+    ('Newcastle United', 'Arsenal', 0, 0, 0),
+    ('Newcastle United', 'Aston Villa', 0, 0, 0),
+    ('Newcastle United', 'Brentford', 0, 0, 0),
+    ('Newcastle United', 'Brighton and Hove Albion', 0, 0, 0),
+    ('Newcastle United', 'Chelsea', 0, 0, 0),
+    ('Newcastle United', 'Crystal Palace', 0, 0, 0),
+    ('Newcastle United', 'Everton', 0, 0, 0),
+    ('Newcastle United', 'Fulham', 0, 0, 0),
+    ('Newcastle United', 'Ipswich Town', 0, 0, 0),
+    ('Newcastle United', 'Leicester City', 0, 0, 0),
+    ('Newcastle United', 'Liverpool', 0, 0, 0),
+    ('Newcastle United', 'Manchester City', 0, 0, 0),
+    ('Newcastle United', 'Manchester United', 0, 0, 0),
+    ('Newcastle United', 'Nottingham Forest', 0, 0, 0),
+    ('Newcastle United', 'Southampton', 0, 0, 0),
+    ('Newcastle United', 'Tottenham Hotspur', 0, 0, 0),
+    ('Newcastle United', 'West Ham United', 0, 0, 0),    
+    ('Newcastle United', 'Wolverhampton Wanderers', 0, 0, 0);
+
+-- Insert Match Comparison Score Data
+
+INSERT INTO bournemouth_home_matches (home_team, home_score, away_team, away_score) VALUES
+    ('AFC Bournemouth', 0, 'Arsenal', 0),
+    ('AFC Bournemouth', 0, 'Aston Villa', 0),
+    ('AFC Bournemouth', 0, 'Brentford', 0),
+    ('AFC Bournemouth', 0, 'Brighton and Hove Albion', 0),
+    ('AFC Bournemouth', 0, 'Chelsea', 0),
+    ('AFC Bournemouth', 0, 'Crystal Palace', 0),
+    ('AFC Bournemouth', 0, 'Everton', 0),
+    ('AFC Bournemouth', 0, 'Fulham', 0),
+    ('AFC Bournemouth', 0, 'Ipswich Town', 0),
+    ('AFC Bournemouth', 0, 'Leicester City', 0),
+    ('AFC Bournemouth', 0, 'Liverpool', 0),
+    ('AFC Bournemouth', 0, 'Manchester City', 0),
+    ('AFC Bournemouth', 0, 'Manchester United', 0),
+    ('AFC Bournemouth', 0, 'Nottingham Forest', 0),
+    ('AFC Bournemouth', 0, 'Southampton', 0),
+    ('AFC Bournemouth', 0, 'Tottenham Hotspur', 0),
+    ('AFC Bournemouth', 0, 'West Ham United', 0),
+    ('AFC Bournemouth', 0, 'Wolverhampton Wanderers', 0);

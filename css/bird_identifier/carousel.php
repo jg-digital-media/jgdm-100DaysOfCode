33d9552ed2d7a5ca2c67f918affa4bb2592b69c5
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-02-27 12:52:02 +0000

    basic navigation

--- css/bird_identifier/carousel.php
+++ css/bird_identifier/carousel.php
@@ -38,32 +38,19 @@
     <!-- CSS Stylesheets -->
     <link rel="stylesheet" type="text/css" href="style.css" />
 
-    <title>Home | Bird Identifier</title>
+    <title>Slider | Bird Identifier</title>
 </head>
 <body>
 
     <h1>Bird Identifier</h1>
 
-    <p>index.php</p>    
-
+    <p>carousel.php</p>
+    
     <ul>
         <li><a href="index.php">Home</a></li>
         <li><a href="carousel.php">Carousel</a></li>
         <li><a href="filterable.php">Filterable</a></li>
     </ul>
-
-    <!--     
-        https://jgdm-projects.s3.eu-west-2.amazonaws.com/bird_identifier/IMG_1701.JPG
-        https://jgdm-projects.s3.eu-west-2.amazonaws.com/bird_identifier/IMG_1692.JPG
-        https://jgdm-projects.s3.eu-west-2.amazonaws.com/bird_identifier/IMG_1492.JPG
-        https://jgdm-projects.s3.eu-west-2.amazonaws.com/bird_identifier/IMG_1417.JPG
-        https://jgdm-projects.s3.eu-west-2.amazonaws.com/bird_identifier/IMG_0870.JPG
-        https://jgdm-projects.s3.eu-west-2.amazonaws.com/bird_identifier/IMG_0859.JPG
-        https://jgdm-projects.s3.eu-west-2.amazonaws.com/bird_identifier/IMG_0710.JPG
-        https://jgdm-projects.s3.eu-west-2.amazonaws.com/bird_identifier/IMG_0703.JPG
-        https://jgdm-projects.s3.eu-west-2.amazonaws.com/bird_identifier/IMG_0586.JPG
-        https://jgdm-projects.s3.eu-west-2.amazonaws.com/bird_identifier/IMG_0173.JPG
-        https://jgdm-projects.s3.eu-west-2.amazonaws.com/bird_identifier/IMG_0142.JPG
-    -->
+    
 </body>
-</html>
+</html

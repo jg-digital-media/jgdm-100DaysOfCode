06b70d9c2870617c928864514487cb60170ced2d
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-02-28 15:58:02 +0000

    apply slick carousel with integrated captions

--- css/bird_identifier/assets/scripts/app.js
+++ css/bird_identifier/assets/scripts/app.js
@@ -1,4 +1,4 @@
-console.log("app.js connected! - 28-02-2025 - 13:39");
+console.log("app.js connected! - 28-02-2025 - 15:32");
 
 $(document).ready(function() {
 
@@ -13,5 +13,18 @@ $(document).ready(function() {
         cssEase: 'linear',
         dots: false,
         arrows: false
+    });
+
+    $('.birds---carousel--container').slick({
+
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        draggable: true,
+        autoplaySpeed: 2000,
+        infinite: true,
+        cssEase: 'linear',
+        dots: false,
+        arrows: true
     });
 });
--- css/bird_identifier/carousel.php
+++ css/bird_identifier/carousel.php
@@ -22,7 +22,12 @@
     <meta name="twitter:title" content="Bird Carousel | Bird Identifier">
     <meta name="twitter:description" content="This website can help you identify the bird species that you see when you're out and about.">
     <meta name="twitter:image" content="https://projects.jonniegrieve.co.uk/bird_identifier/bird.png">
-    <meta name="twitter:card" content="jgdm_bird_identifier">   
+    <meta name="twitter:card" content="jgdm_bird_identifier">      
+
+    <!-- Slick Styling -->
+    <link rel="stylesheet" type="text/css" href ="assets/slick/slick.css" />
+    <link rel="stylesheet" type="text/css" href ="assets/slick/slick-theme.css" /> 
+
     <!-- Favicon -->
     <link rel="icon" href="favicon.png" type="image/png">
 
@@ -36,8 +41,60 @@
 </head>
 <body>
 
-    <h1>Bird Identifier</h1>
+    <header>
+
+        <h1>Bird Identifier</h1>
         
+    </header>
+
+    <main id="container">
+    
+        <a href="index.php" id="back---home">Back to Homepage</a>
+
+        <h2>Bird Carousel</h2>
+
+        <!-- <section>Use this space that you can use to identify the bird species that you see when you're out and about.</section> -->
+
+        <!-- Slick Carousel -->  
+        <div class="birds---carousel--container">
+            <div class="slide">
+                <img src="https://jgdm-projects.s3.eu-west-2.amazonaws.com/bird_identifier/IMG_1701.JPG" alt="Robin">
+                <div class="slide-caption">
+                    <p class="filename">IMG_1701.JPG</p>
+                    <h3 class="bird---name">Robin</h3>
+                    <p class="date">Date: 00/00/0000</p>
+                </div>
+            </div>
+            <div class="slide">
+                <img src="https://jgdm-projects.s3.eu-west-2.amazonaws.com/bird_identifier/IMG_1692.JPG" alt="Cormorant">
+                <div class="slide-caption">
+                    <p class="filename">IMG_1692.JPG</p>
+                    <h3 class="bird---name">Cormorant</h3>
+                    <p class="date">Date: 00/00/0000</p>
+                </div>
+            </div>
+            <div class="slide">
+                <img src="https://jgdm-projects.s3.eu-west-2.amazonaws.com/bird_identifier/IMG_1419.JPG" alt="House Sparrow">
+                <div class="slide-caption">
+                    <p class="filename">IMG_1419.JPG</p>
+                    <h3>House Sparrow</h3>
+                    <p class="date">Date: 00/00/0000</p>
+                </div>
+            </div>
+        </div>
+
+        
+        <!-- nav links -->
+        <ul class="nav---container">
+
+            <!-- <li><a href="index.php">Home</a></li> -->
+            <li><a href="carousel.php">Birds Carousel</a></li>
+            <li><a href="filterable.php">Filterable Birds List</a></li>
+
+        </ul>
+
+    </main>
+
     <!--
         https://jgdm-projects.s3.eu-west-2.amazonaws.com/bird_identifier/IMG_1701.JPG
         https://jgdm-projects.s3.eu-west-2.amazonaws.com/bird_identifier/IMG_1692.JPG
@@ -52,13 +109,17 @@
         https://jgdm-projects.s3.eu-west-2.amazonaws.com/bird_identifier/IMG_0142.JPG 
     -->
 
-    <p>carousel.php</p>
+    <p>carousel.php</p>    
+
     
-    <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="carousel.php">Carousel</a></li>
-        <li><a href="filterable.php">Filterable</a></li>
-    </ul>
+
+    
+    <!-- scripts -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script type="text/javascript" src="assets/slick/slick.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vanilla-lazyload@17.1.0/dist/lazyload.min.js"></script>
+
+    <script type="text/javascript" src="assets/scripts/app.js"></script>
     
 </body>
 </html

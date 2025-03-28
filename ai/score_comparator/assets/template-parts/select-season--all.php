8f3a439a62e3a0d68594709d942cc3a8f8e8abc4
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-03-28 16:50:19 +0000

    add select form in template parts

---
+++ ai/score_comparator/assets/template-parts/select-season--all.php
@@ -0,0 +1,39 @@
+<!-- select-season.php -->
+
+<!-- <select id="form---select--season" class="select---season">
+
+    <option class="select---season--option" id="option---2025" value="2025">2024/2025</option>
+    <option class="select---season--option" id="option---2024" value="2024">2023/2024</option>
+    <option class="select---season--option" id="option---2023" value="2023">2022/2023</option>
+    <option class="select---season--option" id="option---2022" value="2022">2021/2022</option>
+    <option class="select---season--option" id="option---2021" value="2021">2020/2021</option>
+    <option class="select---season--option" id="option---2020" value="2020">2019/2020</option>
+    <option class="select---season--option" id="option---2019" value="2019">2018/2019</option>
+    <option class="select---season--option" id="option---2018" value="2018">2017/2018</option>
+    <option class="select---season--option" id="option---2017" value="2017">2016/2017</option>
+    <option class="select---season--option" id="option---2016" value="2016">2015/2016</option>
+    <option class="select---season--option" id="option---2015" value="2015">2014/2015</option>
+    <option class="select---season--option" id="option---2014" value="2014">2013/2014</option>
+    <option class="select---season--option" id="option---2013" value="2013">2012/2013</option>
+    <option class="select---season--option" id="option---2012" value="2012">2011/2012</option>
+    <option class="select---season--option" id="option---2011" value="2011">2010/2011</option>
+    <option class="select---season--option" id="option---2010" value="2010">2009/2010</option>
+    <option class="select---season--option" id="option---2009" value="2009">2008/2009</option>
+    <option class="select---season--option" id="option---2008" value="2008">2007/2008</option>
+    <option class="select---season--option" id="option---2007" value="2007">2006/2007</option>
+    <option class="select---season--option" id="option---2006" value="2006">2005/2006</option>
+    <option class="select---season--option" id="option---2005" value="2005">2004/2005</option>
+    <option class="select---season--option" id="option---2004" value="2004">2003/2004</option>
+    <option class="select---season--option" id="option---2003" value="2003">2002/2003</option>
+    <option class="select---season--option" id="option---2002" value="2002">2001/2002</option>
+    <option class="select---season--option" id="option---2001" value="2001">2000/2001</option>
+    <option class="select---season--option" id="option---2000" value="2000">1999/2000</option>
+    <option class="select---season--option" id="option---1999" value="1999">1998/1999</option>
+    <option class="select---season--option" id="option---1998" value="1998">1997/1998</option>
+    <option class="select---season--option" id="option---1997" value="1997">1996/1997</option>
+    <option class="select---season--option" id="option---1996" value="1996">1995/1996</option>
+    <option class="select---season--option" id="option---1995" value="1995">1994/1995</option>
+    <option class="select---season--option" id="option---1994" value="1994">1993/1994</option>
+    <option class="select---season--option" id="season---1993" value="1993">1992/1993</option>
+    
+</select> -->
---
+++ ai/score_comparator/assets/template-parts/select-season.php
@@ -0,0 +1,7 @@
+<!-- select-season.php -->
+<!-- <select id="form---select--season" class="select---season">  
+
+    <option class="select---season--option" id="option---2025" value="2025">2024/2025</option>
+    <option class="select---season--option" id="season---1993" value="1993">1992/1993</option> 
+
+</select> -->

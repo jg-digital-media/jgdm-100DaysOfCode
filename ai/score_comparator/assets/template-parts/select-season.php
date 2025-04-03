33cd8caab7da78dac87eed4314bafc16d0bcbf3c
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-04-03 17:40:02 +0100

    implement season dropdown box

--- ai/score_comparator/assets/template-parts/select-season.php
+++ ai/score_comparator/assets/template-parts/select-season.php
@@ -1,4 +1,5 @@
 <!-- select-season.php -->
+<label for="form---select--season">Select Season:</label>
 <select id="form---select--season" class="select---season">  
 
     <option class="select---season--option" id="option---2025" value="2025">2024/2025</option>

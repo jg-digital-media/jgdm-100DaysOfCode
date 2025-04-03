06ed3c07a5d2935dd5234135a0b2f04ec4391900
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-04-03 13:59:24 +0100

    select seasons dropdown

--- ai/score_comparator/assets/template-parts/select-season.php
+++ ai/score_comparator/assets/template-parts/select-season.php
@@ -1,7 +1,7 @@
 <!-- select-season.php -->
-<!-- <select id="form---select--season" class="select---season">  
+<select id="form---select--season" class="select---season">  
 
     <option class="select---season--option" id="option---2025" value="2025">2024/2025</option>
-    <option class="select---season--option" id="season---1993" value="1993">1992/1993</option> 
+    <option class="select---season--option" id="season---2024" value="2024">2023/2024</option> 
 
-</select> -->
+</select>

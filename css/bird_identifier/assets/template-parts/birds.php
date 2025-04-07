730998781d853f886c58a2352afa120abac57101
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-04-07 11:20:21 +0100

    add data for birds list

---
+++ css/bird_identifier/assets/template-parts/birds.php
@@ -0,0 +1,24 @@
+<ul>
+    <li>Robin</li>
+    <li>Cormorant</li>
+    <li>House Sparrow</li>
+    <li>House Sparrows</li>
+    <li>Bullfinch</li>
+    <li>Grey Heron</li>
+    <li>European Jackdaw</li>
+    <li>White Goose</li>
+    <li>Duck</li>
+    <li>Yellow Hammer</li>
+    <li>Seagull</li>
+    <li>Egret</li>
+    <li>Blackbird</li>
+    <li>Commmon Blackbird (Female)</li>
+    <li>Magpie</li>
+    <li>Swan</li>  
+    <li>Common Merganser</li>
+    <li>Common Moorhen</li>
+    <li>Homing Woodpigeon</li>
+    <li>White-throated Dipper</li>
+    <li>Black-headed Gull</li>
+    <li>Little Grebe</li>
+</ul>

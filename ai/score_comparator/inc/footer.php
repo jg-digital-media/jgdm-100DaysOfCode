a38dfc4e1b5c61149bff6af93c73065adb149cc8
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-04-08 14:19:16 +0100

    add header and footer templates

---
+++ ai/score_comparator/inc/footer.php
@@ -0,0 +1,10 @@
+     
+    <footer>
+        
+        &copy; (2025) Created by <a href="https://www.jonniegrieve.co.uk" target="_blank">Jonnie Grieve Digital Media</a> All Rights Reserved
+        
+    </footer>
+
+    <script src="app.js"></script>
+</body>
+</html>
---
+++ ai/score_comparator/inc/header.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-N9WB623MVP"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-N9WB623MVP');
+    </script>
+
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css?family=Merriweather|Odibee+Sans|Quicksand&display=swap" rel="stylesheet">
+
+    <!-- Favicon -->
+    <link rel="icon" href="icon.png" type="image/png">
+
+    <!-- Meta Tags -->
+    <meta name="description" content="Welcome to the Football Score comparator. Match up your teams and see how their results compare over a season">
+    <meta name="keywords" content="Football, Football Results, Football Scores, Football Comparisons, Match Comparions, Match your teams, Compare football results">
+    <meta name="image" content="assets/images/app-image.png">
+
+    <!-- FACEBOOK: Open Graph -->
+    <meta property="og:title" content="Football Score Comparator | JGDM">
+    <meta property="og:description" content="Welcome to the Football Score comparator. Match up your teams and see how their results compare over a season">
+    <meta property="og:image" content="assets/images/app-image.png">
+    <meta property="og:url" content="https://projects.jonniegrieve.co.uk/score_comparator/">
+
+    <!-- TWITTER: Open Graph -->
+    <meta name="twitter:title" content="Football Score Comparator | JGDM">
+    <meta name="twitter:description" content="Welcome to the Football Score comparator. Match up your teams and see how their results compare over a season">
+    <meta name="twitter:image" content="assets/images/app-image.png">
+    <meta name="twitter:card" content="football_score_comparator">
+
+    <!-- Canonical link -->
+    <link rel="canonical" href="https://projects.jonniegrieve.co.uk/score_comparator/">
+    
+    <!-- Styles -->
+    <link rel="stylesheet" type="text/css" href="style.css" />
+
+    <title>Football Score Comparator</title>
+
+</head>
+<body>
+
+    <header>
+
+        <h1 class="main---title">Football Score Comparator</h1>
+        <div class="tagline">Based on Newcastle United <span>(v3)</span> <span id="about---this--app"><a href="about.php">About this app</a></span></div>
+        
+    </header>

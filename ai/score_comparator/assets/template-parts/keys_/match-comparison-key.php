c55ee1d243382ff8536c90427d3dc9b59b7e3efe
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-04-28 10:51:12 +0100

    use template part for comparison key

---
+++ ai/score_comparator/assets/template-parts/keys_/match-comparison-key.php
@@ -0,0 +1,14 @@
+        <div class="key key---match--win" title="Score Comparison: Win" alt="Score Comparison: Win">W</div>
+        <div class="key key---match--loss" title="Score Comparison: Lose" alt="Score Comparison: Lose">L</div>         
+        <div class="key key---match--draw" title="Score Comparison: Draw" alt="Score Comparison: Draw">D</div>  
+
+        <!-- <div class="key key---result--compares-higher" title="Match Result Compares Higher" alt="Match Result Compares Higher"></div> -->
+        <!--   -->       
+        <!-- <div class="key key---result--compares-lower" title="Match Result Compares Lower" alt="Match Result Compares Lower"></div> -->
+
+        <div class="key key---match--compares-largerwin" title="Compares as Larger win" alt="Compares as Larger win">LW</div>
+        <div class="key key---match--compares-largerwin" title="Compares as smaller win" alt="Compares as smaller win">SW</div>
+        <div class="key key---match--compares-largerdefeat" title="Compares as larger defeat" alt="Compares as larger defeat">LD</div>
+        <div class="key key---match--compares-smallerdefeat" title="Compares as smaller defeat" alt="Compares as smaller defeat">SD</div><div class="key key---result--matches-exactly" title="Match Result Matches Exactly" alt="Match Result Matches Exactly">EX</div>
+        <div class="key key---match--stilltoplay" title="Still to play" alt="Still to play">L</div>
+        <!-- -->

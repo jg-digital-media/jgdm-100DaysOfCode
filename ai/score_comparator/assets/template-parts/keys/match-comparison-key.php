7431813c433c359b1c5dcf73ffdb94c3552f5af7
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-04-29 10:49:40 +0100

    edit the key

--- ai/score_comparator/assets/template-parts/keys/match-comparison-key.php
+++ ai/score_comparator/assets/template-parts/keys/match-comparison-key.php
@@ -7,8 +7,7 @@
         <!-- <div class="key key---result--compares-lower" title="Match Result Compares Lower" alt="Match Result Compares Lower"></div> -->
 
         <div class="key key---match--compares-largerwin" title="Compares as Larger win" alt="Compares as Larger win">LW</div>
-        <div class="key key---match--compares-largerwin" title="Compares as smaller win" alt="Compares as smaller win">SW</div>
+        <div class="key key---match--compares-smallerwin" title="Compares as smaller win" alt="Compares as smaller win">SW</div>
         <div class="key key---match--compares-largerdefeat" title="Compares as larger defeat" alt="Compares as larger defeat">LD</div>
         <div class="key key---match--compares-smallerdefeat" title="Compares as smaller defeat" alt="Compares as smaller defeat">SD</div><div class="key key---result--matches-exactly" title="Match Result Matches Exactly" alt="Match Result Matches Exactly">EX</div>
-        <div class="key key---match--stilltoplay" title="Still to play" alt="Still to play">L</div>
-        <!-- -->
+        <!--<div class="key key---match--stilltoplay" title="Still to play" alt="Still to play">L</div>-->

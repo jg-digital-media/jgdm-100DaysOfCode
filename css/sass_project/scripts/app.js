06b08758f83f8c5ad28cbb94abbfd716d30567b1
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-05-01 11:43:29 +0100

    fix carousel slider to container ID

--- css/sass_project/scripts/app.js
+++ css/sass_project/scripts/app.js
@@ -12,10 +12,10 @@
  * // file path to json file - server - ../../../data/sass---project--list.json
  * // absolute path - https://www.jonniegrieve.co.uk/data/project-list.json
  * 
- * Last Update: 10:42 - 19/08/2024
+ * Last Update: 11:14 - 01/05/2025
  */
 
-console.log("connected to app.js")
+console.log("connected to app.js - 01-05-2025 - 11:29");
 
 /* Make the Carousel */ 
 $(document).ready(function() {
@@ -23,29 +23,49 @@ $(document).ready(function() {
     $('.js-carousel').slick({
 
         arrows: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        adaptiveHeight: true
     });
 });
 
 
 /* Get project list */
+
+
+$(document).ready(function() {
+
     //jQuery.getJSON('../../../data/sass---project--list.json', function(photoData) {     
     jQuery.getJSON('data/sass---project--list.json', function(photoData) { 
 
+        // get the dynamic count and display on screen
+        const image_total_count = document.getElementById("image_total_count");
+        const total =  photoData.projects.length 
 
-    // get the dynamic count and display on screen
-    const image_total_count = document.getElementById("image_total_count");
-    const total =  photoData.projects.length 
+        image_total_count.textContent = total;
+        
+        // iterate through the project instances via JSON
+        let itemAll = photoData.projects.length;
+        console.log( photoData.projects.length );
 
-    image_total_count.textContent = total;
-    
-    // iterate through the project instances via JSON
-    let itemAll = photoData.projects.length;
-    console.log( photoData.projects.length );
+        for (let i=0; i < itemAll; i++) { 
 
-    for (let i=0; i < itemAll; i++) { 
+            jQuery(`<img class="slide" src="${ photoData.projects[i].img_url}" alt="${ photoData.projects[i].project_alt }" />`).appendTo('#js_carousel');
 
-        jQuery(`<img class="slide" src="${ photoData.projects[i].img_url}" alt="${ photoData.projects[i].project_alt }" />`).appendTo('.js-carousel');
+        }
+        
+        $('#js_carousel').slick({
 
-    }
-    
+            arrows: false,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            adaptiveHeight: true
+        });
+        
+    });
+
 });

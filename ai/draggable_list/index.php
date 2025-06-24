5842e6b15fd06f47272ab923bef7e60b82f10cde
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-06-24 16:18:48 +0100

    new project - draggable list

---
+++ ai/draggable_list/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Draggable List</title>
+</head>
+<body>
+    
+    <h1>Draggable List</h1>
+
+    <ul id="list">       
+
+        <li>AFC Bournemouth</li>
+        <li>Arsenal FC</li>
+        <li>Aston Villa</li>
+        <li>Brentford FC</li>
+        <li>Brighton & Hove Albion</li>
+        <li>Burnley FC</li>
+        <li>Chelsea FC</li>
+        <li>Crystal Palace</li>
+        <li>Everton FC</li>
+        <li>Fulham FC</li>
+        <li>Leeds United</li>
+        <li>Liverpool FC</li>
+        <li>Manchester City</li>
+        <li>Manchester United</li>
+        <li>Newcastle United</li>
+        <li>Nottingham Forest</li>
+        <li>Sunderland AFC</li>
+        <li>Tottenham Hotspur</li>
+        <li>West Ham United</li>
+        <li>Wolverhampton Wanderers</li>
+    </ul>
+
+        
+
+</body>
+</html>

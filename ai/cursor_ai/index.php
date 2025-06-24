4f722cb0aea5412675cf5fb6051536004a996597
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-06-24 10:52:17 +0100

    update cursor ai homepage

--- ai/cursor_ai/index.php
+++ ai/cursor_ai/index.php
@@ -16,49 +16,46 @@
     </header>
 
     <main>
+
         <section id="section---about--cursor">
 
             <h2>About Cursor AI</h2>
 
-            <p>Cursor AI is the leading Text Editor that bundles with AI enhances your browsing experience by providing intelligent suggestions and actions based on your codebase.</p>
+            <p>Cursor AI is the leading Text Editor that bundles with AI to enhance your development experience by providing intelligent suggestions and actions based on your codebase.</p>
 
-            <p>Using this AI technology, I've been able to build projects that I've previously ever been able to do. It's not that I don't understand programming concepts.  It's more that I have a block in my head that prevents me from putting them togther.  Things like Condition statementws, event listeners, loopps, functions, arguments, parameters. They're all in there to a certain extent in my head.  It's like having the pieces of a jigsaw puzzle and simply not being able to work out what piece goes where. Ever.</p>
+            <p>Using this AI technology, I've been able to build projects that I've previously never been able to do. For me, it's not that I don't understand programming concepts. It's more that I have a block in my head that prevents me from putting them together and solving problems. Things like Condition statements, event listeners, loops, functions, arguments and parameters. They're all in there to a certain extent in my head. I liken it to having the pieces of a jigsaw puzzle and simply not being able to work out what piece goes where. Ever.</p>
                 
-            <p>And this is after over 20 years now of working wirh programming in some form.</p>
+            <p>And this is after over 20 years now of working with programming in some form.</p>
 
-            <h3>And this is where Vibe coding comes in.</h3>
+            <h3 class = "l3-heading">And this is where &quot;Vibe Coding&quot; comes in.</h3>
 
-            <p>I'm still learning about Vibe coding as I go but I recently realised i've been doing it more and more without really knowing it was a thing. </p>
+            <p>I'm still learning about Vibe Coding as I go, but I recently realised that I've been doing it more and more without really knowing it was a thing. </p>
             
-            <p>I said it was a "thing" but there's nothing official about the term. I don't think Computer Science approves or recommends the approach yet. But for people like me though it is a God send.  I've always said, Vibe code, or use AI to code, with caution.  You know you rely on it to build projects, but you also know you do have to spend time studying the code and understanding that it all it is doing is relying on assumptions. That is assumptions about your input and the data it scrapes online.
+            <p>I said it was a "thing", but there's nothing official about the term. I don't think Computer Science approves or recommends the approach yet. But for people like me, though, it is a Godsend. I've always said, Vibe code, or use AI to code, with caution.  You know you rely on it to build projects, but you also know you do have to spend time studying the code and understanding that all that is happening with all these language models (LLMs) is that they are relying on assumptions. That is to say assumptions about your input and the data it scrapes online..
 
-            <h3>Pros and Cons of Vibe Coding</h3>
+            <h3 class = "l3-heading">Pros and Cons of Vibe Coding</h3>
 
-            <p>Pros:</p>
+            <h4 class = "l4-heading" id="heading-pros">Pros:</h4>
             <ul>
-                <li>You can build projects that you've never been able to do before.</li>
-
-                <li><strong>Confidence Boost: </strong> You don't have to fear the blank page — AI can help sketch things out, and you edit/steer.</li>
-
+                <li><strong>You can build projects that you've never been able to do before.</strong> This is an empowering feeling knowing that with the right tools you basically have an electronic collaborator by your side who will never have enough of your requests.</li>
+                <li><strong>Confidence Boost: </strong> You don't have to fear the blank page — AI can help sketch things out, and you edit/steer as you go along.</li>
                 <li><strong>Exploration-Friendly: </strong> You can test out ideas fast without worrying about doing it the “proper” way first.</li>
-
-                <li><strong>Pair Programming Feel: </strong> Cursor and tools like me can guide you through thought processes, edge cases, or “what next?” moments.</li>
-                <li><strong>Speed & Flow: </strong> You can go from idea to working code super fast — even if you're fuzzy on the implementation.</li>
-                <li><strong>Learning On the Go: </strong> AI can explain or suggest things as you build, which helps build intuition without deep-diving docs every time. </li>             
+                <li><strong>Pair Programming Feel: </strong> Cursor and tools like ChatGPT and Anthropic's models can guide you through thought processes, edge cases, or “what next?” moments.</li>
+                <li><strong>Speed & Flow: </strong> You can go from idea to working code super fast — even if you're not sure about the implementation.</li>
+                <li><strong>Learning On the Go: </strong> AI can explain or suggest things as you build, which helps build intuition without deep-diving into documentation every time.</li>             
                 
             </ul>
 
-            <p>Cons:</p>
+            <h4 class = "l4-heading" id="heading-cons">Cons:</h4>
             <ul>
-                <li>You have to spend time studying the code and understanding that it all it is doing is relying on assumptions. That is assumptions about your input and the data it scrapes online.</li>
-                <li><strong>shallow Understanding Risk: </strong> </li>
+                <li><strong>Shallow Understanding Risk: </strong> It's one thing to have code generated for you by your chosen AI. You have to spend time studying the code and understanding that it all it is doing is relying on assumptions. That is assumptions about your input and the data it scrapes online. I have felt myself going down this route plenty of times where I got something working the way I want and that's enough for me. That's a path to be wary of if you care about your code and good development.</li>
                 <li><strong>Overconfidence Trap: </strong>AI-generated code can look right but be subtly wrong — you still need to review and reason through it.</li>
                 <li><strong>Less Structure: </strong>If you're constantly improvising with the AI, your code can become patchy, inconsistent, or hard to scale.</li>
                 <li><strong>AI Echo Chamber: </strong>If your prompts are vague or your direction's unclear, the AI might reinforce bad assumptions instead of challenge them.</li>
 
         </section>
 
-        <section>
+        <section id="section---cursor--features">
 
             <h2>Features of Cursor AI</h2>
 
@@ -93,13 +90,15 @@
 
             <p><a href="https://projects.jonniegrieve.co.uk/darts_scorer_app" target="_blank" class="projects---in--cursor">Darts Scorer App</a> - This is a simple web application that you can use to score your Darts matches. </p>
             <p><a href="https://projects.jonniegrieve.co.uk/score_comparator" target="_blank" class="projects---in--cursor">Score Comparator - Football</a> - Match up your teams and see how their results compare over a season.</p>
-            <p><a href="https://projects.jonniegrieve.co.uk/work_counter" target="_blank" class="projects---in--cursor">Work Counter</a> - Apply your hourly rate and watch the pennies count up until your job is done. (This could make a great standalome app or PWA).</p>    
+            <p><a href="https://projects.jonniegrieve.co.uk/work_counter" target="_blank" class="projects---in--cursor">Work Counter</a> - Apply your hourly rate and watch the pennies count up until your job is done. (This could make a great standalone app or <abbr title="Progressive Web Application">PWA</abbr>).</p>    
             <p><a href="https://projects.jonniegrieve.co.uk/ai/dynamic_list" target="_blank" class="projects---in--cursor">Dynamic List - Sqlite and PHP</a> - A list that persists - going beyond use of localstorage </p>  
             <p><a href="https://projects.jonniegrieve.co.uk/bird_identifier" target="_blank" class="projects---in--cursor">Bird Identifier</a> - Use this space to search for and identify the bird species that you see when you're out and about </p>
             <!--<p><a href="https://projects.jonniegrieve.co.uk/todo_list" target="_blank" class="projects---in--cursor">Interactive Todo List with Javascript and PHP</a> - A list that persists - going beyond use of localstorage</p>-->
-            <!--<p><a href="https://projects.jonniegrieve.co.uk/snooker_scorer" target="_blank" class="projects---in--cursor">Snooker Scorer</a></p>-->
+            <p><a href="https://projects.jonniegrieve.co.uk/snooker_scorer" target="_blank" class="projects---in--cursor">Snooker Scorer</a> - Use this app to score your Snooker Frames as your players play through them.</p>
+
         </section>
     </main>
+    
     <footer>
         <p>Page design generated by Cursor AI &copy; (2025) Cursor AI.</p>
     </footer>

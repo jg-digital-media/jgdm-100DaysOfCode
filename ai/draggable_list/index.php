e88dc4f581ee25ec2875ef00e580f0049a24fb36
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-07-03 15:52:06 +0100

    ordered list of football teams

--- ai/draggable_list/index.php
+++ ai/draggable_list/index.php
@@ -3,11 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Draggable List</title>
+
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     
     <h1>Draggable List</h1>
+
+    <a href="#" id="add---more--btn">Add More ></a>
 
     <ul id="list">       
 
@@ -34,6 +39,6 @@
     </ul>
 
         
-
+    <script src="app.js"></script>
 </body>
 </html>

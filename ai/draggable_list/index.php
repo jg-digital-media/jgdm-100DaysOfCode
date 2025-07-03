272039ec9c7a64248ee44a3b5e624b1f14eb01b3
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-07-03 16:11:58 +0100

    main nav list

--- ai/draggable_list/index.php
+++ ai/draggable_list/index.php
@@ -9,33 +9,20 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+  
+    <header>
     
-    <h1>Draggable List</h1>
+        <h1>Draggable List</h1>
 
-    <a href="#" id="add---more--btn">Add More ></a>
+        <a href="premierleague.php">Premier League</a>
 
-    <ul id="list">       
+    </header>
 
-        <li>AFC Bournemouth</li>
-        <li>Arsenal FC</li>
-        <li>Aston Villa</li>
-        <li>Brentford FC</li>
-        <li>Brighton & Hove Albion</li>
-        <li>Burnley FC</li>
-        <li>Chelsea FC</li>
-        <li>Crystal Palace</li>
-        <li>Everton FC</li>
-        <li>Fulham FC</li>
-        <li>Leeds United</li>
-        <li>Liverpool FC</li>
-        <li>Manchester City</li>
-        <li>Manchester United</li>
-        <li>Newcastle United</li>
-        <li>Nottingham Forest</li>
-        <li>Sunderland AFC</li>
-        <li>Tottenham Hotspur</li>
-        <li>West Ham United</li>
-        <li>Wolverhampton Wanderers</li>
+    <a href="#" id="add---more--btn">Add Item ></a>
+
+    <ul id="list">     
+
+        
     </ul>
 
         

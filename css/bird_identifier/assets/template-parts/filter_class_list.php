d33810cbe42336d60a4e60f6be13ac842f42ed25
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-07-14 13:34:23 +0100

    add simple list of filter classes

---
+++ css/bird_identifier/assets/template-parts/filter_class_list.php
@@ -0,0 +1,21 @@
+<!-- Bird List - filter_class_list.php -->
+<ul>
+
+    <li>bird---class--water</li>
+    <li>bird---class--garden</li>
+    <li>bird---class--urban</li>
+    <li>bird---class--coastal</li>
+    <li>bird---class--countryside</li>
+    <li>bird---class--small</li>
+    <li>bird---class--medium</li>
+    <li>bird---class--large</li>
+    <li>bird---class--birdprey</li>
+    <li>bird---class--red-orange</li>
+    <li>bird---class--black</li>
+    <li>bird---class--white-light</li>
+    <li>bird---class--yellow</li>
+    <li>bird---class--mixed-brown</li>
+    <li>bird---class--grey</li>
+    <li>bird---class--grey-black</li>
+    
+</ul>

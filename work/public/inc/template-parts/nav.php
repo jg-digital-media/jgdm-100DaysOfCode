a9d22356068a9a8393c0e8f034bafb5102b9d1cd
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-07-23 08:36:41 +0100

    move project directories

--- work/public/inc/template-parts/nav.php
+++
@@ -1,7 +0,0 @@
-<nav>
-    <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="previous_rounds.php">Previous Rounds</a></li>
-        <li><a href="journal.php">Journal</a></li>
-    </ul>
-</nav>

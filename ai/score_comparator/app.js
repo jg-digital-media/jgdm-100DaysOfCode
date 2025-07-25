a1cfc26b816c9a271169a2d1f07214acac1b0c2b
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-07-25 10:04:29 +0100

    fix cardiff match data 18-19

--- ai/score_comparator/app.js
+++ ai/score_comparator/app.js
@@ -31,7 +31,7 @@ document.addEventListener('DOMContentLoaded', function() {
             <option id="select---home--arsenal" value="Arsenal">Arsenal</option>
             <option id="select---home--brighton" value="Brighton & Hove Albion">Brighton & Hove Albion</option>
             <option id="select---home--burnley" value="Burnley">Burnley</option>  
-            <option id="select---home--cardiff" value="Chelsea">Cardiff City</option> 
+            <option id="select---home--cardiff" value="Cardiff City">Cardiff City</option> 
             <option id="select---home--chelsea" value="Chelsea">Chelsea</option>           
             <option id="select---home--crystalpalace" value="Crystal Palace">Crystal Palace</option>  
             <option id="select---home--everton" value="Everton">Everton</option>

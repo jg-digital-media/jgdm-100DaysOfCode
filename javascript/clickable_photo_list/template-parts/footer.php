9398f857231cda1f07dbb938460d0995d8f3adc0
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-09-03 09:26:31 +0100

    create footer and header templates

---
+++ javascript/clickable_photo_list/template-parts/footer.php
@@ -0,0 +1,13 @@
+  
+  
+  <footer>
+        
+        <p>Footer</p>
+
+        <!-- <a href="notes.php">Notes</a> -->
+
+    </footer>
+
+    <script type="text/javascript" src="app.js"></script>
+</body>
+</html>
---
+++ javascript/clickable_photo_list/template-parts/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- stylesheet -->
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+    <title>Clickable Photo List | Jonnie Grieve Digital Media</title>
+</head>
+<body>
+    
+    <header>
+
+        <h1>Clickable Photo List</h1>
+
+        <h2>A Subtitle</h2>
+
+    </header>
+    

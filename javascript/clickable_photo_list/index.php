ba1006a4e63ddd484c97c43dfd731b1f114be017
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-09-03 09:27:31 +0100

    build application markup

--- javascript/clickable_photo_list/index.php
+++ javascript/clickable_photo_list/index.php
@@ -3,3 +3,37 @@
 echo "<p>index.php</p>";
 
 ?>
+
+<?php include_once("template-parts/header.php"); ?>
+
+    <p><a href="notes.php">Notes</a></p>
+
+    <section class="photo---element--container">
+
+        <div id="photos">
+
+            <img src="https://jgdm-projects.s3.eu-west-2.amazonaws.com/clickable_photo_list/flowers/IMG_8194.JPG" alt="Daisy Flower">
+
+        </div>
+
+    </section>
+
+    <aside id="url---list">
+        
+        <ul>
+
+            <li><a href="https://jgdm-projects.s3.eu-west-2.amazonaws.com/clickable_photo_list/flowers/IMG_8194.JPG">Daisy Flower</a></li>
+
+            <li><a href="https://jgdm-projects.s3.eu-west-2.amazonaws.com/clickable_photo_list/flowers/IMG_8204.JPG">Japanese Anomone</a></li>
+
+            <li><a href="https://jgdm-projects.s3.eu-west-2.amazonaws.com/<li>clickable_photo_list/flowers/IMG_8197.JPG">Yarrow (Achillea millefolium)</a></li>
+
+            <li><a href="https://jgdm-projects.s3.eu-west-2.amazonaws.com/clickable_photo_list/flowers/IMG_8226.JPG">Echinops</a></li>
+
+            <li><a href="https://jgdm-projects.s3.eu-west-2.amazonaws.com/clickable_photo_list/flowers/IMG_8198.JPG">Calendula officinalis</a></li>
+
+        </ul>
+
+    </aside>
+   
+<?php include_once("template-parts/footer.php"); ?>
---
+++ javascript/clickable_photo_list/notes.php
@@ -0,0 +1,15 @@
+<?php 
+
+echo "<p>notes.php</p>";
+
+?>
+
+<?php include_once("template-parts/header.php"); ?>
+
+    <p><a href="index.php">Home</a></p>
+
+    <section>
+
+    </section>
+   
+<?php include_once("template-parts/footer.php"); ?>

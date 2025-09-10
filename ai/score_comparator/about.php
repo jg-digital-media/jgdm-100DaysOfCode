59ff76931925e81ca5b76ce42e8c5c778744e25f
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-09-10 16:21:44 +0100

    end phase 3.6 - dev note

--- ai/score_comparator/about.php
+++ ai/score_comparator/about.php
@@ -165,14 +165,15 @@
 
         </ul>
 
-         <code class="version---heading">v3.6</code>
+        <code class="version---heading">v3.6</code>
 
         <ul class="development---history"> 
 
             <li>Expanded the app for readiness to add match data for season <code><strong>2017/2018</strong></code>.</li>
             <li>Concurrently, I'm adding the scores incrementally for the <code><strong>2025/2026</strong></code> season which began during this phase. (15/08/2025)</li>
-            <!-- <li><code><strong>Phase ends (25/07/2025)</strong></code></li> -->
-
+            <li>Complete seasons include: <code><strong>2024/2025</strong></code>, <code><strong>2023/2024</strong></code>, <code><strong>2022/2023</strong></code>, <code><strong>2021/2022</strong></code> <code><strong>2020/2021</strong></code> <code><strong>2019/2020</strong></code>, <code><strong>2018/2019</strong> </code>, and <code><strong>2017/2018</strong></code>
+            <li><code><strong>Phase ends (10/09/2025)</strong></code></li>
+        </ul>
 
         <h2>Points to take.</h2>
 

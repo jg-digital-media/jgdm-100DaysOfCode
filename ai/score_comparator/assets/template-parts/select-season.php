5df89d24a313adbadcb78da2f988eb11130b47b4
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-12-01 13:15:12 +0000

    fix id attributes for consistency

--- ai/score_comparator/assets/template-parts/select-season.php
+++ ai/score_comparator/assets/template-parts/select-season.php
@@ -3,8 +3,8 @@
 
 <select id="form---select--season" class="select---season">  
 
-    <option class="select---season--option" id="option---2026" value="2026">2025/2026</option>
-    <option class="select---season--option" id="option---2025" value="2025">2024/2025</option>
+    <option class="select---season--option" id="season---2026" value="2026">2025/2026</option>
+    <option class="select---season--option" id="season---2025" value="2025">2024/2025</option>
     <option class="select---season--option" id="season---2024" value="2024">2023/2024</option> 
     <option class="select---season--option" id="season---2023" value="2023">2022/2023</option>  
     <option class="select---season--option" id="season---2022" value="2022">2021/2022</option>  

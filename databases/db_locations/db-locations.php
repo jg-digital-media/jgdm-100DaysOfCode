61169cfc36900ca38baab7dcd0a2f94e28bef5a9
Jonathan Grieve <mail@jonniegrieve.co.uk>
2026-04-08 12:29:33 +0100

    add db locations page

--- databases/db_locations/db-locations.php
+++ databases/db_locations/db-locations.php
@@ -1,1 +1,16 @@
-db_locations/index.php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Database Locations and Backups</title>
+</head>
+<body>
+
+    <h1>Database Locations and Backups</h1>
+    
+    <p>db_locations/db-locations.php.</p>    
+    
+</body>
+</html>
+

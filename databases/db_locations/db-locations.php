8134af847a6d117c9f2cd033c4848050430bd1b4
Jonathan Grieve <mail@jonniegrieve.co.uk>
2026-04-22 16:34:52 +0100

    add to db locations list

--- databases/db_locations/db-locations.php
+++ databases/db_locations/db-locations.php
@@ -10,6 +10,21 @@
     <h1>Database Locations and Backups</h1>
     
     <p>db_locations/db-locations.php.</p>    
+
+
+    <h2>repositories.json</h2>
+
+    <p>contains 42 items <code>  server/_assets/htdocs-backup/repositories.json<!-- https://projects.jonniegrieve.co.uk/assets/lists/repositories/repositories.json --></code></p>
+
+    <h2>project---data--dynamic.json (htdocs JSON data file)</h2>
+
+    <p>contains 307 items <code>  server/_assets/htdocs-backup/project---data--dynamic.json<!-- https://projects.jonniegrieve.co.uk/assets/lists/project_list/project---data--dynamic.json --></code></p>
+
+    <h2>project_list.php ></h2>
+    <p>contains 97 items<code>  jgdm-100daysofcode/database/db_project-list.json</code></p>
+
+    <h2>project_list.php </h2>
+    <p>contains 97 items - contains "project-dev-status" key<code>  subdomains/projects/assets/data/project-list.json</code></p>
     
 </body>
 </html>

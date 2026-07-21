f45050b4b9980fae894ff0bb993dff11e0cfbf24
Jonathan Grieve <mail@jonniegrieve.co.uk>
2026-07-21 14:16:50 +0100

    update project locations

--- databases/db_locations/db-locations.php
+++ databases/db_locations/db-locations.php
@@ -19,6 +19,9 @@
 
     <p>server/_assets/htdocs-backup/project---data--dynamic.json - contains <strong>310</strong> items <code> <!-- https://projects.jonniegrieve.co.uk/assets/lists/project_list/project---data--dynamic.json --></code></p>
 
+    <h2>project_list.json <strong><em>Not up to date</em></strong></h2>
+    <p>contains 94 items "project-dev-status" key<code>  subdomains/projects/assets/data/project_list.json</code></p>
+
     <h2>project_list.php <strong><em>Not up to date</em></strong></h2>
     <p>contains 93 items<code>  jgdm-100daysofcode/databases/db_locations/project-list.json</code></p>
 

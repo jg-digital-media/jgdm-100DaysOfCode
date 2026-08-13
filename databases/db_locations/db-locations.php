42163857935194b3754528a1a8c6df8838e24eae
Jonathan Grieve <mail@jonniegrieve.co.uk>
2026-08-13 12:41:15 +0100

    link script and styles

--- databases/db_locations/db-locations.php
+++ databases/db_locations/db-locations.php
@@ -3,30 +3,47 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
     <title>Database Locations and Backups</title>
+
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 
-    <h1>Database Locations and Backups</h1>
+    <header>
+
+        <h1>Database Locations and Backups</h1>
+
+    </header>
     
-    <p>db_locations/db-locations.php.</p>
 
-    <h2>repositories.json</h2>
+    <main>
 
-    <p>contains <strong>44</strong> items <code>  server/_assets/htdocs-backup/repositories.json<!-- https://projects.jonniegrieve.co.uk/assets/lists/repositories/repositories.json --></code></p>
+        <article class="database---location--instance">
 
-    <h2>htdocs JSON data file (Backup)</h2>
+            <p>db_locations/db-locations.php.</p>
 
-    <p>server/_assets/htdocs-backup/project---data--dynamic.json - contains <strong>310</strong> items <code> <!-- https://projects.jonniegrieve.co.uk/assets/lists/project_list/project---data--dynamic.json --></code></p>
+            <h2>repositories.json</h2>
 
-    <h2>project_list.json <strong><em>Not up to date</em></strong></h2>
-    <p>contains 94 items "project-dev-status" key<code>  subdomains/projects/assets/data/project_list.json</code></p>
+        </article>
 
-    <h2>project_list.php <strong><em>Not up to date</em></strong></h2>
-    <p>contains 93 items<code>  jgdm-100daysofcode/databases/db_locations/project-list.json</code></p>
+        <p>contains <strong>44</strong> items <code>  server/_assets/htdocs-backup/repositories.json<!-- https://projects.jonniegrieve.co.uk/assets/lists/repositories/repositories.json --></code></p>
 
-    <h2>project_list.php <strong><em>Not up to date</em></strong></h2>
-    <p>contains 93 items - contains "project-dev-status" key<code>  subdomains/projects/assets/data/project-list.json</code></p>
+        <h2>htdocs JSON data file (Backup)</h2>
+
+        <p>server/_assets/htdocs-backup/project---data--dynamic.json - contains <strong>310</strong> items <code> <!-- https://projects.jonniegrieve.co.uk/assets/lists/project_list/project---data--dynamic.json --></code></p>
+
+        <h2>project_list.json <strong><em>Not up to date</em></strong></h2>
+        <p>contains 94 items "project-dev-status" key<code>  subdomains/projects/assets/data/project_list.json</code></p>
+
+        <h2>project_list.php <strong><em>Not up to date</em></strong></h2>
+        <p>contains 93 items<code>  jgdm-100daysofcode/databases/db_locations/project-list.json</code></p>
+
+        <h2>project_list.php <strong><em>Not up to date</em></strong></h2>
+        <p>contains 93 items - contains "project-dev-status" key<code>  subdomains/projects/assets/data/project-list.json</code></p>
+
+    </main>
     
 </body>
 </html>

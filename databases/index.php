6679655a0feeeddd37d5dfdb61b3140043dcabda
Jonathan Grieve <mail@jonniegrieve.co.uk>
2026-08-13 15:46:51 +0100

    revert dir as databases page

--- databases/index.php
+++ databases/index.php
@@ -3,37 +3,89 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-N9WB623MVP"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
+    <link href="https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&amp;family=Karla:ital,wght@0,200..800;1,200..800&amp;family=Noto+Sans:ital,wght@0,100..900;1,100..900&amp;family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;family=Quicksand:wght@300..700&amp;family=Roboto:ital,wght@0,100..900;1,100..900" rel="stylesheet">
 
-        gtag('config', 'G-N9WB623MVP');
-    </script>
+    <title>Database Locations and Backups (Local)</title>
 
-    <link rel="stylesheet" type="text/css"href="../../style.css">    
-
-    <title>JGDM #100daysOfCode | Databases</title>
-
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
-
 <body>
 
-    <section>
+    <header>
+        
+        <img src="jg_logo.png" alt="jgdm logo" title="jgdm logo" id="page_logo">
+
+        <h1>Database Locations and Backups (Local)</h1>
+
+    </header>
+    
+
+    <main>
+
+        <article class="database---location--instance">
+
+            <h2>repositories.json <em class="database---project--style">status text - not up to date</em></h2>
+
+            <p>Repository JSON file</p>
+            <p class="record---count">0</p>
+            <p class="db---location">Location: <code>db_locations/db-locations.php</code></p>
+
+        </article>
+
+        <article class="database---location--instance">
+
+            <h2>htdocs JSON data file (Backup) <em class="database---project--style">status text - not up to date</em></h2>
+            <p>contains <strong>44</strong> items <code></code></p>
+            <!-- https://projects.jonniegrieve.co.uk/assets/lists/repositories/repositories.json -->             
+            <p class="record---count">0</p>
+            <p class="db---location">Location: <code>server/_assets/htdocs-backup/repositories.json</code></p>
       
-        <h1>&quot;100DaysOfCode&quot; Database Files on localhost <a id="goback" href="../">Back</a> </h1> 
+        </article>
 
-        <ul>
-            <li>project-list.json - json file <span class="project_status review">review</span></li>
-            <li>project-list.sql - sql file <span class="project_status review">review</span></li>
-            <li>repositories.json - json file <span class="project_status review">review</span></li>
-        </ul>
+        <article class="database---location--instance">
 
-    </section>
+            <h2>project---data--dynamic.json <em class="database---project--style">Not up to date</em></h2>
+            <p>DB description - contains <strong>310</strong> items <code> <!-- https://projects.jonniegrieve.co.uk/assets/lists/project_list/project---data--dynamic.json --></code></p>
+            <p class="record---count">0</p>
+            <p class="db---location">Location: <code>server/_assets/htdocs-backup/project---data--dynamic.json</code></p>
 
+        </article>
+
+        <article class="database---location--instance">
+
+            <h2>project_list.json <em class="database---project--style">Not up to date</em></h2>
+            <p>contains 94 items "project-dev-status" key</p>
+            <p class="record---count">0</p>
+            <p class="db---location">Location: <code>subdomains/projects/assets/data/project_list.json</code></p>
+
+        </article>
+
+        <article class="database---location--instance">
+
+            <h2>project_list.php <em class="database---project--style">Not up to date</em></h2>
+            <p>contains 93 items</p>
+            <p class="record---count">0</p>
+            <p class="db---location">Location: <code>jgdm-100daysofcode/databases/db_locations/project-list.json</code></p>
+
+        </article>
+
+        <article class="database---location--instance">
+
+            <h2>project_list.php <em class="database---project--style">Not up to date</em></h2>
+            <p>contains 93 items - contains "project-dev-status" key</p>
+            <p class="record---count">0</p>
+            <p class="db---location">Location: <code>subdomains/projects/assets/data/project-list.json</code></p>
+
+        </article>
+
+    </main>
+
+    <script src="app.js" type="text/javascript"></script>
+    
 </body>
 </html>
+

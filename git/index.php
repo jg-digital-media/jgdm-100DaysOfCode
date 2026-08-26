a38f84f1940b3e654e6f4792a0cdd7396aa94dda
Jonathan Grieve <mail@jonniegrieve.co.uk>
2026-08-26 14:25:05 +0100

    add command list page

---
+++ git/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Git Commands</title>
+</head>
+<body>
+
+    <h1>Git Commands</h1>
+
+    <p>index.php</p>
+
+
+<!--     
+    
+git push origin new_branding  - push new branch to remote reository
+    
+<div class="command-item">
+    <dt>Xampp Directory!</dt>
+    <dd>
+        <code>cd c:/xampp/htdocs</code>
+        <button type="button" class="copy-btn">Copy</button>
+    </dd>
+</div>
+
+-->
+</body>
+</html>
